d619ba35e4d316556de3644d9feda3756dec3326
mensk <mensk@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2002-04-29 14:17:58 +0000

    added class generation to framework tests
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@48 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ tests/wo/frameworks/art/src/java/org/objectstyle/art/Exhibit.java
@@ -0,0 +1,10 @@
+
+package org.objectstyle.art;
+
+
+import com.webobjects.foundation.*;
+import com.webobjects.eocontrol.*;
+
+/** Exhibit	*/
+public class Exhibit extends org.objectstyle.art.auto._Exhibit {
+}
