7d041ddb53acd36e170a49eb7a0274fd42750f10
ulrichk <ulrichk@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2002-12-18 16:39:01 +0000

    Improved the early startup.
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@483 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- src/wolips/java/org/objectstyle/wolips/plugin/EarlyStartup.java
+++ src/wolips/java/org/objectstyle/wolips/plugin/EarlyStartup.java
@@ -1,10 +1,67 @@
+/* ====================================================================
+ *
+ * The ObjectStyle Group Software License, Version 1.0
+ *
+ * Copyright (c) 2002 The ObjectStyle Group
+ * and individual authors of the software.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        ObjectStyle Group (http://objectstyle.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "ObjectStyle Group" and "Cayenne"
+ *    must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact andrus@objectstyle.org.
+ *
+ * 5. Products derived from this software may not be called "ObjectStyle"
+ *    nor may "ObjectStyle" appear in their names without prior written
+ *    permission of the ObjectStyle Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE OBJECTSTYLE GROUP OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the ObjectStyle Group.  For more
+ * information on the ObjectStyle Group, please see
+ * <http://objectstyle.org/>.
+ *
+ */
+
 package org.objectstyle.wolips.plugin;
 
 import java.net.URL;
 
-import org.eclipse.ant.core.AntRunner;
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jdt.core.ElementChangedEvent;
@@ -13,6 +70,7 @@ import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 import org.objectstyle.wolips.IWOLipsPluginConstants;
 import org.objectstyle.wolips.WOLipsPlugin;
+import org.objectstyle.wolips.builder.RunAnt;
 import org.objectstyle.wolips.env.Environment;
 import org.objectstyle.wolips.io.WOLipsLog;
 import org.objectstyle.wolips.listener.JavaElementChangeListener;
@@ -46,12 +104,14 @@ public class EarlyStartup {
 		}
 		EarlyStartup.validateMandatoryAttributes();
 		// add resource change listener to update project file on resource changes
-		IResourceChangeListener resourceChangeListener = new ResourceChangeListener();
+		IResourceChangeListener resourceChangeListener =
+			new ResourceChangeListener();
 		WorkbenchHelper.getWorkspace().addResourceChangeListener(
 			resourceChangeListener,
 			IResourceChangeEvent.PRE_AUTO_BUILD);
 		// add element change listener to update project file on classpath changes
-		IElementChangedListener javaElementChangeListener = new JavaElementChangeListener();
+		IElementChangedListener javaElementChangeListener =
+			new JavaElementChangeListener();
 		JavaCore.addElementChangedListener(
 			javaElementChangeListener,
 			ElementChangedEvent.PRE_AUTO_BUILD);
@@ -60,16 +120,17 @@ public class EarlyStartup {
 	 * Method writePropertiesFileToUserHome.
 	 */
 	private static void writePropertiesFileToUserHome() throws Exception {
-		AntRunner antRunner = new AntRunner();
 		URL relativeBuildFile =
 			new URL(
 				WOLipsPlugin.baseURL(),
 				IWOLipsPluginConstants.build_user_home_properties);
 		URL buildFile = Platform.asLocalURL(relativeBuildFile);
-		antRunner.setBuildFileLocation(buildFile.getPath());
-		antRunner.run();
+		IProgressMonitor monitor = new NullProgressMonitor();
+		RunAnt.asAnt(buildFile.getFile().toString(), monitor);
 	}
-
+	/**
+	 * Method validateMandatoryAttributes.
+	 */
 	private static void validateMandatoryAttributes() {
 		if (JavaCore.getClasspathVariable(Environment.NEXT_ROOT) == null) {
 			try {
