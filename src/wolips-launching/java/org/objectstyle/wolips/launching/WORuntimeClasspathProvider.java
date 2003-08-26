63c003b0c65dc6e78e19abb9a72c81c6056777d2
ulrichk <ulrichk@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2003-08-26 09:00:18 +0000

    A jar is a file not a folder.
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@1183 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- src/wolips-launching/java/org/objectstyle/wolips/launching/WORuntimeClasspathProvider.java
+++ src/wolips-launching/java/org/objectstyle/wolips/launching/WORuntimeClasspathProvider.java
@@ -24,7 +24,6 @@ import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.jdt.launching.StandardClasspathProvider;
 import org.objectstyle.wolips.core.project.WOLipsProject;
 
-
 /**
  * @author hn3000
  *
@@ -33,114 +32,138 @@ import org.objectstyle.wolips.core.project.WOLipsProject;
  */
 public class WORuntimeClasspathProvider
 	extends StandardClasspathProvider
-	implements IRuntimeClasspathProvider 
-{
-  public final static String ID = "org.objectstyle.wolips.launching.WORuntimeClasspath";
+	implements IRuntimeClasspathProvider {
+	public final static String ID =
+		"org.objectstyle.wolips.launching.WORuntimeClasspath";
 
-  /* (non-Javadoc)
-	 * @see org.eclipse.jdt.launching.IRuntimeClasspathProvider#computeUnresolvedClasspath(org.eclipse.debug.core.ILaunchConfiguration)
-	 */
+	/* (non-Javadoc)
+		 * @see org.eclipse.jdt.launching.IRuntimeClasspathProvider#computeUnresolvedClasspath(org.eclipse.debug.core.ILaunchConfiguration)
+		 */
 	public IRuntimeClasspathEntry[] computeUnresolvedClasspath(ILaunchConfiguration configuration)
-		throws CoreException 
-  {
+		throws CoreException {
 		return super.computeUnresolvedClasspath(configuration);
 	}
 
-
-  /* (non-Javadoc)
-	 * @see org.eclipse.jdt.launching.IRuntimeClasspathProvider#resolveClasspath(org.eclipse.jdt.launching.IRuntimeClasspathEntry[], org.eclipse.debug.core.ILaunchConfiguration)
-	 */
+	/* (non-Javadoc)
+		 * @see org.eclipse.jdt.launching.IRuntimeClasspathProvider#resolveClasspath(org.eclipse.jdt.launching.IRuntimeClasspathEntry[], org.eclipse.debug.core.ILaunchConfiguration)
+		 */
 	public IRuntimeClasspathEntry[] resolveClasspath(
 		IRuntimeClasspathEntry[] entries,
-		ILaunchConfiguration configuration
-  )
-		throws CoreException 
-  {
-    
-    
-    List others = null;
-    List resolved = null;
-    
-    // used for duplicate removal
-    Set allEntries = new HashSet();
+		ILaunchConfiguration configuration)
+		throws CoreException {
 
-    // resolve WO framework projects ourselves, let super do the rest
-    for (int i = 0; i < entries.length; ++i) {
-      IResource archive = _getWOJavaArchive(entries[i]);
-      if (null != archive) {
-        if (!allEntries.contains(archive.getLocation())) {
-          if (null == resolved) resolved = new ArrayList ();
-          resolved.add(JavaRuntime.newArchiveRuntimeClasspathEntry(archive));
-          
-          allEntries.add(archive.getLocation());
-        }
-      } else {
-        if (null == others) others = new ArrayList ();
-        others.add(entries[i]);
-      }
-    }
+		List others = null;
+		List resolved = null;
 
-    // ... let super do the rest but remove duplicates from the resulting classpath ...
-    if (null != others) {
-      IRuntimeClasspathEntry oe[] = super.resolveClasspath((IRuntimeClasspathEntry[])others.toArray(new IRuntimeClasspathEntry[others.size()]), configuration);
+		// used for duplicate removal
+		Set allEntries = new HashSet();
 
-      if (null == resolved) resolved = new ArrayList ();
-      
-      for (int i = 0; i < oe.length; ++i) {
-        IRuntimeClasspathEntry entry = oe[i];
-        String  ls = entry.getLocation();
-        IPath loc = (null == ls) ? null : new Path(ls); 
-        if (null == loc) {
-          resolved.add(entry);
-        } else {
-          if (!allEntries.contains(loc)) {
-            resolved.add(entry);
-            allEntries.add(loc);
-          }
-        }
-      }
-    }
+		// resolve WO framework projects ourselves, let super do the rest
+		for (int i = 0; i < entries.length; ++i) {
+			IResource archive = _getWOJavaArchive(entries[i]);
+			if (null != archive) {
+				if (!allEntries.contains(archive.getLocation())) {
+					if (null == resolved)
+						resolved = new ArrayList();
+					resolved.add(
+						JavaRuntime.newArchiveRuntimeClasspathEntry(archive));
 
-    return (IRuntimeClasspathEntry[])resolved.toArray(new IRuntimeClasspathEntry[resolved.size()]);
+					allEntries.add(archive.getLocation());
+				}
+			} else {
+				if (null == others)
+					others = new ArrayList();
+				others.add(entries[i]);
+			}
+		}
+
+		// ... let super do the rest but remove duplicates from the resulting classpath ...
+		if (null != others) {
+			IRuntimeClasspathEntry oe[] =
+				super.resolveClasspath(
+					(IRuntimeClasspathEntry[]) others.toArray(
+						new IRuntimeClasspathEntry[others.size()]),
+					configuration);
+
+			if (null == resolved)
+				resolved = new ArrayList();
+
+			for (int i = 0; i < oe.length; ++i) {
+				IRuntimeClasspathEntry entry = oe[i];
+				String ls = entry.getLocation();
+				IPath loc = (null == ls) ? null : new Path(ls);
+				if (null == loc) {
+					resolved.add(entry);
+				} else {
+					if (!allEntries.contains(loc)) {
+						resolved.add(entry);
+						allEntries.add(loc);
+					}
+				}
+			}
+		}
+
+		return (IRuntimeClasspathEntry[]) resolved.toArray(
+			new IRuntimeClasspathEntry[resolved.size()]);
 	}
 
-  IResource _getWOJavaArchive (IRuntimeClasspathEntry entry) throws CoreException {
-    IResource result = null;
-    
-    if (IRuntimeClasspathEntry.PROJECT == entry.getType()) {
-      IProject project = (IProject)entry.getResource();
-      
-      WOLipsProject wop = new WOLipsProject (project);
-      WOLipsProject.NaturesAccessor na = wop.getNaturesAccessor();
-      
-      String projectName = project.getName();
-      String projectNameLC = projectName.toLowerCase();
-      
-      // I'd rather use the knowledge from the IncrementalNature, but that fragment is not
-      // visible here (so I can't use the class, I think) [hn3000]
-      if (na.isFramework()) {
-        if (na.isAnt()) {
-          result = project.getFolder("dist/"+projectName+".framework/Resources/Java/"+projectNameLC+".jar");
-        } else if (na.isIncremental()) {
-          result = project.getFolder("build/"+projectName+".framework/Resources/Java");
-        }
-      } else if (na.isApplication()) { // must be application
-        if (na.isAnt()) {
-          result = project.getFolder("dist/"+projectName+".woa/Contents/Resources/Java/"+projectNameLC+".jar");
-        } else if (na.isIncremental()) {
-          result = project.getFolder("build/"+projectName+".woa/Contents/Resources/Java");
-        }
-      }
-      
-      // check if folder exists, otherwise let Eclipse to its default thing
-      if (
-        (null != result) 
-        && (!result.exists())
-      )  {
-        System.out.println("expected resource is not there: "+result.getLocation().toOSString());
-        result = null;
-      } 
-    }
-    return result;
-  }
+	IResource _getWOJavaArchive(IRuntimeClasspathEntry entry)
+		throws CoreException {
+		IResource result = null;
+
+		if (IRuntimeClasspathEntry.PROJECT == entry.getType()) {
+			IProject project = (IProject) entry.getResource();
+
+			WOLipsProject wop = new WOLipsProject(project);
+			WOLipsProject.NaturesAccessor na = wop.getNaturesAccessor();
+
+			String projectName = project.getName();
+			String projectNameLC = projectName.toLowerCase();
+
+			// I'd rather use the knowledge from the IncrementalNature, but that fragment is not
+			// visible here (so I can't use the class, I think) [hn3000]
+			if (na.isFramework()) {
+				if (na.isAnt()) {
+					result =
+						project.getFile(
+							"dist/"
+								+ projectName
+								+ ".framework/Resources/Java/"
+								+ projectNameLC
+								+ ".jar");
+				} else if (na.isIncremental()) {
+					result =
+						project.getFolder(
+							"build/"
+								+ projectName
+								+ ".framework/Resources/Java");
+				}
+			} else if (na.isApplication()) { // must be application
+				if (na.isAnt()) {
+					result =
+						project.getFile(
+							"dist/"
+								+ projectName
+								+ ".woa/Contents/Resources/Java/"
+								+ projectNameLC
+								+ ".jar");
+				} else if (na.isIncremental()) {
+					result =
+						project.getFolder(
+							"build/"
+								+ projectName
+								+ ".woa/Contents/Resources/Java");
+				}
+			}
+
+			// check if folder exists, otherwise let Eclipse to its default thing
+			if ((null != result) && (!result.exists())) {
+				System.out.println(
+					"expected resource is not there: "
+						+ result.getLocation().toOSString());
+				result = null;
+			}
+		}
+		return result;
+	}
 }
