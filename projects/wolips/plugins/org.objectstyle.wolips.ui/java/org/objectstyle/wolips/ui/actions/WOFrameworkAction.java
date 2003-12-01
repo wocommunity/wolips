283b4870afa207288f4d04d135380cd014b2adc1
ulrichk <ulrichk@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2003-12-01 10:06:02 +0000

    Removed old filter.
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@1354 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- projects/wolips/plugins/org.objectstyle.wolips.ui/java/org/objectstyle/wolips/ui/actions/WOFrameworkAction.java
+++ projects/wolips/plugins/org.objectstyle.wolips.ui/java/org/objectstyle/wolips/ui/actions/WOFrameworkAction.java
@@ -56,29 +56,9 @@
 
 package org.objectstyle.wolips.ui.actions;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.Dialog;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.window.Window;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableItem;
-import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.dialogs.SelectionDialog;
 import org.objectstyle.wolips.core.plugin.WOLipsPlugin;
-import org.objectstyle.wolips.core.project.IClasspathVariablesAccessor;
-import org.objectstyle.wolips.core.project.WOLipsCore;
 import org.objectstyle.wolips.core.util.WorkbenchUtilities;
-import org.objectstyle.wolips.ui.view.WOFrameworkDialogWrapper;
 
 /**
  * Adding WOFrameworks
@@ -87,16 +67,6 @@ import org.objectstyle.wolips.ui.view.WOFrameworkDialogWrapper;
  *
  */
 public class WOFrameworkAction extends ActionOnIProject {
-
-	private static String WOSystemFrameworkAddID = "WOSystemFramework.Add.ID";
-	private static String WOLocalFrameworkAddID = "WOLocalFramework.Add.ID";
-	private static String WOUserHomeFrameworkAddID =
-		"WOUserHomeFramework.Add.ID";
-	private static String ProjectWonderHomeFrameworkAddID =
-		"ProjectWonderHomeFramework.Add.ID";
-	private static String WOOtherFrameworkAddID = "WOOtherFramework.Add.ID";
-	private static String SelectOneClasspathVariableKey =
-		"SelectOneClasspathVariable";
 
 	/**
 	 * Constructor for WOFrameworkAction.
@@ -115,152 +85,6 @@ public class WOFrameworkAction extends ActionOnIProject {
 	 */
 	public void run(IAction action) {
 		WOLipsPlugin.informUser(WorkbenchUtilities.getShell(), "Please use the WOLips Classpath Container instead.");
-		if (true)
-			return;
-		if (project() == null)
-			return;
-		IJavaProject javaProject = JavaCore.create(project());
-		if (action.getId().equals(WOSystemFrameworkAddID)) {
-			WOFrameworkAction.frameworkDialog(
-				this.part,
-				javaProject,
-				WOLipsPlugin
-					.getDefault()
-					.getWOEnvironment()
-					.getNEXT_SYSTEM_ROOT(),
-				false,
-				true);
-			return;
-		} else if (action.getId().equals(WOLocalFrameworkAddID)) {
-			WOFrameworkAction.frameworkDialog(
-				this.part,
-				javaProject,
-				WOLipsPlugin
-					.getDefault()
-					.getWOEnvironment()
-					.getNEXT_LOCAL_ROOT(),
-				true,
-				true);
-			return;
-		} else if (action.getId().equals(WOUserHomeFrameworkAddID)) {
-			WOFrameworkAction.frameworkDialog(
-				this.part,
-				javaProject,
-				IClasspathVariablesAccessor.UserHomeClasspathVariable,
-				false,
-				true);
-			return;
-		} else if (action.getId().equals(ProjectWonderHomeFrameworkAddID)) {
-			WOFrameworkAction.frameworkDialog(
-				this.part,
-				javaProject,
-				IClasspathVariablesAccessor.ProjectWonderHomeClasspathVariable,
-				false,
-				true);
-			return;
-		} else if (action.getId().equals(WOOtherFrameworkAddID)) {
-			Object[] classpathVariables = this.selectClasspath();
-			if (classpathVariables == null)
-				return;
-			for (int i = 0; i < classpathVariables.length; i++) {
-				String classpathVariable = (String) classpathVariables[i];
-				WOFrameworkAction.frameworkDialog(
-					this.part,
-					javaProject,
-					classpathVariable,
-					false,
-					false);
-				return;
-			}
-		}
-		MessageDialog.openInformation(
-			this.part.getSite().getShell(),
-			Messages.getString("ErrorDialog.title"),
-			Messages.getString("ErrorDialog.invalid.selection"));
-	}
-
-	/**
-	 * @return
-	 */
-	private Object[] selectClasspath() {
-		SelectionDialog dialog =
-			new SelectionDialog(part.getSite().getShell()) {
-			private Table includeTable;
-
-			protected Control createDialogArea(Composite parentComposite) {
-				Composite composite =
-					(Composite) super.createDialogArea(parentComposite);
-				Composite parent = new Composite(composite, SWT.NULL);
-				GridLayout layout = new GridLayout();
-				layout.marginWidth = 0;
-				layout.marginHeight = 0;
-				layout.numColumns = 2;
-				parent.setLayout(layout);
-				GridData data = new GridData();
-				data.verticalAlignment = GridData.FILL;
-				data.horizontalAlignment = GridData.FILL;
-				parent.setLayoutData(data);
-
-				//includeTable = new Table(parent, SWT.CHECK | SWT.BORDER);
-				includeTable = new Table(parent, SWT.CHECK | SWT.BORDER);
-				GridData gd = new GridData(GridData.FILL_BOTH);
-				gd.widthHint = this.convertWidthInCharsToPixels(20);
-				//gd.widthHint = 150;
-				gd.heightHint = 200;
-				includeTable.setLayoutData(gd);
-				List list = this.getInitialElementSelections();
-				for (int i = 0; i < list.size(); i++) {
-					String string = list.get(i).toString();
-					if (WOLipsCore
-						.getClasspathVariablesAccessor()
-						.isUnderWOLipsControl(string))
-						continue;
-					TableItem item = new TableItem(includeTable, SWT.NONE);
-					item.setText(string);
-					item.setChecked(false);
-				}
-				Dialog.applyDialogFont(parent);
-				return parent;
-			}
-
-			protected void okPressed() {
-				TableItem[] items = includeTable.getItems();
-				ArrayList arrayList = new ArrayList();
-				for (int i = 0; i < items.length; i++) {
-					TableItem item = items[i];
-					if (item.getChecked())
-						arrayList.add(item.getText());
-				}
-				this.setResult(arrayList);
-				super.okPressed();
-			}
-		};
-		String[] classpathVariables = JavaCore.getClasspathVariableNames();
-		dialog.setInitialSelections(classpathVariables);
-		dialog.setTitle(
-			Messages.getString(
-				WOFrameworkAction.SelectOneClasspathVariableKey));
-		dialog.open();
-
-		if (dialog.getReturnCode() != Window.OK)
-			return null;
-		Object[] result = dialog.getResult();
-		return result;
-	}
-
-	private static void frameworkDialog(
-		IWorkbenchPart aPart,
-		IJavaProject aProject,
-		String classPathVariableName,
-		boolean addLocalFrameworkSectionToPBProject,
-		boolean addLibraryFrameworks) {
-		WOFrameworkDialogWrapper frameworkDialog =
-			new WOFrameworkDialogWrapper(
-				aPart,
-				aProject,
-				classPathVariableName,
-				addLocalFrameworkSectionToPBProject,
-				addLibraryFrameworks);
-		frameworkDialog.executeDialog();
+		return;
 	}
 }
