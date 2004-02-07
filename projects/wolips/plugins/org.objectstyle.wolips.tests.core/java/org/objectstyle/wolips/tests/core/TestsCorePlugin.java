6cca9c4720ede6184a1ceac89ffecab8357777e4
ulrichk <ulrichk@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2004-02-07 15:36:42 +0000

    New tests core plugin.
    git-svn-id: https://svn.objectstyle.org/repos/woproject/branches/eclipse_3_0_0@1520 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ projects/wolips/plugins/org.objectstyle.wolips.tests.core/java/org/objectstyle/wolips/tests/core/TestsCorePlugin.java
@@ -0,0 +1,16 @@
+package org.objectstyle.wolips.tests.core;
+
+import org.eclipse.core.runtime.*;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class TestsCorePlugin extends Plugin {
+	
+	/**
+	 * The constructor.
+	 */
+	public TestsCorePlugin(IPluginDescriptor descriptor) {
+		super(descriptor);
+	}
+}
