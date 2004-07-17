ad25e7927e9bcc4a78442056e79cbcb2c73a9791
ulrichk <ulrichk@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2004-07-17 08:09:24 +0000

    *** empty log message ***
    git-svn-id: https://svn.objectstyle.org/repos/woproject/branches/eclipse_3_0_0@1754 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ projects/wolips/goodies/mac/NativeHelper/Controller.h
@@ -0,0 +1,11 @@
+/* Controller */
+
+#import <Cocoa/Cocoa.h>
+
+@interface Controller : NSObject
+{
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification;
+
+@end
---
+++ projects/wolips/goodies/mac/NativeHelper/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  NativeHelper
+//
+//  Created by Ulrich Kšster on Fri Jun 11 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
