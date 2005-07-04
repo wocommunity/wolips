abe88db77a313bf49a66cde398bd4db6e1ee6748
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2005-07-04 15:07:15 +0000

    Attempt to fix Petri's MissingResourceException
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@2013 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- projects/wolips/plugins/org.objectstyle.wolips.wodclipse/java/org/objectstyle/wolips/wodclipse/editors/WODEditor.java
+++ projects/wolips/plugins/org.objectstyle.wolips.wodclipse/java/org/objectstyle/wolips/wodclipse/editors/WODEditor.java
@@ -1,5 +1,7 @@
 package org.objectstyle.wolips.wodclipse.editors;
 
+import java.util.ResourceBundle;
+
 import org.eclipse.jdt.internal.ui.IJavaHelpContextIds;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.IVerticalRuler;
@@ -8,7 +10,6 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.editors.text.TextEditor;
 import org.eclipse.ui.texteditor.ContentAssistAction;
 import org.eclipse.ui.texteditor.ITextEditorActionDefinitionIds;
-import org.objectstyle.wolips.wodclipse.WodclipsePlugin;
 
 public class WODEditor extends TextEditor {
   public WODEditor() {
@@ -25,7 +26,11 @@ public class WODEditor extends TextEditor {
 
   protected void createActions() {
     super.createActions();
-    ContentAssistAction action = new ContentAssistAction(WodclipsePlugin.getDefault().getResourceBundle(), "ContentAssistProposal.", this); //$NON-NLS-1$
+
+    String BUNDLE_FOR_CONSTRUCTED_KEYS = "org.eclipse.jdt.internal.ui.javaeditor.ConstructedJavaEditorMessages";//$NON-NLS-1$
+    ResourceBundle fgBundleForConstructedKeys = ResourceBundle.getBundle(BUNDLE_FOR_CONSTRUCTED_KEYS);
+    //WodclipsePlugin.getDefault().getResourceBundle()
+    ContentAssistAction action = new ContentAssistAction(fgBundleForConstructedKeys, "ContentAssistProposal.", this); //$NON-NLS-1$
     action.setActionDefinitionId(ITextEditorActionDefinitionIds.CONTENT_ASSIST_PROPOSALS);
     setAction("ContentAssistProposal", action);
     markAsStateDependentAction("ContentAssistProposal", true);
