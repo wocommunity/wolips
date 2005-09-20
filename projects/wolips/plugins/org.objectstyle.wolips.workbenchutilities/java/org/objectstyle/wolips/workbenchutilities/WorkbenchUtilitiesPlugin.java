5430c5bd70448f0b719ce89a286852cf6ea0123a
ulrichk <ulrichk@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2005-09-20 09:44:41 +0000

    *** empty log message ***
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@2263 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- projects/wolips/plugins/org.objectstyle.wolips.workbenchutilities/java/org/objectstyle/wolips/workbenchutilities/WorkbenchUtilitiesPlugin.java
+++ projects/wolips/plugins/org.objectstyle.wolips.workbenchutilities/java/org/objectstyle/wolips/workbenchutilities/WorkbenchUtilitiesPlugin.java
@@ -2,7 +2,7 @@
  *
  * The ObjectStyle Group Software License, Version 1.0
  *
- * Copyright (c) 2004 The ObjectStyle Group
+ * Copyright (c) 2004 - 2005 The ObjectStyle Group
  * and individual authors of the software.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
