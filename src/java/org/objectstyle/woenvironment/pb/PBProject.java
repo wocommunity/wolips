5494cd1d215b94708e032d9ec4166547ec0cb091
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2005-09-28 02:41:05 +0000

    sort PB.project file entries
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk/woproject/projects/woenvironment@2291 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- src/java/org/objectstyle/woenvironment/pb/PBProject.java
+++ src/java/org/objectstyle/woenvironment/pb/PBProject.java
@@ -60,8 +60,12 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.objectstyle.cayenne.wocompat.PropertyListSerialization;
 
@@ -74,308 +78,334 @@ import org.objectstyle.cayenne.wocompat.PropertyListSerialization;
  * @author Andrei Adamchik
  */
 public class PBProject {
-	public static final String DEFAULT_APP_PROJECT = "pbindex/woapp/PB.project";
+  public static final String DEFAULT_APP_PROJECT = "pbindex/woapp/PB.project";
 
-	public static final String DEFAULT_FRAMEWORK_PROJECT = "pbindex/woframework/PB.project";
+  public static final String DEFAULT_FRAMEWORK_PROJECT = "pbindex/woframework/PB.project";
 
-	public static final String DYNAMIC_CODE_GEN = "DYNAMIC_CODE_GEN";
+  public static final String DYNAMIC_CODE_GEN = "DYNAMIC_CODE_GEN";
 
-	public static final String FILESTABLE = "FILESTABLE";
+  public static final String FILESTABLE = "FILESTABLE";
 
-	public static final String CLASSES = "CLASSES";
+  public static final String CLASSES = "CLASSES";
 
-	public static final String FRAMEWORKS = "FRAMEWORKS";
+  public static final String FRAMEWORKS = "FRAMEWORKS";
 
-	public static final String FRAMEWORKSEARCH = "FRAMEWORKSEARCH";
+  public static final String FRAMEWORKSEARCH = "FRAMEWORKSEARCH";
 
-	public static final String OTHER_LINKED = "OTHER_LINKED";
+  public static final String OTHER_LINKED = "OTHER_LINKED";
 
-	public static final String OTHER_SOURCES = "OTHER_SOURCES";
+  public static final String OTHER_SOURCES = "OTHER_SOURCES";
 
-	public static final String WOAPP_RESOURCES = "WOAPP_RESOURCES";
+  public static final String WOAPP_RESOURCES = "WOAPP_RESOURCES";
 
-	public static final String WOCOMPONENTS = "WO_COMPONENTS";
+  public static final String WOCOMPONENTS = "WO_COMPONENTS";
 
-	public static final String WEB_SERVER_RESOURCES = "WEBSERVER_RESOURCES";
+  public static final String WEB_SERVER_RESOURCES = "WEBSERVER_RESOURCES";
 
-	public static final String PROJECTNAME = "PROJECTNAME";
+  public static final String PROJECTNAME = "PROJECTNAME";
 
-	public static final String PROJECTTYPE = "PROJECTTYPE";
+  public static final String PROJECTTYPE = "PROJECTTYPE";
 
-	public static final String PROJECTVERSION = "PROJECTVERSION";
+  public static final String PROJECTVERSION = "PROJECTVERSION";
 
-	public static final String SUBPROJECTS = "SUBPROJECTS";
+  public static final String SUBPROJECTS = "SUBPROJECTS";
 
-	public static final String YES = "YES";
-
-	public static final String NO = "NO";
-
-	protected boolean isFramework;
-
-	protected File projectFile;
-
-	protected Map pbProject;
-
-	protected Map filesTable;
-
-	private boolean hasUnsavedChanges = false;
-
-	/**
-	 * Creates a new PBProject object with an associated project file assumed to
-	 * be called "PB.project" and located in the current directory. If file does
-	 * not exist, PBProject object is initialized using default template.
-	 */
-	public PBProject(boolean isFramework) throws IOException {
-		this(new File("PB.project"), isFramework);
-	}
-
-	/**
-	 * Creates a new PBProject object with an associated project file. If file
-	 * does not exist, PBProject object is initialized using default template.
-	 */
-	public PBProject(File projectFile, boolean isFramework) throws IOException {
-		this.projectFile = projectFile;
-		this.isFramework = isFramework;
-
-		if (projectFile == null) {
-			throw new NullPointerException("Project file is null.");
-		}
-
-		this.update();
-	}
-
-	public String getDefaultTemplate() {
-		return (isFramework) ? DEFAULT_FRAMEWORK_PROJECT : DEFAULT_APP_PROJECT;
-	}
-
-	/**
-	 * Updates itself from the underlying <code>PB.project</code> file. If the
-	 * file does not exist, uses a default template to load a skeleton project.
-	 */
-	public void update() throws IOException {
-		if (projectFile == null || !projectFile.exists()) {
-			InputStream in = this.getClass().getClassLoader()
-					.getResourceAsStream(getDefaultTemplate());
-			pbProject = (Map) PropertyListSerialization
-					.propertyListFromStream(in);
-			in.close();
-			in = null;
-		} else {
-			pbProject = (Map) PropertyListSerialization
-					.propertyListFromFile(projectFile);
-		}
-
-		if (pbProject == null) {
-			throw new IOException("Error reading project file: " + projectFile);
-		}
-
-		readFilesTable();
-		hasUnsavedChanges = false;
-	}
-
-	/**
-	 * Stores changes made to this object in the underlying PB.project file.
-	 */
-	public void saveChanges() {
-		this.saveFilesTable();
-		PropertyListSerialization.propertyListToFile(projectFile, pbProject);
-	}
-
-	public boolean isDynamicCodeGen() {
-		return PBProject.YES.equals(pbProject.get(PBProject.DYNAMIC_CODE_GEN));
-	}
-
-	public void setDynamicCodeGen(boolean aBoolean) {
-		String flag = (aBoolean) ? PBProject.YES : PBProject.NO;
-		pbProject.put(PBProject.DYNAMIC_CODE_GEN, flag);
-	}
-
-	public List getClasses() {
-		return (List) getFilesTable().get(PBProject.CLASSES);
-	}
-
-	public void setClasses(List anArray) {
-		getFilesTable().put(PBProject.CLASSES, anArray);
-	}
-
-	public List getWebServerResources() {
-		return (List) getFilesTable().get(PBProject.WEB_SERVER_RESOURCES);
-	}
-
-	public void setWebServerResources(List anArray) {
-		getFilesTable().put(PBProject.WEB_SERVER_RESOURCES, anArray);
-	}
-
-	public List getWebServerResources(String language) {
-		if (language == null) {
-			return getWebServerResources();
-		}
-		return (List) getFilesTable().get(
-				language + "_" + PBProject.WEB_SERVER_RESOURCES);
-	}
-
-	public void setWebServerResources(List anArray, String language) {
-		if (language == null) {
-			setWebServerResources(anArray);
-			return;
-		}
-		getFilesTable().put(language + "_" + PBProject.WEB_SERVER_RESOURCES,
-				anArray);
-	}
-
-	public List getFrameworkSearch() {
-		return (List) pbProject.get(PBProject.FRAMEWORKSEARCH);
-	}
-
-	public void setFrameworkSearch(List anArray) {
-		pbProject.put(PBProject.FRAMEWORKSEARCH, anArray);
-	}
-
-	public List getFrameworks() {
-		return (List) getFilesTable().get(PBProject.FRAMEWORKS);
-	}
-
-	public void setFrameworks(List anArray) {
-		getFilesTable().put(PBProject.FRAMEWORKS, anArray);
-	}
-
-	public List getSubprojects() {
-		return (List) getFilesTable().get(PBProject.SUBPROJECTS);
-	}
-
-	public void setSubprojects(List anArray) {
-		getFilesTable().put(PBProject.SUBPROJECTS, anArray);
-	}
-
-	public List getOtherLinked() {
-		return (List) getFilesTable().get(PBProject.OTHER_LINKED);
-	}
-
-	public void setOtherLinked(List anArray) {
-		getFilesTable().put(PBProject.OTHER_LINKED, anArray);
-	}
-
-	public List getOtherSources() {
-		return (List) getFilesTable().get(PBProject.OTHER_SOURCES);
-	}
-
-	public void setOtherSources(List anArray) {
-		getFilesTable().put(PBProject.OTHER_SOURCES, anArray);
-	}
-
-	public List getWoAppResources() {
-		return (List) getFilesTable().get(PBProject.WOAPP_RESOURCES);
-	}
-
-	public void setWoAppResources(List anArray) {
-		getFilesTable().put(PBProject.WOAPP_RESOURCES, anArray);
-	}
-
-	public List getWoAppResources(String language) {
-		if (language == null) {
-			return getWoAppResources();
-		}
-		return (List) getFilesTable().get(
-				language + "_" + PBProject.WOAPP_RESOURCES);
-	}
-
-	public void setWoAppResources(List anArray, String language) {
-		if (language == null) {
-			setWoAppResources(anArray);
-			return;
-		}
-		getFilesTable()
-				.put(language + "_" + PBProject.WOAPP_RESOURCES, anArray);
-	}
-
-	public List getWoComponents() {
-		return (List) getFilesTable().get(PBProject.WOCOMPONENTS);
-	}
-
-	public void setWoComponents(List anArray) {
-		getFilesTable().put(PBProject.WOCOMPONENTS, anArray);
-	}
-
-	public List getWoComponents(String language) {
-		if (language == null) {
-			return getWoComponents();
-		}
-		return (List) getFilesTable().get(
-				language + "_" + PBProject.WOCOMPONENTS);
-	}
-
-	public void setWoComponents(List anArray, String language) {
-		if (language == null) {
-			setWoComponents(anArray);
-			return;
-		}
-		getFilesTable().put(language + "_" + PBProject.WOCOMPONENTS, anArray);
-	}
-
-	public String getProjectName() {
-		return (String) pbProject.get(PBProject.PROJECTNAME);
-	}
-
-	public void setProjectName(String aString) {
-		pbProject.put(PBProject.PROJECTNAME, aString);
-	}
-
-	public String getProjectType() {
-		return (String) pbProject.get(PBProject.PROJECTTYPE);
-	}
-
-	public void setProjectType(String aString) {
-		pbProject.put(PBProject.PROJECTTYPE, aString);
-	}
-
-	public String getProjectVersion() {
-		return (String) pbProject.get(PBProject.PROJECTVERSION);
-	}
-
-	public void setProjectVersion(String aString) {
-		pbProject.put(PBProject.PROJECTVERSION, aString);
-	}
-
-	protected Map getFilesTable() {
-		return filesTable;
-	}
-
-	protected void saveFilesTable() {
-		pbProject.put(PBProject.FILESTABLE, filesTable);
-	}
-
-	protected void readFilesTable() {
-		filesTable = (Map) pbProject.get(PBProject.FILESTABLE);
-	}
-
-	/**
-	 * Returns the projectFile.
-	 * 
-	 * @return File
-	 */
-	public File getProjectFile() {
-		return projectFile;
-	}
-
-	/**
-	 * Sets the projectFile.
-	 * 
-	 * @param projectFile
-	 *            The projectFile to set
-	 */
-	public void setProjectFile(File projectFile) {
-		this.projectFile = projectFile;
-	}
-
-	/**
-	 * Method forgetAll deletes all classes, other linked, other sources,
-	 * webserver resources and wocomponent entries.
-	 */
-	public void forgetAllFiles() {
-		this.setClasses(new ArrayList());
-		this.setOtherLinked(new ArrayList());
-		this.setOtherSources(new ArrayList());
-		this.setWebServerResources(new ArrayList());
-		this.setWoComponents(new ArrayList());
-	}
+  public static final String YES = "YES";
+
+  public static final String NO = "NO";
+
+  protected boolean isFramework;
+
+  protected File projectFile;
+
+  protected Map pbProject;
+
+  protected Map filesTable;
+
+  private boolean hasUnsavedChanges = false;
+
+  /**
+   * Creates a new PBProject object with an associated project file assumed to
+   * be called "PB.project" and located in the current directory. If file does
+   * not exist, PBProject object is initialized using default template.
+   */
+  public PBProject(boolean isFramework) throws IOException {
+    this(new File("PB.project"), isFramework);
+  }
+
+  /**
+   * Creates a new PBProject object with an associated project file. If file
+   * does not exist, PBProject object is initialized using default template.
+   */
+  public PBProject(File projectFile, boolean isFramework) throws IOException {
+    this.projectFile = projectFile;
+    this.isFramework = isFramework;
+
+    if (projectFile == null) {
+      throw new NullPointerException("Project file is null.");
+    }
+
+    this.update();
+  }
+
+  public String getDefaultTemplate() {
+    return (isFramework) ? DEFAULT_FRAMEWORK_PROJECT : DEFAULT_APP_PROJECT;
+  }
+
+  /**
+   * Updates itself from the underlying <code>PB.project</code> file. If the
+   * file does not exist, uses a default template to load a skeleton project.
+   */
+  public void update() throws IOException {
+    if (projectFile == null || !projectFile.exists()) {
+      InputStream in = this.getClass().getClassLoader().getResourceAsStream(getDefaultTemplate());
+      pbProject = (Map) PropertyListSerialization.propertyListFromStream(in);
+      in.close();
+      in = null;
+    }
+    else {
+      pbProject = (Map) PropertyListSerialization.propertyListFromFile(projectFile);
+    }
+
+    if (pbProject == null) {
+      throw new IOException("Error reading project file: " + projectFile);
+    }
+
+    readFilesTable();
+    hasUnsavedChanges = false;
+  }
+
+  /**
+   * Stores changes made to this object in the underlying PB.project file.
+   */
+  public void saveChanges() {
+    this.saveFilesTable();
+    Map sortedMap = sortedMap(pbProject);
+    PropertyListSerialization.propertyListToFile(projectFile, sortedMap);
+  }
+
+  private Map sortedMap(Map _map) {
+    TreeMap sortedMap = new TreeMap(_map);
+    Iterator entriesIter = sortedMap.entrySet().iterator();
+    while (entriesIter.hasNext()) {
+      Map.Entry entry = (Map.Entry) entriesIter.next();
+      Object value = entry.getValue();
+      if (value instanceof List) {
+        entry.setValue(sortedList((List) value));
+      }
+      else if (value instanceof Map) {
+        entry.setValue(sortedMap((Map) value));
+      }
+    }
+    return sortedMap;
+  }
+
+  private List sortedList(List _list) {
+    LinkedList sortedList = new LinkedList(_list);
+    int size = sortedList.size();
+    for (int i = 0; i < size; i++) {
+      Object entry = sortedList.get(i);
+      if (entry instanceof List) {
+        sortedList.set(i, sortedList((List) entry));
+      }
+      else if (entry instanceof Map) {
+        sortedList.set(i, sortedMap((Map) entry));
+      }
+    }
+    Collections.sort(sortedList);
+    return sortedList;
+  }
+
+  public boolean isDynamicCodeGen() {
+    return PBProject.YES.equals(pbProject.get(PBProject.DYNAMIC_CODE_GEN));
+  }
+
+  public void setDynamicCodeGen(boolean aBoolean) {
+    String flag = (aBoolean) ? PBProject.YES : PBProject.NO;
+    pbProject.put(PBProject.DYNAMIC_CODE_GEN, flag);
+  }
+
+  public List getClasses() {
+    return (List) getFilesTable().get(PBProject.CLASSES);
+  }
+
+  public void setClasses(List anArray) {
+    getFilesTable().put(PBProject.CLASSES, anArray);
+  }
+
+  public List getWebServerResources() {
+    return (List) getFilesTable().get(PBProject.WEB_SERVER_RESOURCES);
+  }
+
+  public void setWebServerResources(List anArray) {
+    getFilesTable().put(PBProject.WEB_SERVER_RESOURCES, anArray);
+  }
+
+  public List getWebServerResources(String language) {
+    if (language == null) {
+      return getWebServerResources();
+    }
+    return (List) getFilesTable().get(language + "_" + PBProject.WEB_SERVER_RESOURCES);
+  }
+
+  public void setWebServerResources(List anArray, String language) {
+    if (language == null) {
+      setWebServerResources(anArray);
+      return;
+    }
+    getFilesTable().put(language + "_" + PBProject.WEB_SERVER_RESOURCES, anArray);
+  }
+
+  public List getFrameworkSearch() {
+    return (List) pbProject.get(PBProject.FRAMEWORKSEARCH);
+  }
+
+  public void setFrameworkSearch(List anArray) {
+    pbProject.put(PBProject.FRAMEWORKSEARCH, anArray);
+  }
+
+  public List getFrameworks() {
+    return (List) getFilesTable().get(PBProject.FRAMEWORKS);
+  }
+
+  public void setFrameworks(List anArray) {
+    getFilesTable().put(PBProject.FRAMEWORKS, anArray);
+  }
+
+  public List getSubprojects() {
+    return (List) getFilesTable().get(PBProject.SUBPROJECTS);
+  }
+
+  public void setSubprojects(List anArray) {
+    getFilesTable().put(PBProject.SUBPROJECTS, anArray);
+  }
+
+  public List getOtherLinked() {
+    return (List) getFilesTable().get(PBProject.OTHER_LINKED);
+  }
+
+  public void setOtherLinked(List anArray) {
+    getFilesTable().put(PBProject.OTHER_LINKED, anArray);
+  }
+
+  public List getOtherSources() {
+    return (List) getFilesTable().get(PBProject.OTHER_SOURCES);
+  }
+
+  public void setOtherSources(List anArray) {
+    getFilesTable().put(PBProject.OTHER_SOURCES, anArray);
+  }
+
+  public List getWoAppResources() {
+    return (List) getFilesTable().get(PBProject.WOAPP_RESOURCES);
+  }
+
+  public void setWoAppResources(List anArray) {
+    getFilesTable().put(PBProject.WOAPP_RESOURCES, anArray);
+  }
+
+  public List getWoAppResources(String language) {
+    if (language == null) {
+      return getWoAppResources();
+    }
+    return (List) getFilesTable().get(language + "_" + PBProject.WOAPP_RESOURCES);
+  }
+
+  public void setWoAppResources(List anArray, String language) {
+    if (language == null) {
+      setWoAppResources(anArray);
+      return;
+    }
+    getFilesTable().put(language + "_" + PBProject.WOAPP_RESOURCES, anArray);
+  }
+
+  public List getWoComponents() {
+    return (List) getFilesTable().get(PBProject.WOCOMPONENTS);
+  }
+
+  public void setWoComponents(List anArray) {
+    getFilesTable().put(PBProject.WOCOMPONENTS, anArray);
+  }
+
+  public List getWoComponents(String language) {
+    if (language == null) {
+      return getWoComponents();
+    }
+    return (List) getFilesTable().get(language + "_" + PBProject.WOCOMPONENTS);
+  }
+
+  public void setWoComponents(List anArray, String language) {
+    if (language == null) {
+      setWoComponents(anArray);
+      return;
+    }
+    getFilesTable().put(language + "_" + PBProject.WOCOMPONENTS, anArray);
+  }
+
+  public String getProjectName() {
+    return (String) pbProject.get(PBProject.PROJECTNAME);
+  }
+
+  public void setProjectName(String aString) {
+    pbProject.put(PBProject.PROJECTNAME, aString);
+  }
+
+  public String getProjectType() {
+    return (String) pbProject.get(PBProject.PROJECTTYPE);
+  }
+
+  public void setProjectType(String aString) {
+    pbProject.put(PBProject.PROJECTTYPE, aString);
+  }
+
+  public String getProjectVersion() {
+    return (String) pbProject.get(PBProject.PROJECTVERSION);
+  }
+
+  public void setProjectVersion(String aString) {
+    pbProject.put(PBProject.PROJECTVERSION, aString);
+  }
+
+  protected Map getFilesTable() {
+    return filesTable;
+  }
+
+  protected void saveFilesTable() {
+    pbProject.put(PBProject.FILESTABLE, filesTable);
+  }
+
+  protected void readFilesTable() {
+    filesTable = (Map) pbProject.get(PBProject.FILESTABLE);
+  }
+
+  /**
+   * Returns the projectFile.
+   * 
+   * @return File
+   */
+  public File getProjectFile() {
+    return projectFile;
+  }
+
+  /**
+   * Sets the projectFile.
+   * 
+   * @param projectFile
+   *            The projectFile to set
+   */
+  public void setProjectFile(File projectFile) {
+    this.projectFile = projectFile;
+  }
+
+  /**
+   * Method forgetAll deletes all classes, other linked, other sources,
+   * webserver resources and wocomponent entries.
+   */
+  public void forgetAllFiles() {
+    this.setClasses(new ArrayList());
+    this.setOtherLinked(new ArrayList());
+    this.setOtherSources(new ArrayList());
+    this.setWebServerResources(new ArrayList());
+    this.setWoComponents(new ArrayList());
+  }
 }
