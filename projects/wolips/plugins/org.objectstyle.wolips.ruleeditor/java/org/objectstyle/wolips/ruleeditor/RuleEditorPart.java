042fe636431e875fd6592e9b0eb219e16dcf5a17
ulrichk <ulrichk@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2005-11-13 08:16:48 +0000

    new ruleditor plugin
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@2440 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ projects/wolips/plugins/org.objectstyle.wolips.ruleeditor/java/org/objectstyle/wolips/ruleeditor/RuleEditorPart.java
@@ -0,0 +1,136 @@
+/*
+ * ====================================================================
+ * 
+ * The ObjectStyle Group Software License, Version 1.0
+ * 
+ * Copyright (c) 2005 The ObjectStyle Group and individual authors of the
+ * software. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if any, must
+ * include the following acknowlegement: "This product includes software
+ * developed by the ObjectStyle Group (http://objectstyle.org/)." Alternately,
+ * this acknowlegement may appear in the software itself, if and wherever such
+ * third-party acknowlegements normally appear.
+ * 
+ * 4. The names "ObjectStyle Group" and "Cayenne" must not be used to endorse or
+ * promote products derived from this software without prior written permission.
+ * For written permission, please contact andrus@objectstyle.org.
+ * 
+ * 5. Products derived from this software may not be called "ObjectStyle" nor
+ * may "ObjectStyle" appear in their names without prior written permission of
+ * the ObjectStyle Group.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * OBJECTSTYLE GROUP OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ * 
+ * This software consists of voluntary contributions made by many individuals on
+ * behalf of the ObjectStyle Group. For more information on the ObjectStyle
+ * Group, please see <http://objectstyle.org/>.
+ *  
+ */
+package org.objectstyle.wolips.ruleeditor;
+
+import java.io.IOException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.part.EditorPart;
+import org.objectstyle.wolips.ruleeditor.model.D2WModel;
+
+/**
+ * @author omar
+ * @author uli
+ */
+public class RuleEditorPart extends EditorPart {
+
+	private IFileEditorInput fileEditorInput;
+
+	private RuleEditor ruleEditor;
+
+	private boolean isDirty;
+
+	private D2WModel model;
+
+	public RuleEditorPart() {
+		super();
+	}
+
+	public void doSave(IProgressMonitor monitor) {
+		model.saveChanges();
+	}
+
+	public void doSaveAs() {
+		throw new IllegalStateException();
+	}
+
+	public void init(IEditorSite site, IEditorInput input)
+			throws PartInitException {
+		fileEditorInput = (IFileEditorInput) input;
+		super.setSite(site);
+		super.setInput(input);
+	}
+
+	public boolean isDirty() {
+		return isDirty;
+	}
+
+	public boolean isSaveAsAllowed() {
+		return false;
+	}
+
+	public void createPartControl(Composite parent) {
+		ruleEditor = new RuleEditor();
+		String fileName = fileEditorInput.getFile().getLocation().toOSString();
+		model = new D2WModel() {
+
+			public void setHasUnsavedChanges(boolean hasUnsavedChanges) {
+				super.setHasUnsavedChanges(hasUnsavedChanges);
+				RuleEditorPart.this.setDirty(hasUnsavedChanges);
+			}
+		};
+		try {
+			model.init(fileName);
+		} catch (IOException e) {
+			RuleEditorPlugin.getDefault().log("Error while saving model", e);
+		}
+		ruleEditor.setD2WModel(model);
+		ruleEditor.createContents(parent);
+	}
+
+	public void setFocus() {
+		ruleEditor.setFocus();
+	}
+
+	/**
+	 * @param isDirty
+	 *            The isDirty to set.
+	 */
+	public void setDirty(boolean isDirty) {
+		this.isDirty = isDirty;
+		this.firePropertyChange(IEditorPart.PROP_DIRTY);
+	}
+}
