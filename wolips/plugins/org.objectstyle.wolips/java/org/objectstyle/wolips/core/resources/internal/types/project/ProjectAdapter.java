36efdc6c5ad331cedd6260ebb8323ff22a154dc2
uli <uli@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2006-05-13 12:46:27 +0000

    refactoring: removed projectbuild plugin
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@2622 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/plugins/org.objectstyle.wolips/java/org/objectstyle/wolips/core/resources/internal/types/project/ProjectAdapter.java
+++ wolips/plugins/org.objectstyle.wolips/java/org/objectstyle/wolips/core/resources/internal/types/project/ProjectAdapter.java
@@ -55,19 +55,33 @@
  */
 package org.objectstyle.wolips.core.resources.internal.types.project;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.eclipse.core.resources.ICommand;
 import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
@@ -87,6 +101,17 @@ public class ProjectAdapter extends AbstractResourceAdapter implements
 	private IProject underlyingProject;
 
 	private boolean isFramework;
+
+	/**
+	 * Comment for <code>BuilderNotFound</code>
+	 */
+	public static final int BuilderNotFound = -1;
+
+	protected static final String TARGET_BUILDER_ID = "org.objectstyle.wolips.targetbuilder.targetbuilder";
+
+	protected static final String INCREMENTAL_BUILDER_ID = "org.objectstyle.wolips.incrementalbuilder";
+
+	private static final String ANT_BUILDER_ID = "org.objectstyle.wolips.antbuilder";
 
 	public ProjectAdapter(IProject project, boolean isFramework) {
 		super(project);
@@ -298,5 +323,624 @@ public class ProjectAdapter extends AbstractResourceAdapter implements
 				return true;
 		}
 		return false;
+	}
+
+	/**
+	 * Installs the target builder.
+	 * 
+	 * @param position
+	 * @throws CoreException
+	 */
+	public void installTargetBuilder(int position) throws CoreException {
+		if (!this.isTargetBuilderInstalled())
+			this.installBuilderAtPosition(ProjectAdapter.TARGET_BUILDER_ID,
+					position, null);
+	}
+
+	/**
+	 * Removes the target builder.
+	 * 
+	 * @return postion of TargetBuilder if not found
+	 *         IBuilderAccessor.BuilderNotFoundwill be returned.
+	 * @throws CoreException
+	 */
+	public int removeTargetBuilder() throws CoreException {
+		if (!this.isTargetBuilderInstalled())
+			return ProjectAdapter.BuilderNotFound;
+		int returnValue = this
+				.positionForBuilder(ProjectAdapter.TARGET_BUILDER_ID);
+		this.removeBuilder(ProjectAdapter.TARGET_BUILDER_ID);
+		return returnValue;
+	}
+
+	/**
+	 * Installs the ant builder.
+	 * 
+	 * @throws CoreException
+	 */
+	public void installAntBuilder() throws CoreException {
+		if (!this.isAntBuilderInstalled())
+			this.installBuilder(ProjectAdapter.ANT_BUILDER_ID);
+	}
+
+	/**
+	 * Removes the ant builder.
+	 * 
+	 * @throws CoreException
+	 */
+	public void removeAntBuilder() throws CoreException {
+		if (this.isAntBuilderInstalled())
+			this.removeBuilder(ProjectAdapter.ANT_BUILDER_ID);
+	}
+
+	/**
+	 * Installs the incremetal builder.
+	 * 
+	 * @throws CoreException
+	 */
+	public void installIncrementalBuilder() throws CoreException {
+		if (!this.isIncrementalBuilderInstalled())
+			this.installBuilder(ProjectAdapter.INCREMENTAL_BUILDER_ID);
+	}
+
+	/**
+	 * Removes the incremental builder.
+	 * 
+	 * @throws CoreException
+	 */
+	public void removeIncrementalBuilder() throws CoreException {
+		if (this.isIncrementalBuilderInstalled())
+			this.removeBuilder(ProjectAdapter.INCREMENTAL_BUILDER_ID);
+	}
+
+	/**
+	 * Installs the java builder.
+	 * 
+	 * @throws CoreException
+	 */
+	public void installJavaBuilder() throws CoreException {
+		if (!this.isJavaBuilderInstalled())
+			this.installBuilder(JavaCore.BUILDER_ID);
+	}
+
+	/**
+	 * Installs the java builder.
+	 * 
+	 * @param position
+	 * @throws CoreException
+	 */
+	public void installJavaBuilder(int position) throws CoreException {
+		if (!this.isJavaBuilderInstalled())
+			this.installBuilderAtPosition(JavaCore.BUILDER_ID, position, null);
+	}
+
+	/**
+	 * Removes the incremental builder.
+	 * 
+	 * @return postion of JavaBuilder if not found
+	 *         IBuilderAccessor.BuilderNotFoundwill be returned.
+	 * @throws CoreException
+	 */
+	public int removeJavaBuilder() throws CoreException {
+		if (!this.isJavaBuilderInstalled())
+			return ProjectAdapter.BuilderNotFound;
+		int returnValue = this.positionForBuilder(JavaCore.BUILDER_ID);
+		this.removeBuilder(JavaCore.BUILDER_ID);
+		return returnValue;
+	}
+
+	/**
+	 * @return Return true if the target builder is installed.
+	 */
+	public boolean isTargetBuilderInstalled() {
+		return this.isBuilderInstalled(ProjectAdapter.TARGET_BUILDER_ID);
+	}
+
+	/**
+	 * @return Return true if the ant builder is installed.
+	 */
+	public boolean isAntBuilderInstalled() {
+		return this.isBuilderInstalled(ProjectAdapter.ANT_BUILDER_ID);
+	}
+
+	/**
+	 * @return Return true if the incremental builder is installed.
+	 */
+	public boolean isIncrementalBuilderInstalled() {
+		return this.isBuilderInstalled(ProjectAdapter.INCREMENTAL_BUILDER_ID);
+	}
+
+	/**
+	 * @return Return true if the java builder is installed.
+	 */
+	public boolean isJavaBuilderInstalled() {
+		return this.isBuilderInstalled(JavaCore.BUILDER_ID);
+	}
+
+	/**
+	 * @return The builer args.
+	 */
+	public Map getBuilderArgs() {
+		Map result = null;
+		try {
+			IProjectDescription desc = this.getUnderlyingProject()
+					.getDescription();
+			List cmdList = Arrays.asList(desc.getBuildSpec());
+			Iterator iter = cmdList.iterator();
+			while (iter.hasNext()) {
+				ICommand cmd = (ICommand) iter.next();
+				if (this.isWOLipsBuilder(cmd.getBuilderName())) {
+					result = cmd.getArguments();
+					break;
+				}
+			}
+		} catch (Exception up) {
+			// if anything went wrong, we simply don't have any args (yet)
+			// might wanna log the exception, though
+		}
+		if (null == result) {
+			// this doesn't exist pre-JDK1.3, is that a problem?
+			result = Collections.EMPTY_MAP;
+			// result = new HashMap();
+		}
+		return (result);
+	}
+
+	/**
+	 * @param name
+	 *            Name of a build command
+	 * @return boolean whether this is one of ours
+	 */
+	private boolean isWOLipsBuilder(String name) {
+		return (name.equals(ProjectAdapter.INCREMENTAL_BUILDER_ID) || name
+				.equals(ProjectAdapter.ANT_BUILDER_ID));
+	}
+
+	/**
+	 * Method removeJavaBuilder.
+	 * 
+	 * @param aBuilder
+	 * @throws CoreException
+	 */
+	private void removeBuilder(String aBuilder) throws CoreException {
+		IProjectDescription desc = null;
+		ICommand[] coms = null;
+		ArrayList comList = null;
+		List tmp = null;
+		ICommand[] newCom = null;
+		try {
+			desc = this.getUnderlyingProject().getDescription();
+			coms = desc.getBuildSpec();
+			comList = new ArrayList();
+			tmp = Arrays.asList(coms);
+			comList.addAll(tmp);
+			boolean foundJBuilder = false;
+			for (int i = 0; i < comList.size(); i++) {
+				if (((ICommand) comList.get(i)).getBuilderName().equals(
+						aBuilder)) {
+					comList.remove(i);
+					foundJBuilder = true;
+				}
+			}
+			if (foundJBuilder) {
+				newCom = new ICommand[comList.size()];
+				for (int i = 0; i < comList.size(); i++) {
+					newCom[i] = (ICommand) comList.get(i);
+				}
+				desc.setBuildSpec(newCom);
+				this.getUnderlyingProject().setDescription(desc, null);
+			}
+		} finally {
+			desc = null;
+			coms = null;
+			comList = null;
+			tmp = null;
+			newCom = null;
+		}
+	}
+
+	/**
+	 * Method installBuilder.
+	 * 
+	 * @param aBuilder
+	 * @throws CoreException
+	 */
+	private void installBuilder(String aBuilder) throws CoreException {
+		IProjectDescription desc = null;
+		ICommand[] coms = null;
+		ICommand[] newIc = null;
+		ICommand command = null;
+		try {
+			desc = this.getUnderlyingProject().getDescription();
+			coms = desc.getBuildSpec();
+			boolean foundJBuilder = false;
+			for (int i = 0; i < coms.length; i++) {
+				if (coms[i].getBuilderName().equals(aBuilder)) {
+					foundJBuilder = true;
+				}
+			}
+			if (!foundJBuilder) {
+				newIc = null;
+				command = desc.newCommand();
+				command.setBuilderName(aBuilder);
+				newIc = new ICommand[coms.length + 1];
+				System.arraycopy(coms, 0, newIc, 0, coms.length);
+				newIc[coms.length] = command;
+				desc.setBuildSpec(newIc);
+				this.getUnderlyingProject().setDescription(desc, null);
+			}
+		} finally {
+			desc = null;
+			coms = null;
+			newIc = null;
+			command = null;
+		}
+	}
+
+	/**
+	 * Method isBuilderInstalled.
+	 * 
+	 * @param anID
+	 * @return boolean
+	 */
+	private boolean isBuilderInstalled(String anID) {
+		try {
+			ICommand[] nids = this.getUnderlyingProject().getDescription()
+					.getBuildSpec();
+			for (int i = 0; i < nids.length; i++) {
+				if (nids[i].getBuilderName().equals(anID))
+					return true;
+			}
+		} catch (Exception anException) {
+			CorePlugin.getDefault().log(anException);
+			return false;
+		}
+		return false;
+	}
+
+	/**
+	 * Method positionForBuilder.
+	 * 
+	 * @param aBuilder
+	 * @return int
+	 * @throws CoreException
+	 */
+	private int positionForBuilder(String aBuilder) throws CoreException {
+		IProjectDescription desc = null;
+		ICommand[] coms = null;
+		try {
+			desc = this.getUnderlyingProject().getDescription();
+			coms = desc.getBuildSpec();
+			for (int i = 0; i < coms.length; i++) {
+				if (coms[i].getBuilderName().equals(aBuilder))
+					return i;
+			}
+		} finally {
+			desc = null;
+			coms = null;
+		}
+		return BuilderNotFound;
+	}
+
+	/**
+	 * Method installBuilderAtPosition.
+	 * 
+	 * @param aBuilder
+	 * @param installPos
+	 * @param arguments
+	 * @throws CoreException
+	 */
+	private void installBuilderAtPosition(String aBuilder, int installPos,
+			Map arguments) throws CoreException {
+		if (installPos == ProjectAdapter.BuilderNotFound) {
+			CorePlugin
+					.getDefault()
+
+					.log(
+							"Somebody tries to install builder: "
+									+ aBuilder
+									+ " at an illegal position. This may happen if the removed builder does not exist.");
+			return;
+		}
+		IProjectDescription desc = this.getUnderlyingProject().getDescription();
+		ICommand[] coms = desc.getBuildSpec();
+		if (arguments == null)
+			arguments = new HashMap();
+		for (int i = 0; i < coms.length; i++) {
+			if (coms[i].getBuilderName().equals(aBuilder)
+					&& coms[i].getArguments().equals(arguments))
+				return;
+		}
+		ICommand[] newIc = null;
+		ICommand command = desc.newCommand();
+		command.setBuilderName(aBuilder);
+		command.setArguments(arguments);
+		newIc = new ICommand[coms.length + 1];
+		if (installPos <= 0) {
+			System.arraycopy(coms, 0, newIc, 1, coms.length);
+			newIc[0] = command;
+		} else if (installPos >= coms.length) {
+			System.arraycopy(coms, 0, newIc, 0, coms.length);
+			newIc[coms.length] = command;
+		} else {
+			System.arraycopy(coms, 0, newIc, 0, installPos);
+			newIc[installPos] = command;
+			System.arraycopy(coms, installPos, newIc, installPos + 1,
+					coms.length - installPos);
+		}
+		desc.setBuildSpec(newIc);
+		this.getUnderlyingProject().setDescription(desc, null);
+	}
+
+	/**
+	 * @return null if the project is not an application othewise invokes the
+	 *         same method on ProjectAdapter
+	 */
+	public IPath getWorkingDir() {
+		IPath path = null;
+		if (this.isAntBuilderInstalled()) {
+			path = this.getUnderlyingProject().getFolder("dist").getLocation();
+		} else {
+			path = this.getUnderlyingProject().getFolder("build").getLocation();
+		}
+		path = path.append(this.getUnderlyingProject().getName() + ".woa");
+		return path;
+	}
+
+	private Properties getBuildProperties() throws CoreException, IOException {
+		Properties properties = new Properties();
+		IFile file = this.getUnderlyingProject().getFile("build.properties");
+		if (file.exists()) {
+			InputStream inputStream = file.getContents();
+			properties.load(inputStream);
+			inputStream.close();
+		}
+		return properties;
+	}
+
+	private void setBuildProperties(Properties properties)
+			throws CoreException, IOException {
+		if (this.getBuildProperties().equals(properties))
+			return;
+		IFile file = this.getUnderlyingProject().getFile("build.properties");
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		properties.store(byteArrayOutputStream, null);
+		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(
+				byteArrayOutputStream.toByteArray());
+		file.setContents(byteArrayInputStream, true, true,
+				new NullProgressMonitor());
+	}
+
+	/**
+	 * @return generate webxml
+	 */
+	public boolean getWebXML() {
+		String returnValue = null;
+		try {
+			returnValue = (String) this.getBuildProperties().get("webXML");
+		} catch (CoreException e) {
+			CorePlugin.getDefault().log(e);
+		} catch (IOException e) {
+			CorePlugin.getDefault().log(e);
+		}
+		if (returnValue == null || !"true".equalsIgnoreCase(returnValue)) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * @param webXML
+	 *            generate webxml
+	 */
+	public void setWebXML(boolean webXML) {
+		try {
+			Properties properties = this.getBuildProperties();
+			if (!webXML) {
+				properties.put("webXML", "false");
+			} else {
+				properties.put("webXML", "true");
+			}
+			this.setBuildProperties(properties);
+		} catch (CoreException e) {
+			CorePlugin.getDefault().log(e);
+		} catch (IOException e) {
+			CorePlugin.getDefault().log(e);
+		}
+	}
+
+	/**
+	 * @param convertNullValueToEmptyString
+	 * @return webxml custom content
+	 */
+	public String getWebXML_CustomContent(boolean convertNullValueToEmptyString) {
+		String returnValue = null;
+		try {
+			returnValue = (String) this.getBuildProperties().get(
+					"webXML_CustomContent");
+		} catch (CoreException e) {
+			CorePlugin.getDefault().log(e);
+		} catch (IOException e) {
+			CorePlugin.getDefault().log(e);
+		}
+		if (convertNullValueToEmptyString && returnValue == null) {
+			return "";
+		}
+		return returnValue;
+	}
+
+	/**
+	 * @param webXML_CustomContent
+	 *            webxml custom content
+	 */
+	public void setWebXML_CustomContent(String webXML_CustomContent) {
+		try {
+			Properties properties = this.getBuildProperties();
+			if (webXML_CustomContent == null) {
+				properties.put("webXML_CustomContent", "");
+			} else {
+				properties.put("webXML_CustomContent", webXML_CustomContent);
+			}
+			this.setBuildProperties(properties);
+		} catch (CoreException e) {
+			CorePlugin.getDefault().log(e);
+		} catch (IOException e) {
+			CorePlugin.getDefault().log(e);
+		}
+	}
+
+	public String getEOGeneratorArgs(boolean convertNullValueToEmptyString) {
+		String returnValue = null;
+		try {
+			returnValue = (String) this.getBuildProperties().get(
+					"eogeneratorArgs");
+		} catch (CoreException e) {
+			CorePlugin.getDefault().log(e);
+		} catch (IOException e) {
+			CorePlugin.getDefault().log(e);
+		}
+		if (convertNullValueToEmptyString && returnValue == null) {
+			return "";
+		}
+		return returnValue;
+	}
+
+	public void setEOGeneratorArgs(String eogeneratorArgs) {
+		try {
+			Properties properties = this.getBuildProperties();
+			if (eogeneratorArgs == null) {
+				properties.put("eogeneratorArgs", "");
+			} else {
+				properties.put("eogeneratorArgs", eogeneratorArgs);
+			}
+			this.setBuildProperties(properties);
+		} catch (CoreException e) {
+			CorePlugin.getDefault().log(e);
+		} catch (IOException e) {
+			CorePlugin.getDefault().log(e);
+		}
+	}
+
+	/**
+	 * @param convertNullValueToEmptyString
+	 * @return principalClass.
+	 */
+	public String getPrincipalClass(boolean convertNullValueToEmptyString) {
+		String returnValue = null;
+		try {
+			returnValue = (String) this.getBuildProperties().get(
+					"principalClass");
+		} catch (CoreException e) {
+			CorePlugin.getDefault().log(e);
+		} catch (IOException e) {
+			CorePlugin.getDefault().log(e);
+		}
+		if (convertNullValueToEmptyString && returnValue == null) {
+			return "";
+		}
+		return returnValue;
+	}
+
+	/**
+	 * @param principalClass
+	 *            the principalClass for the Info.plist
+	 */
+	public void setPrincipalClass(String principalClass) {
+		try {
+			Properties properties = this.getBuildProperties();
+			if (principalClass == null) {
+				properties.put("principalClass", "");
+			} else {
+				properties.put("principalClass", principalClass);
+			}
+			this.setBuildProperties(properties);
+		} catch (CoreException e) {
+			CorePlugin.getDefault().log(e);
+		} catch (IOException e) {
+			CorePlugin.getDefault().log(e);
+		}
+	}
+
+	/**
+	 * @param convertNullValueToEmptyString
+	 * @return The CustomContent for the Info.plist
+	 */
+	public String getCustomInfoPListContent(
+			boolean convertNullValueToEmptyString) {
+		String returnValue = null;
+		try {
+			returnValue = (String) this.getBuildProperties().get(
+					"customInfoPListContent");
+		} catch (CoreException e) {
+			CorePlugin.getDefault().log(e);
+		} catch (IOException e) {
+			CorePlugin.getDefault().log(e);
+		}
+		if (convertNullValueToEmptyString && returnValue == null) {
+			return "";
+		}
+		return returnValue;
+	}
+
+	/**
+	 * @param customInfoPListContent
+	 *            The CustomContent for the Info.plist
+	 */
+	public void setCustomInfoPListContent(String customInfoPListContent) {
+		try {
+			Properties properties = this.getBuildProperties();
+			if (customInfoPListContent == null) {
+				properties.put("customInfoPListContent", "");
+			} else {
+				properties
+						.put("customInfoPListContent", customInfoPListContent);
+			}
+			this.setBuildProperties(properties);
+		} catch (CoreException e) {
+			CorePlugin.getDefault().log(e);
+		} catch (IOException e) {
+			CorePlugin.getDefault().log(e);
+		}
+	}
+
+	/**
+	 * @param convertNullValueToEmptyString
+	 * @return The EOAdaptorClassName for the Info.plist
+	 */
+	public String getEOAdaptorClassName(boolean convertNullValueToEmptyString) {
+		String returnValue = null;
+		try {
+			returnValue = (String) this.getBuildProperties().get(
+					"eoAdaptorClassName");
+		} catch (CoreException e) {
+			CorePlugin.getDefault().log(e);
+		} catch (IOException e) {
+			CorePlugin.getDefault().log(e);
+		}
+		if (convertNullValueToEmptyString && returnValue == null) {
+			return "";
+		}
+		return returnValue;
+	}
+
+	/**
+	 * @param eoAdaptorClassName
+	 *            the eoadaptorclassname for the Info.plist
+	 */
+	public void setEOAdaptorClassName(String eoAdaptorClassName) {
+		try {
+			Properties properties = this.getBuildProperties();
+			if (eoAdaptorClassName == null) {
+				properties.put("eoAdaptorClassName", "");
+			} else {
+				properties.put("eoAdaptorClassName", eoAdaptorClassName);
+			}
+			this.setBuildProperties(properties);
+		} catch (CoreException e) {
+			CorePlugin.getDefault().log(e);
+		} catch (IOException e) {
+			CorePlugin.getDefault().log(e);
+		}
+
 	}
 }
