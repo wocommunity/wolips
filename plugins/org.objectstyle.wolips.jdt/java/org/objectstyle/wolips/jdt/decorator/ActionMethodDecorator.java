707644acbcd68e99d8d15cce831b1056526d8a66
uli <uli@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2006-05-20 07:35:12 +0000

    playing with decorators
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@2703 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ plugins/org.objectstyle.wolips.jdt/java/org/objectstyle/wolips/jdt/decorator/ActionMethodDecorator.java
@@ -0,0 +1,143 @@
+/* ====================================================================
+ * 
+ * The ObjectStyle Group Software License, Version 1.0 
+ *
+ * Copyright (c) 2006 The ObjectStyle Group 
+ * and individual authors of the software.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:  
+ *       "This product includes software developed by the 
+ *        ObjectStyle Group (http://objectstyle.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "ObjectStyle Group" and "Cayenne" 
+ *    must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written 
+ *    permission, please contact andrus@objectstyle.org.
+ *
+ * 5. Products derived from this software may not be called "ObjectStyle"
+ *    nor may "ObjectStyle" appear in their names without prior written
+ *    permission of the ObjectStyle Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE OBJECTSTYLE GROUP OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the ObjectStyle Group.  For more
+ * information on the ObjectStyle Group, please see
+ * <http://objectstyle.org/>.
+ *
+ */
+package org.objectstyle.wolips.jdt.decorator;
+
+import org.eclipse.jdt.core.Flags;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.IDecoration;
+import org.eclipse.jface.viewers.ILabelDecorator;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.ILightweightLabelDecorator;
+import org.eclipse.swt.graphics.Image;
+
+public class ActionMethodDecorator implements ILabelDecorator,
+		ILightweightLabelDecorator {
+
+	public ActionMethodDecorator() {
+		super();
+	}
+
+	public String decorateText(String text, Object element) {
+		return text;
+	}
+
+	private boolean isAction(Object element) {
+		if (!(element instanceof IMethod)) {
+			return false;
+		}
+		try {
+			IMethod method = (IMethod) element;
+			if (!method.getJavaProject().isOnClasspath(method)) {
+				return false;
+			}
+			if (method.isConstructor()) {
+				return false;
+			}
+			if (method.isMainMethod()) {
+				return false;
+			}
+			int flags = method.getFlags();
+			if (Flags.isPrivate(flags) || Flags.isStatic(flags)) {
+				return false;
+			}
+			if (method.getNumberOfParameters() > 0) {
+				return false;
+			}
+			String returnType = method.getReturnType();
+			if (returnType == null || returnType.length() == 0) {
+				return false;
+			}
+			if (returnType.equals("QWOComponent;")) {
+				return true;
+			}
+			if (returnType.equals("QWOActionResults;")) {
+				return true;
+			}
+		} catch (JavaModelException e) {
+			// shit happens
+		}
+		return false;
+	}
+
+	public Image decorateImage(Image image, Object element) {
+		return image;
+	}
+
+	public void decorate(Object element, IDecoration decoration) {
+		if (this.isAction(element)) {
+			decoration.addOverlay(ImageDescriptor.createFromFile(
+					ActionMethodDecorator.class, "action_overlay.gif"));
+		}
+	}
+
+	public boolean isLabelProperty(Object element, String property) {
+		return true;
+	}
+
+	public void removeListener(ILabelProviderListener listener) {
+		return;
+	}
+
+	public void addListener(ILabelProviderListener listener) {
+		return;
+	}
+
+	public void dispose() {
+		return;
+	}
+}
