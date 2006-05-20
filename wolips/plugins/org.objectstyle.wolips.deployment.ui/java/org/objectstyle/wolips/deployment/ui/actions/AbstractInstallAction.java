c1cc5baf9a56015574f3efa3e1b17df7fc24549b
uli <uli@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2006-05-20 07:56:40 +0000

    avoid synthetic accessor
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@2704 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/plugins/org.objectstyle.wolips.deployment.ui/java/org/objectstyle/wolips/deployment/ui/actions/AbstractInstallAction.java
+++ wolips/plugins/org.objectstyle.wolips.deployment.ui/java/org/objectstyle/wolips/deployment/ui/actions/AbstractInstallAction.java
@@ -2,7 +2,7 @@
  * 
  * The ObjectStyle Group Software License, Version 1.0 
  *
- * Copyright (c) 2004 - 2005 The ObjectStyle Group 
+ * Copyright (c) 2004 - 2006 The ObjectStyle Group 
  * and individual authors of the software.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -88,7 +88,7 @@ public abstract class AbstractInstallAction extends AbstractActionOnIResource {
 		}
 	}
 
-	private class Runnable implements IRunnableWithProgress {
+	class Runnable implements IRunnableWithProgress {
 		IProject[] projectsToBuild;
 
 		String projectName = "";
