76686742390742e505e5712266e9321f1db19a49
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2006-09-02 16:01:35 +0000

    on an entity, if the name matches the table name and the class name (sans package), then keep them in sync as you change the entity name
    on an attribute, if the name matches the column name, then keep them in sync as you change the attribute name
    
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@3109 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ plugins/org.objectstyle.wolips.eomodeler/java/org/objectstyle/wolips/eomodeler/editors/entity/EntityNameSyncer.java
@@ -0,0 +1,31 @@
+package org.objectstyle.wolips.eomodeler.editors.entity;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import org.objectstyle.wolips.eomodeler.model.EOEntity;
+import org.objectstyle.wolips.eomodeler.utils.ComparisonUtils;
+
+public class EntityNameSyncer implements PropertyChangeListener {
+
+	public void propertyChange(PropertyChangeEvent _evt) {
+		EOEntity entity = (EOEntity) _evt.getSource();
+		String propertyName = _evt.getPropertyName();
+		if (EOEntity.NAME.equals(propertyName)) {
+			String oldName = (String) _evt.getOldValue();
+			String newName = (String) _evt.getNewValue();
+			if (ComparisonUtils.equals(oldName, entity.getExternalName())) {
+				entity.setExternalName(newName);
+			}
+			String className = entity.getClassName();
+			if (ComparisonUtils.equals(oldName, className)) {
+				entity.setClassName(newName);
+			}
+			else if (className != null && className.endsWith("." + oldName)) {
+				String oldPackage = className.substring(0, className.lastIndexOf('.') + 1);
+				entity.setClassName(oldPackage + newName);
+			}
+		}
+	}
+
+}
