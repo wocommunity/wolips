d2792acf97ed8b6b274739fcaa1e0b4dcb05f252
uli <uli@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2006-09-07 19:20:46 +0000

    restructuring
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@3166 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- plugins/org.objectstyle.wolips.eomodeler/java/org/objectstyle/wolips/eomodeler/editors/fetchspec/EOSortOrderingsConstants.java
+++
@@ -1,7 +0,0 @@
-package org.objectstyle.wolips.eomodeler.editors.fetchspec;
-
-import org.objectstyle.wolips.eomodeler.model.EOSortOrdering;
-
-public class EOSortOrderingsConstants {
-  public static final String[] COLUMNS = { EOSortOrdering.KEY, EOSortOrdering.ASCENDING, EOSortOrdering.CASE_INSENSITIVE }; 
-}
