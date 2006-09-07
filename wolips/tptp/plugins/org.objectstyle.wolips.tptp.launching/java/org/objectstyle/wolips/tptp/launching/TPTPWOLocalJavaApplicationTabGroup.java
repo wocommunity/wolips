82ba09745df1b4410fb05e43e79aaae1d8e2489a
uli <uli@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2006-09-07 18:53:23 +0000

    restructuring
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@3152 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ wolips/tptp/plugins/org.objectstyle.wolips.tptp.launching/java/org/objectstyle/wolips/tptp/launching/TPTPWOLocalJavaApplicationTabGroup.java
@@ -0,0 +1,131 @@
+/* ====================================================================
+ *
+ * The ObjectStyle Group Software License, Version 1.0
+ *
+ * Copyright (c) 2005 The ObjectStyle Group
+ * and individual authors of the software.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        ObjectStyle Group (http://objectstyle.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "ObjectStyle Group" and "Cayenne"
+ *    must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact andrus@objectstyle.org.
+ *
+ * 5. Products derived from this software may not be called "ObjectStyle"
+ *    nor may "ObjectStyle" appear in their names without prior written
+ *    permission of the ObjectStyle Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE OBJECTSTYLE GROUP OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the ObjectStyle Group.  For more
+ * information on the ObjectStyle Group, please see
+ * <http://objectstyle.org/>.
+ *
+ */
+
+package org.objectstyle.wolips.tptp.launching;
+
+import java.util.ArrayList;
+
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.CommonTab;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.debug.ui.ILaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.sourcelookup.SourceLookupTab;
+import org.eclipse.hyades.trace.internal.ui.TraceConstants;
+import org.eclipse.hyades.trace.ui.UIPlugin;
+import org.eclipse.hyades.trace.ui.internal.launcher.ProfileEnvironmentTab;
+import org.eclipse.hyades.trace.ui.internal.launcher.ProfileTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaArgumentsTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaClasspathTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaMainTab;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.objectstyle.wolips.launching.delegates.WOJavaLocalApplicationLaunchConfigurationDelegate;
+import org.objectstyle.wolips.launching.ui.CommonWOArgumentsTab;
+import org.objectstyle.wolips.launching.ui.LogTab;
+
+/**
+ * @author uli To change this generated comment edit the template variable
+ *         "typecomment": Window>Preferences>Java>Templates. To enable and
+ *         disable the creation of type comments go to
+ *         Window>Preferences>Java>Code Generation.
+ */
+public class TPTPWOLocalJavaApplicationTabGroup extends
+		AbstractLaunchConfigurationTabGroup {
+
+	/**
+	 * @see ILaunchConfigurationTabGroup#createTabs(ILaunchConfigurationDialog,
+	 *      String)
+	 */
+	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+		// check preferences whether profiling is enabled
+		IPreferenceStore store = UIPlugin.getDefault().getPreferenceStore();
+		boolean isProfilingEnabled = store
+				.getBoolean(TraceConstants.PROF_OPTION_KEY);
+
+		ArrayList tabs = new ArrayList(10);
+		tabs.add(new JavaMainTab());
+		tabs.add(new JavaArgumentsTab());
+		tabs.add(new CommonWOArgumentsTab());
+		tabs.add(new LogTab());
+		tabs.add(new JavaClasspathTab());
+
+		if (isProfilingEnabled) {
+			tabs.add(new ProfileTab(false));
+		}
+
+		tabs.add(new SourceLookupTab());
+		tabs.add(new ProfileEnvironmentTab());
+		tabs.add(new CommonTab());
+
+		ILaunchConfigurationTab[] array = new ILaunchConfigurationTab[tabs
+				.size()];
+		tabs.toArray(array);
+		setTabs(array);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup#setDefaults(org.eclipse.debug.core.ILaunchConfigurationWorkingCopy)
+	 */
+	public void setDefaults(ILaunchConfigurationWorkingCopy wc) {
+		WOJavaLocalApplicationLaunchConfigurationDelegate
+				.initConfiguration(wc);
+		super.setDefaults(wc);
+	}
+}
