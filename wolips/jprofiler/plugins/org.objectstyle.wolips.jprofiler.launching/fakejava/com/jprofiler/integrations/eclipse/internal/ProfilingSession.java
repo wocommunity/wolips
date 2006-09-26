d0655c414c4a57bb111f61ec03653d42fb190bd2
uli <uli@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2006-09-26 14:24:14 +0000

    fake class to build the stuff without the required plugin
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@3375 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ wolips/jprofiler/plugins/org.objectstyle.wolips.jprofiler.launching/fakejava/com/jprofiler/integrations/eclipse/internal/ProfilingSession.java
@@ -0,0 +1,19 @@
+package com.jprofiler.integrations.eclipse.internal;
+
+import java.util.Map;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+
+public class ProfilingSession {
+
+    public static ProfilingSession createProfilingSession(ILaunchConfigurationDelegate delegate, ILaunchConfiguration configuration) throws CoreException {
+    	return null;
+    }
+public Map getModifiedAttributesMap(Map map) {
+    	return null;
+    }
+
+
+}
