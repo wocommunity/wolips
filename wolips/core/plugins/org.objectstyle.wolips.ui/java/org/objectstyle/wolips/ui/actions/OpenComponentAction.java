a409afc2e612ce295d01fb01d41d1b9fccbd579e
uli <uli@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2006-10-21 13:58:48 +0000

    code review
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@3477 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/core/plugins/org.objectstyle.wolips.ui/java/org/objectstyle/wolips/ui/actions/OpenComponentAction.java
+++ wolips/core/plugins/org.objectstyle.wolips.ui/java/org/objectstyle/wolips/ui/actions/OpenComponentAction.java
@@ -66,10 +66,6 @@ import org.objectstyle.wolips.workbenchutilities.actions.AbstractActionOnIResour
 
 /**
  * @author uli
- * 
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates. To enable and disable the creation of type
- * comments go to Window>Preferences>Java>Code Generation.
  */
 public class OpenComponentAction extends AbstractActionOnIResource {
 
