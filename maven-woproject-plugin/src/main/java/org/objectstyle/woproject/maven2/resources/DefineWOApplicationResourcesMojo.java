86386cfebafd027a122f10574fe728fd13dde593
uli <uli@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2006-10-26 15:27:00 +0000

    dependencyPath
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk/woproject/maven2@3491 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- maven-woproject-plugin/src/main/java/org/objectstyle/woproject/maven2/resources/DefineWOApplicationResourcesMojo.java
+++ maven-woproject-plugin/src/main/java/org/objectstyle/woproject/maven2/resources/DefineWOApplicationResourcesMojo.java
@@ -1,11 +1,12 @@
 package org.objectstyle.woproject.maven2.resources;
 
 //org.apache.maven.plugins:maven-compiler-plugin:compile
-import java.io.File;
-import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
-import org.apache.maven.model.Resource;
-import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
@@ -28,6 +29,15 @@ public class DefineWOApplicationResourcesMojo extends DefineResourcesMojo {
 	 */
 	private MavenProject project;
 
+	/**
+	 * The maven repo.
+	 * 
+	 * @parameter expression="${maven.repo.local}"
+	 * @required
+	 * @readonly
+	 */
+	private String mavenRepoLocal;
+
 	public DefineWOApplicationResourcesMojo() throws MojoExecutionException {
 		super();
 	}
@@ -39,67 +49,81 @@ public class DefineWOApplicationResourcesMojo extends DefineResourcesMojo {
 
 	private void defineClasspath() {
 		getLog().info("Defining wo classpath....");
+		List dependencies = this.getProject().getDependencies();
+		// Set set = this.getProject().getArtifacts();
+		Iterator dependenciesIterator = dependencies.iterator();
+		while (dependenciesIterator.hasNext()) {
+			Dependency dependency = (Dependency) dependenciesIterator.next();
+			String depenendencyGroup = dependency.getGroupId();
+			String depenendencyArtifact = dependency.getArtifactId();
+			String depenendencyVersion = dependency.getVersion();
+			String dependencyPath = mavenRepoLocal + "/" + depenendencyGroup
+					+ "/" + depenendencyArtifact + "/" + depenendencyVersion
+					+ "/" + depenendencyArtifact + "-" + depenendencyVersion
+					+ ".jar";
+			getLog().info("Defining wo classpath: dependencyPath: " + dependencyPath);
 		}
+	}
 
 	public MavenProject getProject() {
 		return project;
 	}
-	
-//
-//
-//	<j:forEach var="artifact" items="${pom.artifacts}">
-//
-//		<j:set var="dep" value="${artifact.dependency}"/>
-//		<j:set var="depordering"
-//			value="${dep.getProperty('woaclasspath.ordering')}"/>
-//		<j:set var="depclasspathsystementry"
-//			value="${dep.getProperty('woaclasspath.system.entry')}"/>
-//		<j:choose>
-//			<j:when test="${dep.jar != null}">
-//				<j:set var="libname" value="${dep.jar}"/>
-//			</j:when>
-//			<j:otherwise>
-//				<j:set var="libname"
-//					value="${dep.artifactId}-${dep.version}.jar"/>
-//			</j:otherwise>
-//		</j:choose>
-//		<j:set var="lib"
-//			value="${maven.repo.local}${file.separator}${dep.groupId}${file.separator}${dep.type}s${file.separator}${libname}"/>
-//
-//		<j:choose>
-//			<j:when test="${depclasspathsystementry == null}">
-//				<j:choose>
-//					<j:when
-//						test="${dep.type == 'jar' || dep.type=='ejb' || empty(dep.type)}">
-//						<j:choose>
-//								<echo>Including lib: ${dep.artifactId}</echo>
-//							<j:when test="${depordering != null}">
-//								<j:set var="libwithordering"
-//									value="${maven.ordering.dir}${file.separator}${depordering}${libname}"/>
-//								<ant:copy file="${lib}"
-//									tofile="${libwithordering}"/>
-//								<ant:lib file="${libwithordering}">
-//								</ant:lib>
-//							</j:when>
-//							<j:otherwise>
-//								<ant:lib file="${lib}">
-//								</ant:lib>
-//							</j:otherwise>
-//						</j:choose>
-//					</j:when>
-//					<j:otherwise>
-//						<echo>Unknwon type set for dependency ${dep.artifatId}</echo>
-//					</j:otherwise>
-//				</j:choose>
-//			</j:when>
-//			<j:otherwise>
-//				<ant:frameworks root="${wo.wosystemroot}">
-//					<include name="${depclasspathsystementry}"/>
-//				</ant:frameworks>
-//<!--						<ant:echo message="${wo.wosystemroot}"/>-->
-//				<ant:echo message="Including lib: ${depclasspathsystementry}"/>
-//			</j:otherwise>
-//		</j:choose>
-//
-//	</j:forEach>
+
+	//
+	//
+	// <j:forEach var="artifact" items="${pom.artifacts}">
+	//
+	// <j:set var="dep" value="${artifact.dependency}"/>
+	// <j:set var="depordering"
+	// value="${dep.getProperty('woaclasspath.ordering')}"/>
+	// <j:set var="depclasspathsystementry"
+	// value="${dep.getProperty('woaclasspath.system.entry')}"/>
+	// <j:choose>
+	// <j:when test="${dep.jar != null}">
+	// <j:set var="libname" value="${dep.jar}"/>
+	// </j:when>
+	// <j:otherwise>
+	// <j:set var="libname"
+	// value="${dep.artifactId}-${dep.version}.jar"/>
+	// </j:otherwise>
+	// </j:choose>
+	// <j:set var="lib"
+	// value="${maven.repo.local}${file.separator}${dep.groupId}${file.separator}${dep.type}s${file.separator}${libname}"/>
+	//
+	// <j:choose>
+	// <j:when test="${depclasspathsystementry == null}">
+	// <j:choose>
+	// <j:when
+	// test="${dep.type == 'jar' || dep.type=='ejb' || empty(dep.type)}">
+	// <j:choose>
+	// <echo>Including lib: ${dep.artifactId}</echo>
+	// <j:when test="${depordering != null}">
+	// <j:set var="libwithordering"
+	// value="${maven.ordering.dir}${file.separator}${depordering}${libname}"/>
+	// <ant:copy file="${lib}"
+	// tofile="${libwithordering}"/>
+	// <ant:lib file="${libwithordering}">
+	// </ant:lib>
+	// </j:when>
+	// <j:otherwise>
+	// <ant:lib file="${lib}">
+	// </ant:lib>
+	// </j:otherwise>
+	// </j:choose>
+	// </j:when>
+	// <j:otherwise>
+	// <echo>Unknwon type set for dependency ${dep.artifatId}</echo>
+	// </j:otherwise>
+	// </j:choose>
+	// </j:when>
+	// <j:otherwise>
+	// <ant:frameworks root="${wo.wosystemroot}">
+	// <include name="${depclasspathsystementry}"/>
+	// </ant:frameworks>
+	// <!-- <ant:echo message="${wo.wosystemroot}"/>-->
+	// <ant:echo message="Including lib: ${depclasspathsystementry}"/>
+	// </j:otherwise>
+	// </j:choose>
+	//
+	// </j:forEach>
 }
