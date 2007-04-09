1e465a27cfb01ffdf9196c9aac65af37a5a387f7
uli <uli@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-04-09 10:20:36 +0000

    WOL-394
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk/woproject/maven2@3915 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- maven-wolifecycle-plugin/src/main/java/org/objectstyle/woproject/maven2/wolifecycle/WOMojo.java
+++ maven-wolifecycle-plugin/src/main/java/org/objectstyle/woproject/maven2/wolifecycle/WOMojo.java
@@ -3,6 +3,7 @@ package org.objectstyle.woproject.maven2.wolifecycle;
 //org.apache.maven.plugins:maven-compiler-plugin:compile
 import java.io.File;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.project.MavenProject;
 
@@ -14,17 +15,13 @@ public abstract class WOMojo extends AbstractMojo {
 		super();
 	}
 
-	protected boolean isWebobjectAppleGroup(String dependencyGroup) {
-		boolean returnValue = false;
-		if (dependencyGroup != null) {
-			if(dependencyGroup.indexOf('.') >= 0) {
-				throw new IllegalStateException();
-			}
-			returnValue = dependencyGroup.equals(MAVEN_WEBOBJECTS_GROUP_ID);
-		}
-		getLog().debug("WOMojo: isWebobjectAppleGroup: " + dependencyGroup + " return value " + returnValue);
-		return returnValue;
+	private boolean containsDot(String text) {
+		return text.indexOf('.') >= 0;
 	}
+
+	public abstract String getProductExtension();
+
+	public abstract MavenProject getProject();
 
 	protected String getProjectFolder() {
 		String projectFolder = this.getProject().getFile().getPath().substring(0, this.getProject().getFile().getPath().length() - 7);
@@ -39,7 +36,21 @@ public abstract class WOMojo extends AbstractMojo {
 		return null;
 	}
 
-	public abstract String getProductExtension();
+	protected boolean isWebobjectAppleGroup(String dependencyGroup) {
+		if (dependencyGroup == null) {
+			return false;
+		}
 
-	public abstract MavenProject getProject();
+		if (containsDot(dependencyGroup)) {
+			throw new IllegalArgumentException("Dependency group cannot contains '.' (dot).");
+		}
+
+		String normalizedGroup = FilenameUtils.separatorsToUnix(dependencyGroup);
+
+		boolean returnValue = MAVEN_WEBOBJECTS_GROUP_ID.equals(normalizedGroup);
+
+		getLog().debug("WOMojo: isWebobjectAppleGroup: " + normalizedGroup + " return value " + returnValue);
+
+		return returnValue;
+	}
 }
