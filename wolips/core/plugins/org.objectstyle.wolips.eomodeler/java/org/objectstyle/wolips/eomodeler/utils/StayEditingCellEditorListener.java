edc031af5c9279419d337020a23f25e817d3d89a
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-05-19 14:19:28 +0000

    WOL-426
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4027 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/core/plugins/org.objectstyle.wolips.eomodeler/java/org/objectstyle/wolips/eomodeler/utils/StayEditingCellEditorListener.java
+++ wolips/core/plugins/org.objectstyle.wolips.eomodeler/java/org/objectstyle/wolips/eomodeler/utils/StayEditingCellEditorListener.java
@@ -3,11 +3,14 @@ package org.objectstyle.wolips.eomodeler.utils;
 import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.jface.viewers.ICellEditorListener;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.events.TraverseEvent;
+import org.eclipse.swt.events.TraverseListener;
 import org.eclipse.swt.widgets.Display;
 
-public class StayEditingCellEditorListener implements ICellEditorListener, SelectionListener {
+public class StayEditingCellEditorListener implements ICellEditorListener, SelectionListener, TraverseListener {
 	private TableViewer _tableViewer;
 
 	private int _column;
@@ -17,8 +20,10 @@ public class StayEditingCellEditorListener implements ICellEditorListener, Selec
 		CellEditor cellEditor = _tableViewer.getCellEditors()[column];
 		if (cellEditor instanceof EMTextCellEditor) {
 			((EMTextCellEditor) cellEditor).getText().addSelectionListener(this);
+			((EMTextCellEditor) cellEditor).getText().addTraverseListener(this);
 		} else if (cellEditor instanceof KeyComboBoxCellEditor) {
 			((KeyComboBoxCellEditor) cellEditor).getComboBox().addSelectionListener(this);
+			((KeyComboBoxCellEditor) cellEditor).getComboBox().addTraverseListener(this);
 		}
 		_column = column;
 	}
@@ -29,6 +34,35 @@ public class StayEditingCellEditorListener implements ICellEditorListener, Selec
 
 	public int getColumn() {
 		return _column;
+	}
+
+	public void keyTraversed(TraverseEvent e) {
+		if (e.detail == SWT.TRAVERSE_TAB_NEXT) {
+			int selectionIndex = _tableViewer.getTable().getSelectionIndex();
+			if (selectionIndex != -1) {
+				final Object thisObject = _tableViewer.getElementAt(selectionIndex);
+				if (thisObject != null) {
+					Display.getCurrent().asyncExec(new Runnable() {
+						public void run() {
+							getTableViewer().editElement(thisObject, getColumn() + 1);
+						}
+					});
+				}
+			}
+		}
+		else if (e.detail == SWT.TRAVERSE_TAB_PREVIOUS) {
+			int selectionIndex = _tableViewer.getTable().getSelectionIndex();
+			if (selectionIndex != -1) {
+				final Object thisObject = _tableViewer.getElementAt(selectionIndex);
+				if (thisObject != null) {
+					Display.getCurrent().asyncExec(new Runnable() {
+						public void run() {
+							getTableViewer().editElement(thisObject, getColumn() - 1);
+						}
+					});
+				}
+			}
+		}
 	}
 
 	public void widgetDefaultSelected(SelectionEvent e) {
