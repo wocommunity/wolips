fc305ae2af208c330ffd29a54c71e8f93a0da1a3
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-05-19 14:30:23 +0000

    WOL-440
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4028 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/plugins/org.objectstyle.wolips.eogenerator.ui/java/org/objectstyle/wolips/eogenerator/ui/dialogs/EOGeneratorResultsDialog.java
+++ wolips/plugins/org.objectstyle.wolips.eogenerator.ui/java/org/objectstyle/wolips/eogenerator/ui/dialogs/EOGeneratorResultsDialog.java
@@ -3,11 +3,11 @@ package org.objectstyle.wolips.eogenerator.ui.dialogs;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
 
 public class EOGeneratorResultsDialog extends MessageDialog {
 	private String myResults;
@@ -16,14 +16,20 @@ public class EOGeneratorResultsDialog extends MessageDialog {
 		super(_parentShell, "EOGenerator Finished", null, "EOGenerator finished with the following results:", INFORMATION, new String[] { IDialogConstants.OK_LABEL }, 0);
 		myResults = _results;
 	}
+	
+	@Override
+	protected int getShellStyle() {
+		return SWT.CLOSE|SWT.RESIZE;
+	}
 
 	protected Control createCustomArea(Composite _parent) {
-		Text resultsText = new Text(_parent, SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER);
+		StyledText resultsText = new StyledText(_parent, SWT.MULTI | SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
 		resultsText.setText(myResults);
 		resultsText.setEditable(false);
+		resultsText.setWordWrap(false);
 		GridData gd = new GridData(GridData.FILL_BOTH);
-		gd.heightHint = 300;
-		gd.widthHint = 500;
+		gd.heightHint = 500;
+		gd.widthHint = 700;
 		resultsText.setLayoutData(gd);
 		return resultsText;
 	}
