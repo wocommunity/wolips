d45ebee9bc8ec42a3b625844931fb8ffefa9cf76
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-05-20 12:46:57 +0000

    WOL-167, clean up insert tag dialog
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4035 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/plugins/org.objectstyle.wolips.componenteditor/java/org/objectstyle/wolips/componenteditor/actions/InsertComponentDialogue.java
+++ wolips/plugins/org.objectstyle.wolips.componenteditor/java/org/objectstyle/wolips/componenteditor/actions/InsertComponentDialogue.java
@@ -1,16 +1,25 @@
 package org.objectstyle.wolips.componenteditor.actions;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
+import org.objectstyle.wolips.wodclipse.core.completion.WodCompletionProposal;
+import org.objectstyle.wolips.wodclipse.core.completion.WodCompletionUtils;
 
 /**
  * <P>
@@ -26,16 +35,16 @@ public class InsertComponentDialogue extends org.eclipse.jface.dialogs.Dialog {
 	public final static int RESULT_CANCEL = 2;
 
 	// no time to localise :-(
-	public final static String LABEL_COMPONENT_INSTANCE_NAME = "Supply a name for your component:";
+	public final static String LABEL_COMPONENT_INSTANCE_NAME = "WebObject Tag Name:";
 
 	// no time to localise :-(
-	public final static String LABEL_COMPONENT_NAME = "Component Name:";
+	public final static String LABEL_COMPONENT_NAME = "Component name:";
 
 	// user interface elements which the user interacts with.
 
 	protected Label _componentNameLabel;
 
-	protected Text _componentNameText;
+	protected Combo _componentNameCombo;
 
 	protected Label _componentInstanceLabel;
 
@@ -47,10 +56,13 @@ public class InsertComponentDialogue extends org.eclipse.jface.dialogs.Dialog {
 
 	protected Button _inline;
 
+	protected IJavaProject _project;
+	
 	protected InsertComponentSpecification _insertComponentSpecification;
 
-	public InsertComponentDialogue(Shell parentShell, InsertComponentSpecification insertComponentSpecification) {
+	public InsertComponentDialogue(Shell parentShell, IJavaProject project, InsertComponentSpecification insertComponentSpecification) {
 		super(parentShell);
+		_project = project;
 		_insertComponentSpecification = insertComponentSpecification;
 	}
 
@@ -67,20 +79,38 @@ public class InsertComponentDialogue extends org.eclipse.jface.dialogs.Dialog {
 		if (_insertComponentSpecification.getComponentName() == null) {
 			_componentNameLabel = new Label(control, 0);
 			_componentNameLabel.setText(LABEL_COMPONENT_NAME);
+			GridData componentNameLabelLayout = new GridData(GridData.FILL_HORIZONTAL);
+			_componentNameLabel.setLayoutData(componentNameLabelLayout);
 
-			_componentNameText = new Text(control, SWT.BORDER);
-			_componentNameText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+			_componentNameCombo = new Combo(control, SWT.BORDER);
+			GridData componentNameTextLayout = new GridData(GridData.FILL_HORIZONTAL);
+			_componentNameCombo.setLayoutData(componentNameTextLayout);
+			_componentNameCombo.addModifyListener(new ModifyListener() {
+				public void modifyText(ModifyEvent e) {
+					String partialName = _componentNameCombo.getText();
+					if (partialName.length() > 2) {
+						Set<WodCompletionProposal> proposals = new HashSet<WodCompletionProposal>();
+						try {
+							WodCompletionUtils.fillInElementTypeCompletionProposals(_project, partialName, 0, partialName.length(), proposals, false);
+							_componentNameCombo.removeAll();
+							for (WodCompletionProposal elementName : proposals) {
+								_componentNameCombo.add(elementName.getDisplayString());
+							}
+						} catch (Throwable t) {
+							t.printStackTrace();
+						}
+					}
+				}
+			});
 		}
 
 		_inline = new Button(control, SWT.CHECK);
-		_inline.setText("Inline bindings?");
+		_inline.setText("Use inline bindings, or enter webobject tag name:");
 		_inline.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 
-		_componentInstanceLabel = new Label(control, 0);
-		_componentInstanceLabel.setText(LABEL_COMPONENT_INSTANCE_NAME);
-
 		_componentInstanceNameText = new Text(control, SWT.BORDER);
-		_componentInstanceNameText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		GridData componentInstanceNameLayout = new GridData(GridData.FILL_HORIZONTAL);
+		_componentInstanceNameText.setLayoutData(componentInstanceNameLayout);
 		_componentInstanceNameText.addVerifyListener(new ComponentInstanceNameVerifyListener());
 
 		if (null != _insertComponentSpecification.getComponentInstanceName()) {
@@ -97,7 +127,7 @@ public class InsertComponentDialogue extends org.eclipse.jface.dialogs.Dialog {
 			}
 
 			public void widgetSelected(SelectionEvent e) {
-				_componentInstanceLabel.setEnabled(!_inline.getSelection());
+				//_componentInstanceLabel.setEnabled(!_inline.getSelection());
 				_componentInstanceNameText.setEnabled(!_inline.getSelection());
 			}
 		});
@@ -112,8 +142,8 @@ public class InsertComponentDialogue extends org.eclipse.jface.dialogs.Dialog {
 	}
 
 	public void okPressed() {
-		if (_componentNameText != null) {
-			_insertComponentSpecification.setComponentName(_componentNameText.getText());
+		if (_componentNameCombo != null) {
+			_insertComponentSpecification.setComponentName(_componentNameCombo.getText());
 		}
 		_insertComponentSpecification.setComponentInstanceName(_componentInstanceNameText.getText());
 		_insertComponentSpecification.setInline(_inline.getSelection());
