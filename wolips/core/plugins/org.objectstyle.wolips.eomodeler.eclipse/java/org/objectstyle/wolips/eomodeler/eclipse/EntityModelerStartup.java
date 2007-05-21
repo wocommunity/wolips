2f991fdd12bfc4a20f2667e86928cad012e7280b
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-05-21 15:18:19 +0000

    fix for occasional (non-fatal) NPE
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4049 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/core/plugins/org.objectstyle.wolips.eomodeler.eclipse/java/org/objectstyle/wolips/eomodeler/eclipse/EntityModelerStartup.java
+++ wolips/core/plugins/org.objectstyle.wolips.eomodeler.eclipse/java/org/objectstyle/wolips/eomodeler/eclipse/EntityModelerStartup.java
@@ -1,13 +1,12 @@
 package org.objectstyle.wolips.eomodeler.eclipse;
 
 import org.eclipse.ui.IStartup;
-import org.eclipse.ui.PlatformUI;
 
 public class EntityModelerStartup implements IStartup {
 
 	public void earlyStartup() {
 		PackageExplorerDoubleClickHandler doubleClickHandler = new PackageExplorerDoubleClickHandler();
-		PlatformUI.getWorkbench().getActiveWorkbenchWindow().addPageListener(doubleClickHandler);
+		doubleClickHandler.registerListeners();
 	}
 
 }
