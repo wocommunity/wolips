00ca582acee3a757bff9423a35935fd3ad84077b
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-06-26 02:47:08 +0000

    Fixes for inserting components into blank documents and for NPE from WOTextArea insert
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4118 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ wolips/core/plugins/org.objectstyle.wolips.componenteditor/java/org/objectstyle/wolips/componenteditor/actions/InsertComponentWOTextAction.java
@@ -0,0 +1,7 @@
+package org.objectstyle.wolips.componenteditor.actions;
+
+
+public class InsertComponentWOTextAction extends InsertComponentAction {
+	public String getComponentInstanceNameSuffix() { return "Text"; }
+	public String getComponentName() { return "WOText"; }
+}
--- wolips/core/plugins/org.objectstyle.wolips.componenteditor/java/org/objectstyle/wolips/componenteditor/actions/InsertComponentWOTextAreaAction.java
+++
@@ -1,7 +0,0 @@
-package org.objectstyle.wolips.componenteditor.actions;
-
-
-public class InsertComponentWOTextAreaAction extends InsertComponentAction {
-	public String getComponentInstanceNameSuffix() { return "TextArea"; }
-	public String getComponentName() { return "WOTextArea"; }
-}
