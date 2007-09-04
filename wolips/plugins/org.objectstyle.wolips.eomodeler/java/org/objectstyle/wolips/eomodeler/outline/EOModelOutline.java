fd7c63f4861a8c2f8bb3094108bd03ed5fd897f6
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-09-04 14:31:20 +0000

    custom subclass of outline so we can avoid the problem where eomodeler icons appear for all outline views ... I have to believe there's some way to restrict those to a perspective, but who knows ... maybe not
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4227 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ wolips/plugins/org.objectstyle.wolips.eomodeler/java/org/objectstyle/wolips/eomodeler/outline/EOModelOutline.java
@@ -0,0 +1,12 @@
+package org.objectstyle.wolips.eomodeler.outline;
+
+import org.eclipse.ui.views.contentoutline.ContentOutline;
+
+public class EOModelOutline extends ContentOutline {
+	public static String ID_OUTLINE = "org.objectstyle.wolips.eomodeler.outline.EOModelOutline"; //$NON-NLS-1$
+
+	public EOModelOutline() {
+		// nothing right now
+	}
+
+}
