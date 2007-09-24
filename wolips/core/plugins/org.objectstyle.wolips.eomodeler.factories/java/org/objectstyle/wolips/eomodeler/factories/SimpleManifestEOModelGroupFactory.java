26af77b0010e22b451c5bb74ad3f11dcaf040c1b
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-09-24 05:20:11 +0000

    simpler api for loading model groups that supports multiple model group factories support for parsing of IDEA project files for loading models
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4256 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/core/plugins/org.objectstyle.wolips.eomodeler.factories/java/org/objectstyle/wolips/eomodeler/factories/SimpleManifestEOModelGroupFactory.java
+++ wolips/core/plugins/org.objectstyle.wolips.eomodeler.factories/java/org/objectstyle/wolips/eomodeler/factories/SimpleManifestEOModelGroupFactory.java
@@ -11,39 +11,38 @@ import org.objectstyle.wolips.eomodeler.core.model.AbstractManifestEOModelGroupF
 import org.objectstyle.wolips.eomodeler.core.model.ManifestSearchFolder;
 
 public class SimpleManifestEOModelGroupFactory extends AbstractManifestEOModelGroupFactory {
-  @Override
-  public List<ManifestSearchFolder> getSearchFolders(File selectedModelFolder) throws IOException {
-    List<ManifestSearchFolder> searchFolders = new LinkedList<ManifestSearchFolder>();
-    fillInSearchFolders(new File(selectedModelFolder, "EntityModeler.modelpath"), searchFolders);
-    fillInSearchFolders(new File(selectedModelFolder, ".EntityModeler.modelpath"), searchFolders);
-    if (selectedModelFolder != null) {
-	    fillInSearchFolders(new File(selectedModelFolder.getParentFile(), "EntityModeler.modelpath"), searchFolders);
-	    fillInSearchFolders(new File(selectedModelFolder.getParentFile(), ".EntityModeler.modelpath"), searchFolders);
-    }
-    fillInSearchFolders(new File(System.getProperty("user.home"), "EntityModeler.modelpath"), searchFolders);
-    fillInSearchFolders(new File(System.getProperty("user.home"), ".EntityModeler.modelpath"), searchFolders);
-    fillInSearchFolders(new File(System.getProperty("user.home") + "/Library", "EntityModeler.modelpath"), searchFolders);
-    fillInSearchFolders(new File(System.getProperty("user.home") + "/Library", ".EntityModeler.modelpath"), searchFolders);
-    fillInSearchFolders(new File(System.getProperty("user.home") + "/Library/Preferences", "EntityModeler.modelpath"), searchFolders);
-    fillInSearchFolders(new File(System.getProperty("user.home") + "/Library/Preferences", ".EntityModeler.modelpath"), searchFolders);
-    return searchFolders;
-  }
+	@Override
+	public List<ManifestSearchFolder> getSearchFolders(File selectedModelFolder) throws IOException {
+		List<ManifestSearchFolder> searchFolders = new LinkedList<ManifestSearchFolder>();
+		fillInSearchFolders(new File(selectedModelFolder, "EntityModeler.modelpath"), searchFolders);
+		fillInSearchFolders(new File(selectedModelFolder, ".EntityModeler.modelpath"), searchFolders);
+		if (selectedModelFolder != null) {
+			fillInSearchFolders(new File(selectedModelFolder.getParentFile(), "EntityModeler.modelpath"), searchFolders);
+			fillInSearchFolders(new File(selectedModelFolder.getParentFile(), ".EntityModeler.modelpath"), searchFolders);
+		}
+		fillInSearchFolders(new File(System.getProperty("user.home"), "EntityModeler.modelpath"), searchFolders);
+		fillInSearchFolders(new File(System.getProperty("user.home"), ".EntityModeler.modelpath"), searchFolders);
+		fillInSearchFolders(new File(System.getProperty("user.home") + "/Library", "EntityModeler.modelpath"), searchFolders);
+		fillInSearchFolders(new File(System.getProperty("user.home") + "/Library", ".EntityModeler.modelpath"), searchFolders);
+		fillInSearchFolders(new File(System.getProperty("user.home") + "/Library/Preferences", "EntityModeler.modelpath"), searchFolders);
+		fillInSearchFolders(new File(System.getProperty("user.home") + "/Library/Preferences", ".EntityModeler.modelpath"), searchFolders);
+		return searchFolders;
+	}
 
-  protected void fillInSearchFolders(File manifestFile, List<ManifestSearchFolder> searchFolders) throws IOException {
-    if (manifestFile.exists()) {
-      BufferedReader manifestReader = new BufferedReader(new FileReader(manifestFile));
-      try {
-        String searchFolderPath;
-        while ((searchFolderPath = manifestReader.readLine()) != null) {
-          File searchFolder = new File(searchFolderPath).getAbsoluteFile();
-          if (searchFolder != null && searchFolder.exists()) {
-            searchFolders.add(new ManifestSearchFolder(searchFolder));
-          }
-        }
-      }
-      finally {
-        manifestReader.close();
-      }
-    }
-  }
+	protected void fillInSearchFolders(File manifestFile, List<ManifestSearchFolder> searchFolders) throws IOException {
+		if (manifestFile.exists()) {
+			BufferedReader manifestReader = new BufferedReader(new FileReader(manifestFile));
+			try {
+				String searchFolderPath;
+				while ((searchFolderPath = manifestReader.readLine()) != null) {
+					File searchFolder = new File(searchFolderPath).getAbsoluteFile();
+					if (searchFolder != null && searchFolder.exists()) {
+						searchFolders.add(new ManifestSearchFolder(searchFolder));
+					}
+				}
+			} finally {
+				manifestReader.close();
+			}
+		}
+	}
 }
