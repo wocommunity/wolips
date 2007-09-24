d15dbdd71ef22ed35b17c5ab1cd5c9e81a16a08b
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-09-24 16:19:16 +0000

    tag delegates
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4261 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ wolips/core/plugins/org.objectstyle.wolips.htmlpreview/java/org/objectstyle/wolips/htmlpreview/editor/TagDelegate.java
@@ -0,0 +1,18 @@
+package org.objectstyle.wolips.htmlpreview.editor;
+
+import java.util.Stack;
+
+import jp.aonir.fuzzyxml.FuzzyXMLElement;
+import jp.aonir.fuzzyxml.FuzzyXMLNode;
+import jp.aonir.fuzzyxml.internal.RenderContext;
+
+import org.objectstyle.wolips.wodclipse.core.completion.WodParserCache;
+import org.objectstyle.wolips.wodclipse.core.model.IWodElement;
+
+public abstract class TagDelegate {
+	public abstract void renderNode(IWodElement wodElement, FuzzyXMLElement xmlElement, RenderContext renderContext, StringBuffer htmlBuffer, StringBuffer cssBuffer, Stack<WodParserCache> caches, Stack<FuzzyXMLNode> nodes);
+
+	public void reset() {
+		// DO NOTHING
+	}
+}
