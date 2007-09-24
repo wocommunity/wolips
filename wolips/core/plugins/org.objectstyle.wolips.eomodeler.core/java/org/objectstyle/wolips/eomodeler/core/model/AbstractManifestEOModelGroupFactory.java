5f6cf3ae3e8b3d8f0eea685f68b7424e6a30a0cb
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-09-24 05:20:35 +0000

    simpler api for loading model groups that supports multiple model group factories support for parsing of IDEA project files for loading models
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4258 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/core/plugins/org.objectstyle.wolips.eomodeler.core/java/org/objectstyle/wolips/eomodeler/core/model/AbstractManifestEOModelGroupFactory.java
+++ wolips/core/plugins/org.objectstyle.wolips.eomodeler.core/java/org/objectstyle/wolips/eomodeler/core/model/AbstractManifestEOModelGroupFactory.java
@@ -12,69 +12,30 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.objectstyle.wolips.eomodeler.core.utils.URLUtils;
 
 public abstract class AbstractManifestEOModelGroupFactory implements IEOModelGroupFactory {
-	public boolean canLoadModelFrom(Object modelResource) {
-		return modelResource instanceof IResource || modelResource instanceof File || modelResource instanceof URL || modelResource instanceof URI;
-	}
-
-	public boolean canLoadModelGroupFrom(Object modelGroupResource) {
-		return modelGroupResource instanceof File;
-	}
-
-	public EOModel loadModel(Object modelResource, Set<EOModelVerificationFailure> failures, boolean skipOnDuplicates, IProgressMonitor progressMonitor) throws EOModelException {
-		File selectedModelFile;
-		if (modelResource instanceof IResource) {
-			selectedModelFile = ((IResource) modelResource).getLocation().toFile();
-		} else if (modelResource instanceof File) {
-			selectedModelFile = (File) modelResource;
-		} else if (modelResource instanceof URL) {
-			selectedModelFile = URLUtils.cheatAndTurnIntoFile((URL) modelResource);
-		} else if (modelResource instanceof URI) {
-			selectedModelFile = URLUtils.cheatAndTurnIntoFile((URI) modelResource);
-		} else {
-			throw new EOModelException("Unknown model resource: " + modelResource);
-		}
-
-		if (selectedModelFile == null) {
-			throw new EOModelException("Unknown model resource: " + modelResource);
-		}
-
-		File selectedModelFolder;
-		if (selectedModelFile.isFile()) {
-			selectedModelFolder = selectedModelFile.getParentFile();
-		} else {
-			selectedModelFolder = selectedModelFile;
-		}
-		String modelFolderName = selectedModelFolder.getName();
-		if (!modelFolderName.endsWith(".eomodeld")) {
-			throw new EOModelException(selectedModelFolder.getAbsolutePath() + " is not an EOModel folder.");
-		}
+	public void loadModelGroup(Object modelGroupResource, EOModelGroup modelGroup, Set<EOModelVerificationFailure> failures, boolean skipOnDuplicates, IProgressMonitor progressMonitor) throws EOModelException {
 		try {
-			EOModelGroup modelGroup = new EOModelGroup();
-			modelGroup.setEditingModelURL(selectedModelFolder.toURL());
-			List<ManifestSearchFolder> searchFolders = getSearchFolders(selectedModelFolder);
+			File modelGroupFile = null;
+			if (modelGroupResource == null) {
+				modelGroupFile = null;
+			} else if (modelGroupResource instanceof IResource) {
+				modelGroupFile = ((IResource) modelGroupResource).getLocation().toFile();
+			} else if (modelGroupResource instanceof File) {
+				modelGroupFile = (File) modelGroupResource;
+			} else if (modelGroupResource instanceof URL) {
+				modelGroupFile = URLUtils.cheatAndTurnIntoFile((URL) modelGroupResource);
+			} else if (modelGroupResource instanceof URI) {
+				modelGroupFile = URLUtils.cheatAndTurnIntoFile((URI) modelGroupResource);
+			} else {
+				throw new EOModelException("Unknown model resource: " + modelGroupResource);
+			}
+
+			List<ManifestSearchFolder> searchFolders = getSearchFolders(modelGroupFile);
 			for (ManifestSearchFolder searchFolder : searchFolders) {
 				modelGroup.loadModelsFromURL(searchFolder.getFolder().toURL(), searchFolder.getDepth(), failures, skipOnDuplicates, progressMonitor);
 			}
-			EOModel model = modelGroup.loadModelFromURL(selectedModelFolder.toURL(), failures, skipOnDuplicates, progressMonitor);
-			modelGroup.resolve(failures);
-			modelGroup.verify(failures);
-			return model;
-		} catch (IOException e) {
-			throw new EOModelException("Failed to load model.", e);
-		}
-	}
-
-	public EOModelGroup loadModelGroup(Object modelGroupResource, Set<EOModelVerificationFailure> failures, boolean skipOnDuplicates, URL editingModelURL, IProgressMonitor progressMonitor) throws EOModelException {
-		try {
-			EOModelGroup modelGroup = new EOModelGroup();
-			List<ManifestSearchFolder> searchFolders = getSearchFolders(null);
-			modelGroup.setEditingModelURL(editingModelURL);
-			for (ManifestSearchFolder searchFolder : searchFolders) {
-				modelGroup.loadModelsFromURL(searchFolder.getFolder().toURL(), searchFolder.getDepth(), failures, skipOnDuplicates, progressMonitor);
+			if (modelGroupFile != null && modelGroupFile.getName().endsWith(".eomodeld")) {
+				modelGroup.loadModelsFromURL(modelGroupFile.toURL(), 1, failures, skipOnDuplicates, progressMonitor);
 			}
-			modelGroup.resolve(failures);
-			modelGroup.verify(failures);
-			return modelGroup;
 		} catch (IOException e) {
 			throw new EOModelException("Failed to load model groups.", e);
 		}
