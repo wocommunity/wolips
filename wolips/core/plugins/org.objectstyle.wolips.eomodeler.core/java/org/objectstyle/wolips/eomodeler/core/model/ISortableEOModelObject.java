6388b48e40da5ec9583c247462f029b5f754dabb
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-09-27 14:40:31 +0000

    add setName
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4297 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/core/plugins/org.objectstyle.wolips.eomodeler.core/java/org/objectstyle/wolips/eomodeler/core/model/ISortableEOModelObject.java
+++ wolips/core/plugins/org.objectstyle.wolips.eomodeler.core/java/org/objectstyle/wolips/eomodeler/core/model/ISortableEOModelObject.java
@@ -2,4 +2,6 @@ package org.objectstyle.wolips.eomodeler.core.model;
 
 public interface ISortableEOModelObject {
 	public String getName();
+	
+	public void setName(String name) throws DuplicateNameException;
 }
