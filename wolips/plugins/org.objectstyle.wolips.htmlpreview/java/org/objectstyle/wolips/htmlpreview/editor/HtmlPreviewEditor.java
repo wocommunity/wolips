2cc3fdcc3878ae06440ea93420aafd4f2a151048
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-10-06 16:11:29 +0000

    new html preview tab
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4339 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/plugins/org.objectstyle.wolips.htmlpreview/java/org/objectstyle/wolips/htmlpreview/editor/HtmlPreviewEditor.java
+++ wolips/plugins/org.objectstyle.wolips.htmlpreview/java/org/objectstyle/wolips/htmlpreview/editor/HtmlPreviewEditor.java
@@ -43,35 +43,56 @@
  */
 package org.objectstyle.wolips.htmlpreview.editor;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import jp.aonir.fuzzyxml.FuzzyXMLDocument;
 import jp.aonir.fuzzyxml.FuzzyXMLElement;
+import jp.aonir.fuzzyxml.FuzzyXMLNode;
+import jp.aonir.fuzzyxml.FuzzyXMLText;
 import jp.aonir.fuzzyxml.internal.RenderContext;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.browser.LocationEvent;
+import org.eclipse.swt.browser.LocationListener;
+import org.eclipse.swt.browser.StatusTextEvent;
+import org.eclipse.swt.browser.StatusTextListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Item;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.IPropertyListener;
 import org.eclipse.ui.IWorkbenchPartSite;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.part.MultiPageEditorSite;
 import org.objectstyle.wolips.components.editor.EditorInteraction;
 import org.objectstyle.wolips.components.editor.IEmbeddedEditor;
 import org.objectstyle.wolips.components.editor.IEmbeddedEditorSelected;
 import org.objectstyle.wolips.htmlpreview.HtmlPreviewPlugin;
 import org.objectstyle.wolips.templateeditor.TemplateEditor;
+import org.objectstyle.wolips.templateeditor.TemplateSourceEditor;
+import org.objectstyle.wolips.wodclipse.core.Activator;
 import org.objectstyle.wolips.wodclipse.core.completion.WodParserCache;
+import org.objectstyle.wolips.wodclipse.core.model.IWodBinding;
+import org.objectstyle.wolips.wodclipse.core.model.IWodElement;
+import org.objectstyle.wolips.wodclipse.core.preferences.PreferenceConstants;
+import org.objectstyle.wolips.wodclipse.core.util.WodHtmlUtils;
+
+import tk.eclipse.plugin.htmleditor.HTMLUtil;
 
 /**
  * based on an eclipse.org example
  * 
  * @author uli
  */
-public class HtmlPreviewEditor implements IEmbeddedEditor, IEmbeddedEditorSelected, IEditorPart {
+public class HtmlPreviewEditor implements IEmbeddedEditor, IEmbeddedEditorSelected, IEditorPart, StatusTextListener {
 
 	private EditorInteraction _editorInteraction;
 
@@ -80,6 +101,10 @@ public class HtmlPreviewEditor implements IEmbeddedEditor, IEmbeddedEditorSelect
 	private IEditorInput _input;
 
 	private Browser _browser;
+
+	private Map<String, FuzzyXMLNode> _nodeMap;
+
+	private int _counter;
 
 	/**
 	 * Update the contents of the Preview page
@@ -92,13 +117,57 @@ public class HtmlPreviewEditor implements IEmbeddedEditor, IEmbeddedEditorSelect
 		IDocument editDocument = _editorInteraction.getHtmlDocumentProvider().getHtmlEditDocument();
 		String documentContents = editDocument.get();
 
+		_counter = 0;
+		_nodeMap = new HashMap<String, FuzzyXMLNode>();
+
 		try {
 			WodParserCache cache = ((TemplateEditor) _editorInteraction.getHtmlDocumentProvider()).getSourceEditor().getParserCache();
 			FuzzyXMLDocument htmlDocument = cache.getHtmlXmlDocument();
 			RenderContext renderContext = new RenderContext(true);
-			renderContext.setDelegate(new PreviewRenderDelegate(cache));
 			FuzzyXMLElement documentElement = htmlDocument.getDocumentElement();
-			documentContents = documentElement.toXMLString(renderContext);
+			// renderContext.setDelegate(new PreviewRenderDelegate(cache));
+			// documentContents = documentElement.toXMLString(renderContext);
+			StringBuffer documentContentsBuffer = new StringBuffer();
+			documentContentsBuffer.append("<html><body>");
+			documentContentsBuffer.append("<style>");
+			documentContentsBuffer.append("body { font-family: Helvetica; font-size: 8pt; }");
+
+			documentContentsBuffer.append("a { text-decoration: none; }");
+
+			documentContentsBuffer.append("div.element { margin-top: 5px; margin-bottom: 10px; margin-right: 0px; padding: 0px; border: 1px solid rgb(230, 230, 230); border-right: none; }");
+			documentContentsBuffer.append("div.element.wo { border: 1px solid rgb(200, 200, 255); border-right: none; }");
+			documentContentsBuffer.append("div.element.document { margin: 0px; padding: 0px; border: none; }");
+
+			documentContentsBuffer.append("div.element div.summary { background-color: rgb(240, 240, 240); border-bottom: 1px solid rgb(230, 230, 230); padding: 3px; }");
+			documentContentsBuffer.append("div.element.wo div.summary { background-color: rgb(240, 240, 255); border-bottom: 1px solid rgb(200, 200, 255); }");
+			documentContentsBuffer.append("div.element.document div.summary { margin: 0px; padding: 0px; border: none; display: none; }");
+
+			documentContentsBuffer.append("div.element div.summary div.title { font-weight: bold; }");
+			documentContentsBuffer.append("div.element div.summary div.title span.type { font-size: 0.80em; color: rgb(150, 150, 150); }");
+			documentContentsBuffer.append("div.element div.summary div.title.nonwo { color: rgb(180, 180, 180); }");
+			documentContentsBuffer.append("div.element div.summary div.title.missing { font-style: italic; }");
+			documentContentsBuffer.append("div.element div.summary div.title.nonwo span.className { font-weight: bold; color: rgb(120, 120, 200); padding-left: 20px; display: none; }");
+
+			documentContentsBuffer.append("div.element div.summary table.bindings { font-family: Helvetica; font-size: 8pt; margin: 0px; padding: 0px; }");
+			documentContentsBuffer.append("div.element div.summary table.bindings th { text-align: right; font-weight: normal; color: rgb(220, 0, 0); padding-right: 3px; }");
+			documentContentsBuffer.append("div.element div.summary table.bindings td { color: rgb(150, 0, 0); }");
+			documentContentsBuffer.append("div.element div.summary table.bindings td.literal { color: rgb(0, 0, 150); }");
+
+			documentContentsBuffer.append("div.element div.contents { background-color: rgb(255, 255, 255); padding-left: 10px; padding-right: 0px; padding-top: 5px; padding-bottom: 5px; }");
+			documentContentsBuffer.append("div.element.document div.contents { margin: 0px; padding: 0px; border: none; }");
+			documentContentsBuffer.append("div.element.wo div.contents { background-color: rgb(250, 250, 255); }");
+
+			documentContentsBuffer.append("div.element div.summary:hover { border-color: rgb(210, 210, 210); }");
+			documentContentsBuffer.append("div.element div.summary:hover { background-color: rgb(220, 220, 220); }");
+			documentContentsBuffer.append("div.element.wo div.summary:hover { background-color: rgb(220, 220, 245); }");
+			documentContentsBuffer.append("div.element.wo div.summary:hover { border-color: rgb(170, 170, 225); }");
+
+			documentContentsBuffer.append("div.text { display: inline; }");
+
+			documentContentsBuffer.append("</style>");
+			renderElement(documentElement, renderContext, documentContentsBuffer, cache);
+			documentContentsBuffer.append("</body></html>");
+			documentContents = documentContentsBuffer.toString();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -106,6 +175,100 @@ public class HtmlPreviewEditor implements IEmbeddedEditor, IEmbeddedEditorSelect
 		boolean rendered = _browser.setText(documentContents);
 		if (!rendered) {
 			HtmlPreviewPlugin.getDefault().log("Can't create preview of component HTML.");
+		}
+	}
+
+	protected void renderElement(FuzzyXMLNode node, RenderContext renderContext, StringBuffer renderBuffer, WodParserCache cache) {
+		String nodeID = "node" + (_counter++);
+		_nodeMap.put(nodeID, node);
+		if (node instanceof FuzzyXMLElement) {
+			FuzzyXMLElement element = (FuzzyXMLElement) node;
+			String nodeName = element.getName();
+			String className = "element " + nodeName.replace(':', '_');
+
+			boolean woTag = WodHtmlUtils.isWOTag(nodeName);
+
+			if (woTag) {
+				className = className + " wo";
+			}
+
+			renderBuffer.append("<div id = \"" + nodeID + "\" class = \"" + className + "\">");
+
+			renderBuffer.append("<div class = \"summary\" onclick = \"window.status = 'open:" + nodeID + "'\">");
+
+			if (woTag) {
+				boolean wo54 = Activator.getDefault().getPreferenceStore().getBoolean(PreferenceConstants.WO54_KEY);
+				try {
+					IWodElement wodElement = WodHtmlUtils.getOrCreateWodElement(element, wo54, cache);
+					if (wodElement != null) {
+						if (WodHtmlUtils.isInline(nodeName)) {
+							renderBuffer.append("<div class = \"title\"><span class = \"nodeName\">" + wodElement.getElementType() + "</span></div>");
+						} else {
+							renderBuffer.append("<div class = \"title\"><span class = \"nodeName\">" + wodElement.getElementName() + "</span> : <span class = \"type\">" + wodElement.getElementType() + "</span></div>");
+						}
+						List<IWodBinding> wodBindings = wodElement.getBindings();
+						if (wodBindings.size() > 0) {
+							renderBuffer.append("<table class = \"bindings\">");
+							for (IWodBinding wodBinding : wodBindings) {
+								renderBuffer.append("<tr>");
+								renderBuffer.append("<th>" + wodBinding.getName() + "</th>");
+								String bindingClass;
+								if (wodBinding.isKeyPath()) {
+									bindingClass = "keypath";
+								} else {
+									bindingClass = "literal";
+								}
+								renderBuffer.append("<td class = \"" + bindingClass + "\">" + wodBinding.getValue() + "</td>");
+								renderBuffer.append("</tr>");
+							}
+							renderBuffer.append("</table>");
+						}
+					} else {
+						renderBuffer.append("<div class = \"title missing\">" + nodeName + "</div>");
+					}
+				} catch (Throwable t) {
+					// IGNORE
+					t.printStackTrace();
+				}
+			} else {
+				renderBuffer.append("<div class = \"title nonwo\"><span class = \"nodeName\">");
+				renderBuffer.append(nodeName);
+				renderBuffer.append("</span>");
+				String elementClass = element.getAttributeValue("class");
+				if (elementClass != null) {
+					renderBuffer.append("<span class = \"className\">" + elementClass + "</span>");
+				}
+				renderBuffer.append("</div>");
+			}
+
+			renderBuffer.append("</div>");
+
+			if ("script".equalsIgnoreCase(nodeName)) {
+				// don't show script
+			} else if ("style".equalsIgnoreCase(nodeName)) {
+				// don't show style
+			} else {
+				if (!element.isEmpty()) {
+					renderBuffer.append("<div class = \"contents\">");
+					FuzzyXMLNode[] children = element.getChildren();
+					for (FuzzyXMLNode child : children) {
+						renderElement(child, renderContext, renderBuffer, cache);
+					}
+					renderBuffer.append("</div>");
+				}
+			}
+
+			renderBuffer.append("</div>");
+		} else {
+			StringBuffer nodeBuffer = new StringBuffer();
+			node.toXMLString(renderContext, nodeBuffer);
+			String nodeStr = HTMLUtil.escapeHTML(nodeBuffer.toString());
+			boolean isText = (node instanceof FuzzyXMLText);
+			if (isText) {
+				renderBuffer.append("<div class = \"text\">" + nodeStr + "</div>");
+			} else {
+				renderBuffer.append(nodeStr);
+			}
 		}
 	}
 
@@ -132,7 +295,38 @@ public class HtmlPreviewEditor implements IEmbeddedEditor, IEmbeddedEditorSelect
 
 	public void createPartControl(Composite parent) {
 		_browser = new Browser(parent, SWT.READ_ONLY);
+		_browser.addStatusTextListener(this);
+		_browser.addLocationListener(new LocationListener() {
+			public void changed(LocationEvent event) {
+				// DO NOTHING
+			}
 
+			public void changing(LocationEvent event) {
+				if ("about:blank".equals(_browser.getUrl())) {
+					event.doit = false;
+				}
+			}
+		});
+	}
+
+	public void changed(StatusTextEvent event) {
+		String text = event.text;
+		int colonIndex = text.indexOf(':');
+		String command = text.substring(0, colonIndex);
+		String target = text.substring(colonIndex + 1);
+		if ("open".equals(command)) {
+			FuzzyXMLNode selectedNode = _nodeMap.get(target);
+			TemplateSourceEditor sourceEditor = ((TemplateEditor) _editorInteraction.getHtmlDocumentProvider()).getSourceEditor();
+			sourceEditor.getViewer().setSelectedRange(selectedNode.getOffset(), selectedNode.getLength());
+			sourceEditor.getViewer().revealRange(selectedNode.getOffset(), selectedNode.getLength());
+			try {
+				MultiPageEditorSite site = (MultiPageEditorSite)_site;
+				// Circular references be damned
+				site.getMultiPageEditor().getClass().getMethod("switchToHtml").invoke(site.getMultiPageEditor());
+			} catch (Throwable t) {
+				t.printStackTrace();
+			}
+		}
 	}
 
 	public IWorkbenchPartSite getSite() {
@@ -194,6 +388,5 @@ public class HtmlPreviewEditor implements IEmbeddedEditor, IEmbeddedEditorSelect
 	public EditorInteraction getEditorInteraction() {
 		return _editorInteraction;
 	}
-	
-	
+
 }
