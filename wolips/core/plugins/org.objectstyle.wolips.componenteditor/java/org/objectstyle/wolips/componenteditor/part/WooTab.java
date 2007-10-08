9cebe0ba834595a2315ba80635af27a700c08e3a
uli <uli@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-10-08 16:53:23 +0000

    Move woo to it's own tab.
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4369 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ wolips/core/plugins/org.objectstyle.wolips.componenteditor/java/org/objectstyle/wolips/componenteditor/part/WooTab.java
@@ -0,0 +1,106 @@
+/*
+ * ====================================================================
+ * 
+ * The ObjectStyle Group Software License, Version 1.0
+ * 
+ * Copyright (c) 2006 The ObjectStyle Group and individual authors of the
+ * software. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 1.
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer. 2. Redistributions in
+ * binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution. 3. The end-user documentation
+ * included with the redistribution, if any, must include the following
+ * acknowlegement: "This product includes software developed by the ObjectStyle
+ * Group (http://objectstyle.org/)." Alternately, this acknowlegement may
+ * appear in the software itself, if and wherever such third-party
+ * acknowlegements normally appear. 4. The names "ObjectStyle Group" and
+ * "Cayenne" must not be used to endorse or promote products derived from this
+ * software without prior written permission. For written permission, please
+ * contact andrus@objectstyle.org. 5. Products derived from this software may
+ * not be called "ObjectStyle" nor may "ObjectStyle" appear in their names
+ * without prior written permission of the ObjectStyle Group.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * OBJECTSTYLE GROUP OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ * 
+ * This software consists of voluntary contributions made by many individuals
+ * on behalf of the ObjectStyle Group. For more information on the ObjectStyle
+ * Group, please see <http://objectstyle.org/> .
+ *  
+ */
+package org.objectstyle.wolips.componenteditor.part;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.IPropertyListener;
+import org.eclipse.ui.PartInitException;
+import org.objectstyle.wolips.componenteditor.ComponenteditorPlugin;
+import org.objectstyle.wolips.wooeditor.editor.WooEditor;
+
+public class WooTab extends ComponentEditorTab {
+
+	private WooEditor wooEditor;
+
+	private IEditorInput wooInput;
+
+	public WooTab(ComponentEditorPart componentEditorPart, int tabIndex, IEditorInput wooInput) {
+		super(componentEditorPart, tabIndex);
+		this.wooInput = wooInput;
+	}
+
+	public IEditorPart getActiveEmbeddedEditor() {
+		return wooEditor;
+	}
+
+	public void createTab() {
+		wooEditor = new WooEditor();
+		IEditorSite apiSite = this.getComponentEditorPart().publicCreateSite(wooEditor);
+		try {
+			wooEditor.init(apiSite, wooInput);
+		} catch (PartInitException e) {
+			ComponenteditorPlugin.getDefault().log(e);
+		}
+		createInnerPartControl(this.getParentSashForm(), wooEditor);
+		wooEditor.addPropertyListener(new IPropertyListener() {
+			public void propertyChanged(Object source, int propertyId) {
+				WooTab.this.getComponentEditorPart().publicHandlePropertyChange(propertyId);
+			}
+		});
+	}
+
+	public void doSave(IProgressMonitor monitor) {
+		wooEditor.doSave(monitor);
+	}
+
+	public void close(boolean save) {
+		wooEditor.close(save);
+	}
+	
+	public void dispose() {
+		wooEditor.dispose();
+	}
+	
+	public IEditorInput getActiveEditorInput() {
+		return wooInput;
+	}
+
+	public boolean isDirty() {
+		return wooEditor.isDirty();
+	}
+
+}
