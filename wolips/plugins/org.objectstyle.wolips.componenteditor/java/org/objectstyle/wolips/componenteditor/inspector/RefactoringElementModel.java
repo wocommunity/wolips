4f720a64228ef65912ba5253e40476437db89922
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-10-10 07:04:17 +0000

    element type refactoring ... this is sluggish in the bindings view because it's doing error checking after every keystroke and it looks like the cache is getting blown too
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4402 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/plugins/org.objectstyle.wolips.componenteditor/java/org/objectstyle/wolips/componenteditor/inspector/RefactoringElementModel.java
+++ wolips/plugins/org.objectstyle.wolips.componenteditor/java/org/objectstyle/wolips/componenteditor/inspector/RefactoringElementModel.java
@@ -2,40 +2,59 @@ package org.objectstyle.wolips.componenteditor.inspector;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.lang.reflect.InvocationTargetException;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.objectstyle.wolips.bindings.wod.IWodElement;
+import org.objectstyle.wolips.bindings.wod.SimpleWodElement;
 import org.objectstyle.wolips.wodclipse.core.completion.WodParserCache;
+import org.objectstyle.wolips.wodclipse.core.refactoring.ChangeElementTypeRefactoring;
 import org.objectstyle.wolips.wodclipse.core.refactoring.RenameElementsRefactoring;
 
 public class RefactoringElementModel {
 	public static final String ELEMENT_NAME = "elementName";
 
+	public static final String ELEMENT_TYPE = "elementType";
+
 	private PropertyChangeSupport _propertyChange;
 
-	private String _elementName;
+	private SimpleWodElement _wodElement;
 
 	private WodParserCache _cache;
 
 	public RefactoringElementModel(IWodElement element, WodParserCache cache) {
 		_cache = cache;
-		_elementName = element.getElementName();
+		_wodElement = new SimpleWodElement(element);
 		_propertyChange = new PropertyChangeSupport(this);
 	}
+	
+	public SimpleWodElement getWodElement() {
+		return _wodElement;
+	}
 
-	public void setElementName(String elementName) {
-		String oldElementName = _elementName;
-		_elementName = elementName;
-		try {
+	public void setElementName(String elementName) throws CoreException, InvocationTargetException, InterruptedException {
+		String oldElementName = _wodElement.getElementName();
+		if (!_wodElement.isTemporary()) {
 			RenameElementsRefactoring.run(oldElementName, elementName, _cache, new NullProgressMonitor());
-			_propertyChange.firePropertyChange(RefactoringElementModel.ELEMENT_NAME, oldElementName, elementName);
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
+		_wodElement.setElementName(elementName);
+		_propertyChange.firePropertyChange(RefactoringElementModel.ELEMENT_NAME, oldElementName, elementName);
 	}
 
 	public String getElementName() {
-		return _elementName;
+		return _wodElement.getElementName();
+	}
+
+	public void setElementType(String elementType) throws CoreException, InvocationTargetException, InterruptedException {
+		String oldElementType = _wodElement.getElementType();
+		ChangeElementTypeRefactoring.run(elementType, _wodElement, _cache, new NullProgressMonitor());
+		_wodElement.setElementType(elementType);
+		_propertyChange.firePropertyChange(RefactoringElementModel.ELEMENT_TYPE, oldElementType, elementType);
+	}
+	
+	public String getElementType() {
+		return _wodElement.getElementType();
 	}
 
 	public void addPropertyChangeListener(PropertyChangeListener listener) {
