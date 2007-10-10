2ebffbb4c2f25bc7c6cd1a9aac523e6402264cab
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-10-10 07:04:23 +0000

    element type refactoring ... this is sluggish in the bindings view because it's doing error checking after every keystroke and it looks like the cache is getting blown too
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4403 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ wolips/core/plugins/org.objectstyle.wolips.wodclipse.core/java/org/objectstyle/wolips/wodclipse/core/refactoring/ChangeElementTypeRefactoring.java
@@ -0,0 +1,57 @@
+package org.objectstyle.wolips.wodclipse.core.refactoring;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.Position;
+import org.eclipse.text.edits.ReplaceEdit;
+import org.eclipse.text.edits.TextEdit;
+import org.objectstyle.wolips.bindings.wod.IWodElement;
+import org.objectstyle.wolips.wodclipse.core.completion.WodParserCache;
+import org.objectstyle.wolips.wodclipse.core.util.WodDocumentUtils;
+
+public class ChangeElementTypeRefactoring implements IRunnableWithProgress {
+  private IWodElement _element;
+  private String _newType;
+  private WodParserCache _cache;
+
+  public ChangeElementTypeRefactoring(String newType, IWodElement element, WodParserCache cache) {
+    _newType = newType;
+    _element = element;
+    _cache = cache;
+  }
+
+  public void run(IProgressMonitor monitor) throws InvocationTargetException {
+    try {
+      Position typePosition = _element.getElementTypePosition();
+      if (_element.isTemporary()) {
+        IDocument htmlDocument = _cache.getHtmlDocument();
+        if (htmlDocument != null) {
+          List<TextEdit> htmlEdits = new LinkedList<TextEdit>();
+          htmlEdits.add(new ReplaceEdit(typePosition.getOffset(), typePosition.getLength(), _newType));
+          WodDocumentUtils.applyEdits(htmlDocument, htmlEdits);
+        }
+      }
+      else {
+        IDocument wodDocument = _cache.getWodDocument();
+        if (wodDocument != null) {
+          List<TextEdit> wodEdits = new LinkedList<TextEdit>();
+          wodEdits.add(new ReplaceEdit(typePosition.getOffset(), typePosition.getLength(), _newType));
+          WodDocumentUtils.applyEdits(wodDocument, wodEdits);
+        }        
+      }
+    }
+    catch (Exception e) {
+      throw new InvocationTargetException(e, "Failed to refactor.");
+    }
+  }
+
+  public static void run(String newType, IWodElement element, WodParserCache cache, IProgressMonitor progressMonitor) throws CoreException, InvocationTargetException, InterruptedException {
+    TemplateRefactoring.processHtmlAndWod(new ChangeElementTypeRefactoring(newType, element, cache), cache, progressMonitor);
+  }
+}
