3bd4b9995cd4fa4cff3209c98b79aad7a04ae736
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-10-11 15:44:17 +0000

    overview rulers were missing in component editor ... finally tracked down why
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4419 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/core/plugins/org.objectstyle.wolips.tkhtmleditor/java/tk/eclipse/plugin/csseditor/editors/CSSEditor.java
+++ wolips/core/plugins/org.objectstyle.wolips.tkhtmleditor/java/tk/eclipse/plugin/csseditor/editors/CSSEditor.java
@@ -99,7 +99,7 @@ public class CSSEditor extends TextEditor {
 
   @Override
   protected ISourceViewer createSourceViewer(Composite parent, IVerticalRuler ruler, int styles) {
-    ISourceViewer viewer = new ProjectionViewer(parent, ruler, fOverviewRuler, true, styles);
+    ISourceViewer viewer = new ProjectionViewer(parent, ruler, getOverviewRuler(), true, styles);
     getSourceViewerDecorationSupport(viewer);
     viewer.getTextWidget().addVerifyListener(softTabListener);
     return viewer;
