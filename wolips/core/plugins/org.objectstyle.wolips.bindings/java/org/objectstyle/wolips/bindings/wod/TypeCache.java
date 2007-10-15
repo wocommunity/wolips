454a750d531a5cb0b48e0625c0178d54ac9f026a
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-10-15 06:11:33 +0000

    add a binding value key cache, but don't enable it just yet -- i think it accumulates tons of them and holds onto them for a long time
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4444 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/core/plugins/org.objectstyle.wolips.bindings/java/org/objectstyle/wolips/bindings/wod/TypeCache.java
+++ wolips/core/plugins/org.objectstyle.wolips.bindings/java/org/objectstyle/wolips/bindings/wod/TypeCache.java
@@ -1,19 +1,24 @@
 package org.objectstyle.wolips.bindings.wod;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.ITypeHierarchy;
 import org.eclipse.jdt.core.JavaModelException;
 import org.objectstyle.wolips.bindings.api.ApiCache;
+import org.objectstyle.wolips.bindings.utils.BindingReflectionUtils;
 import org.objectstyle.wolips.core.resources.types.SubTypeHierarchyCache;
 import org.objectstyle.wolips.core.resources.types.SuperTypeHierarchyCache;
 
 public class TypeCache {
   private ApiCache _apiCache;
 
-  private HashMap<IType, Map<String, IType>> _typeContextCache;
+  private Map<IType, Map<String, IType>> _typeContextCache;
+  
+  private Map<IType, Map<String, List<BindingValueKey>>> _bindingValueAccessorKeys;
 
   public TypeCache() {
     this(null);
@@ -21,12 +26,36 @@ public class TypeCache {
 
   public TypeCache(ApiCache apiCache) {
     _typeContextCache = new HashMap<IType, Map<String, IType>>();
+    _bindingValueAccessorKeys = new HashMap<IType, Map<String,List<BindingValueKey>>>();
     _apiCache = apiCache;
     if (_apiCache == null) {
       _apiCache = new ApiCache();
     }
   }
 
+  public List<BindingValueKey> getBindingValueAccessorKeys(IJavaProject javaProject, IType type, String name) throws JavaModelException {
+    List<BindingValueKey> bindingValueAccessorKeys = null;
+    Map<String, List<BindingValueKey>> bindingValueAccessorKeysForName = _bindingValueAccessorKeys.get(type);
+    if (bindingValueAccessorKeysForName == null) {
+      bindingValueAccessorKeysForName = new HashMap<String, List<BindingValueKey>>();
+      _bindingValueAccessorKeys.put(type, bindingValueAccessorKeysForName);
+    }
+    else {
+      bindingValueAccessorKeys = bindingValueAccessorKeysForName.get(name);
+    }
+    if (bindingValueAccessorKeys == null) {
+      //System.out.println("TypeCache.getBindingValueAccessorKeys: MISS " + type.getElementName() + ": " + name);
+      bindingValueAccessorKeys = BindingReflectionUtils.getBindingKeys(javaProject, type, name, true, BindingReflectionUtils.ACCESSORS_OR_VOID, this);
+      // MS: Don't cache this for now -- I don't know how many end up in here and how long they
+      // hang around, but I think the answer is "a lot" and "for a long time".  However, it's a huge performance win.
+      // bindingValueAccessorKeysForName.put(name, bindingValueAccessorKeys);
+    }
+    else {
+      //System.out.println("TypeCache.getBindingValueAccessorKeys: HIT  " + type.getElementName() + ": " + name);
+    }
+    return bindingValueAccessorKeys;
+  }
+  
   public ApiCache getApiCache() {
     return _apiCache;
   }
