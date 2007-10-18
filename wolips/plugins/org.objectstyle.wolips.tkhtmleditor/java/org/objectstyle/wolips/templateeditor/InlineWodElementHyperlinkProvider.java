22e71601c491fd09beda4b851f3e1dccc82e6bb1
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-10-18 05:00:55 +0000

    binding hyperlinks
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4494 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/plugins/org.objectstyle.wolips.tkhtmleditor/java/org/objectstyle/wolips/templateeditor/InlineWodElementHyperlinkProvider.java
+++ wolips/plugins/org.objectstyle.wolips.tkhtmleditor/java/org/objectstyle/wolips/templateeditor/InlineWodElementHyperlinkProvider.java
@@ -4,10 +4,14 @@ import jp.aonir.fuzzyxml.FuzzyXMLDocument;
 import jp.aonir.fuzzyxml.FuzzyXMLElement;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.text.Position;
+import org.eclipse.jface.text.Region;
 import org.objectstyle.wolips.bindings.Activator;
+import org.objectstyle.wolips.bindings.wod.IWodBinding;
 import org.objectstyle.wolips.bindings.wod.SimpleWodElement;
 import org.objectstyle.wolips.wodclipse.core.completion.WodParserCache;
-import org.objectstyle.wolips.wodclipse.core.document.WodHyperlink;
+import org.objectstyle.wolips.wodclipse.core.document.WodBindingValueHyperlink;
+import org.objectstyle.wolips.wodclipse.core.document.WodElementTypeHyperlink;
 import org.objectstyle.wolips.wodclipse.core.util.WodHtmlUtils;
 
 import tk.eclipse.plugin.htmleditor.IHyperlinkProvider;
@@ -18,14 +22,33 @@ public class InlineWodElementHyperlinkProvider implements IHyperlinkProvider {
     HTMLHyperlinkInfo hyperlinkInfo = null;
     try {
       if (WodHtmlUtils.isWOTag(element.getName()) && WodHtmlUtils.isInline(element.getName())) {
-        boolean wo54 = Activator.getDefault().isWO54();
-        WodParserCache cache;
-        cache = WodParserCache.parser(file);
-        SimpleWodElement wodElement = WodHtmlUtils.toWodElement(element, wo54, cache.getApiCache());
-        hyperlinkInfo = new HTMLHyperlinkInfo();
-        hyperlinkInfo.setOffset(wodElement.getElementTypePosition().getOffset());
-        hyperlinkInfo.setLength(wodElement.getElementTypePosition().getLength());
-        hyperlinkInfo.setObject(WodHyperlink.toWodHyperlink(wodElement, cache));
+        if (attrName == null) {
+          boolean wo54 = Activator.getDefault().isWO54();
+          WodParserCache cache = WodParserCache.parser(file);
+          SimpleWodElement wodElement = WodHtmlUtils.toWodElement(element, wo54, cache.getApiCache());
+          if (wodElement.isTypeWithin(new Region(offset, 0))) {
+            hyperlinkInfo = new HTMLHyperlinkInfo();
+            hyperlinkInfo.setOffset(wodElement.getElementTypePosition().getOffset());
+            hyperlinkInfo.setLength(wodElement.getElementTypePosition().getLength());
+            hyperlinkInfo.setObject(WodElementTypeHyperlink.toElementTypeHyperlink(wodElement, cache));
+          }
+        }
+        else {
+          boolean wo54 = Activator.getDefault().isWO54();
+          WodParserCache cache;
+          cache = WodParserCache.parser(file);
+          SimpleWodElement wodElement = WodHtmlUtils.toWodElement(element, wo54, cache.getApiCache());
+          IWodBinding wodBinding = wodElement.getBindingNamed(attrName);
+          if (wodBinding != null) {
+            Position valuePosition = wodBinding.getValuePosition();
+            if (valuePosition != null) {
+              hyperlinkInfo = new HTMLHyperlinkInfo();
+              hyperlinkInfo.setOffset(valuePosition.getOffset());
+              hyperlinkInfo.setLength(valuePosition.getLength());
+              hyperlinkInfo.setObject(WodBindingValueHyperlink.toBindingValueHyperlink(wodElement, attrName, cache));
+            }
+          }
+        }
       }
     }
     catch (Exception e) {
