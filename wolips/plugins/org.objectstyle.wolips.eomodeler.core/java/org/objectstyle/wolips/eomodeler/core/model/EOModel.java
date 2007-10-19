1c05b301fe1e5d046d0e548e30e44ae64aff801a
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-10-19 19:56:47 +0000

    debugdebugdebug
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4524 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/plugins/org.objectstyle.wolips.eomodeler.core/java/org/objectstyle/wolips/eomodeler/core/model/EOModel.java
+++ wolips/plugins/org.objectstyle.wolips.eomodeler.core/java/org/objectstyle/wolips/eomodeler/core/model/EOModel.java
@@ -716,6 +716,7 @@ public class EOModel extends UserInfoableEOModelObject<EOModelGroup> implements 
 	}
 
 	public void loadFromURL(URL _modelFolder, boolean createMissingDatabaseConfig, Set<EOModelVerificationFailure> _failures) throws EOModelException, IOException {
+		System.out.println("EOModel.loadFromURL: " + _modelFolder);
 		URL indexURL = new URL(_modelFolder, "index.eomodeld");
 		// if (!indexURL.exists()) {
 		// throw new EOModelException(indexURL + " does not exist.");
