c67621adfc4549096aaf5c3d0bf5ef7fc9972e2e
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-10-20 16:55:28 +0000

    open component action
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4541 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ wolips/plugins/org.objectstyle.wolips.componenteditor/java/org/objectstyle/wolips/componenteditor/actions/OpenComponentAction.java
@@ -0,0 +1,117 @@
+package org.objectstyle.wolips.componenteditor.actions;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.ui.JavaUI;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IActionDelegate2;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.dialogs.SelectionDialog;
+import org.objectstyle.wolips.locate.LocatePlugin;
+import org.objectstyle.wolips.locate.result.LocalizedComponentsLocateResult;
+import org.objectstyle.wolips.workbenchutilities.WorkbenchUtilitiesPlugin;
+
+public class OpenComponentAction extends Action implements IWorkbenchWindowActionDelegate, IActionDelegate2 {
+	private Object _selectedObject;
+
+	public OpenComponentAction() {
+		setText("Open Component");
+		setDescription("Open a Component");
+		setToolTipText("Open a Component");
+	}
+
+	public void run() {
+		runWithEvent(null);
+	}
+
+	public void runWithEvent(Event e) {
+		IJavaProject javaProject = null;
+		if (_selectedObject instanceof IJavaElement) {
+			IJavaElement javaElement = (IJavaElement) _selectedObject;
+			javaProject = javaElement.getJavaProject();
+		} else if (_selectedObject instanceof IResource) {
+			IProject project = ((IResource) _selectedObject).getProject();
+			javaProject = JavaCore.create(project);
+		} else {
+			IEditorPart editorPart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+			if (editorPart != null) {
+				IEditorInput editorInput = editorPart.getEditorInput();
+				if (editorInput instanceof IFileEditorInput) {
+					IFile file = ((IFileEditorInput) editorInput).getFile();
+					javaProject = JavaCore.create(file.getProject());
+				}
+			}
+		}
+
+		if (javaProject != null) {
+			Shell parent = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+			SelectionDialog dialog = new WOElementSelectionDialog(parent, javaProject, PlatformUI.getWorkbench().getProgressService());
+			dialog.setTitle("Open Component");
+			dialog.setMessage("Select a Component to Open");
+
+			int result = dialog.open();
+			if (result != IDialogConstants.OK_ID) {
+				return;
+			}
+			Object[] typeNames = dialog.getResult();
+			if (typeNames != null && typeNames.length > 0) {
+				for (int i = 0; i < typeNames.length; i++) {
+					String typeName = (String) typeNames[i];
+					try {
+						IType type = javaProject.findType(typeName);
+						JavaUI.openInEditor(type, true, true);
+						LocalizedComponentsLocateResult componentsLocateResults = LocatePlugin.getDefault().getLocalizedComponentsLocateResult(type.getUnderlyingResource());
+						IFile wodFile = componentsLocateResults.getFirstWodFile();
+						if (wodFile != null) {
+							WorkbenchUtilitiesPlugin.open(wodFile, "org.objectstyle.wolips.componenteditor.ComponentEditor");
+						}
+					} catch (Throwable e1) {
+						e1.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+
+	public void run(IAction action) {
+		run();
+	}
+
+	public void dispose() {
+		// DO NOTHING
+	}
+
+	public void init(IWorkbenchWindow window) {
+		// DO NOTHING
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		if (selection instanceof IStructuredSelection) {
+			_selectedObject = ((IStructuredSelection) selection).getFirstElement();
+		}
+	}
+
+	public void runWithEvent(IAction action, Event event) {
+		runWithEvent(event);
+	}
+
+	public void init(IAction action) {
+		// DO NOTHING
+	}
+}
--- wolips/plugins/org.objectstyle.wolips.componenteditor/java/org/objectstyle/wolips/componenteditor/actions/WOElementSelectionDialog.java
+++ wolips/plugins/org.objectstyle.wolips.componenteditor/java/org/objectstyle/wolips/componenteditor/actions/WOElementSelectionDialog.java
@@ -3,26 +3,26 @@ package org.objectstyle.wolips.componenteditor.actions;
 import java.lang.reflect.InvocationTargetException;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.search.IJavaSearchConstants;
-import org.eclipse.jdt.core.search.IJavaSearchScope;
-import org.eclipse.jdt.core.search.SearchEngine;
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.search.SearchPattern;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.operation.IRunnableContext;
 import org.eclipse.jface.operation.IRunnableWithProgress;
-import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.dialogs.ElementListSelectionDialog;
+import org.objectstyle.wolips.bindings.utils.BindingReflectionUtils;
+import org.objectstyle.wolips.componenteditor.ComponenteditorPlugin;
 import org.objectstyle.wolips.core.resources.types.TypeNameCollector;
-import org.objectstyle.wolips.wodclipse.core.completion.WodParserCache;
 
 public class WOElementSelectionDialog extends ElementListSelectionDialog {
-	private WodParserCache _cache;
+	private IJavaProject _javaProject;
 
 	private Point _location;
 
@@ -30,25 +30,23 @@ public class WOElementSelectionDialog extends ElementListSelectionDialog {
 
 	private IRunnableContext _context;
 
-	public WOElementSelectionDialog(Shell parent, WodParserCache cache, IRunnableContext context) {
-		super(parent, new LabelProvider());
-		_cache = cache;
+	public WOElementSelectionDialog(Shell parent, IJavaProject javaProject, IRunnableContext context) {
+		super(parent, new ComponentLabelProvider());
+		_javaProject = javaProject;
 		_context = context;
 		setIgnoreCase(true);
 		setMultipleSelection(false);
+		setAllowDuplicates(false);
 	}
 
 	public int open() {
 		try {
-			final TypeNameCollector typeNameCollector = new TypeNameCollector(JavaCore.create(_cache.getProject()), false);
+			final TypeNameCollector typeNameCollector = new TypeNameCollector(_javaProject, false);
 
 			IRunnableWithProgress runnable = new IRunnableWithProgress() {
 				public void run(final IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
 					try {
-					    SearchEngine searchEngine = new SearchEngine();
-					    IJavaSearchScope searchScope = SearchEngine.createWorkspaceScope();
-					    searchEngine.searchAllTypeNames(null, null, SearchPattern.R_PREFIX_MATCH, IJavaSearchConstants.CLASS, searchScope, typeNameCollector, IJavaSearchConstants.WAIT_UNTIL_READY_TO_SEARCH, monitor);
-						System.out.println(".run: " + typeNameCollector.getTypeNames());
+						BindingReflectionUtils.findMatchingElementClassNames("", SearchPattern.R_PREFIX_MATCH, typeNameCollector, monitor);
 					} catch (Exception e) {
 						e.printStackTrace();
 						throw new InvocationTargetException(e);
@@ -119,5 +117,43 @@ public class WOElementSelectionDialog extends ElementListSelectionDialog {
 			}
 		}
 		return result;
+	}
+
+	public static class ComponentLabelProvider implements ILabelProvider {
+		private static Image _componentImage;
+
+		public ComponentLabelProvider() {
+			_componentImage = ComponenteditorPlugin.getImageDescriptor("icons/ComponentEditor.png").createImage();
+		}
+
+		public Image getImage(Object element) {
+			return _componentImage;
+		}
+
+		public String getText(Object element) {
+			String str = (String) element;
+			int dotIndex = str.lastIndexOf('.');
+			if (dotIndex != -1) {
+				str = str.substring(dotIndex + 1);
+			}
+			return str;
+		}
+
+		public void addListener(ILabelProviderListener listener) {
+			// DO NOTHING
+		}
+
+		public void dispose() {
+			_componentImage.dispose();
+		}
+
+		public boolean isLabelProperty(Object element, String property) {
+			return false;
+		}
+
+		public void removeListener(ILabelProviderListener listener) {
+			// DO NOTHING
+		}
+
 	}
 }
