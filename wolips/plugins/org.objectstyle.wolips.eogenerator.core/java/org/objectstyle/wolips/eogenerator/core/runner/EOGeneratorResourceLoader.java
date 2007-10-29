894d218bf7eb8a13b8ed4a501712bef54996d142
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-10-29 21:38:03 +0000

    temporary workaround for EOGenerator
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4564 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ wolips/plugins/org.objectstyle.wolips.eogenerator.core/java/org/objectstyle/wolips/eogenerator/core/runner/EOGeneratorResourceLoader.java
@@ -0,0 +1,10 @@
+package org.objectstyle.wolips.eogenerator.core.runner;
+
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+
+public class EOGeneratorResourceLoader extends ClasspathResourceLoader {
+	public EOGeneratorResourceLoader() {
+		super();
+	}
+
+}
