6a265c43e00f1fcad2795b339ea848709e6d38d1
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-11-03 17:37:59 +0000

    WOL-624 + clean up layout
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4580 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/core/plugins/org.objectstyle.wolips.eomodeler/java/org/objectstyle/wolips/eomodeler/editors/userInfo/BrowserTextObservableValue.java
+++ wolips/core/plugins/org.objectstyle.wolips.eomodeler/java/org/objectstyle/wolips/eomodeler/editors/userInfo/BrowserTextObservableValue.java
@@ -8,15 +8,20 @@ public class BrowserTextObservableValue extends AbstractSWTObservableValue {
 	private final Browser _browser;
 
 	private String _text;
+	private String _defaultStyle;
 
-	public BrowserTextObservableValue(Browser browser) {
+	public BrowserTextObservableValue(Browser browser, String defaultStyle) {
 		super(browser);
 		_browser = browser;
+		_defaultStyle = defaultStyle;
 	}
 
 	public void doSetValue(final Object value) {
 		String oldValue = _text;
 		String newValue = value == null ? "" : value.toString();
+		if (_defaultStyle != null) {
+			newValue += "<style>" + _defaultStyle + "</style>";
+		}
 		_browser.setText(newValue); //$NON-NLS-1$
 		_text = newValue;
 		fireValueChange(Diffs.createValueDiff(oldValue, newValue));
