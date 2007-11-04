4706c71ad310990a511e46a92875709a397bc772
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2007-11-04 13:51:28 +0000

    Sort API files together with WO components rather than treating then like normal files and folders.  Also decorate the label for API files to be "ComponentName API" rather than ComponentName.api like a normal file -- makes them fit in better
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4594 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/plugins/org.objectstyle.wolips.jdt/java/org/objectstyle/wolips/jdt/ui/WOPackageExplorerPart.java
+++ wolips/plugins/org.objectstyle.wolips.jdt/java/org/objectstyle/wolips/jdt/ui/WOPackageExplorerPart.java
@@ -5,6 +5,8 @@ import org.eclipse.jdt.internal.ui.packageview.PackageExplorerPart;
 import org.eclipse.jdt.internal.ui.workingsets.ViewActionGroup;
 import org.eclipse.jdt.ui.PreferenceConstants;
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.widgets.Composite;
 
 public class WOPackageExplorerPart extends PackageExplorerPart {
 	@Override
@@ -15,5 +17,27 @@ public class WOPackageExplorerPart extends PackageExplorerPart {
 			return new WOPackageExplorerContentProvider(showCUChildren);
 		}
 		return new WOWorkingSetAwareContentProvider(showCUChildren, getWorkingSetModel());
+	}
+	
+	@Override
+	public void createPartControl(Composite parent) {
+		super.createPartControl(parent);
+		switchToWOSorter();
+	}
+
+	@Override
+	public void rootModeChanged(int newMode) {
+		super.rootModeChanged(newMode);
+		switchToWOSorter();
+	}
+
+	protected void switchToWOSorter() {
+		TreeViewer viewer = getTreeViewer();
+		boolean showWorkingSets = (getRootMode() == ViewActionGroup.SHOW_WORKING_SETS);
+		if (showWorkingSets) {
+			viewer.setComparator(new WOWorkingSetAwareJavaElementSorter());
+		} else {
+			viewer.setComparator(new WOJavaElementComparator());
+		}
 	}
 }
