a4b6533ae57451680131610709240f1fd933d13f
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2008-01-22 22:05:06 +0000

    added eoqualifier bindings
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4772 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ wolips/plugins/org.objectstyle.wolips.eomodeler.core/java/org/objectstyle/wolips/eomodeler/core/model/qualifier/EOQualifierBinding.java
@@ -0,0 +1,49 @@
+package org.objectstyle.wolips.eomodeler.core.model.qualifier;
+
+import org.objectstyle.wolips.eomodeler.core.model.AbstractEOAttributePath;
+import org.objectstyle.wolips.eomodeler.core.model.EOEntity;
+import org.objectstyle.wolips.eomodeler.core.model.ISortableEOModelObject;
+
+/**
+ * EOQualifierBindings represents the binding of a $qualifierVariable to a
+ * particular keypath of an entity.
+ * 
+ * @author mschrag
+ */
+public class EOQualifierBinding implements ISortableEOModelObject {
+	private EOEntity _entity;
+
+	private String _name;
+
+	private String _keyPath;
+
+	public EOQualifierBinding(EOEntity entity, String name, String keyPath) {
+		_entity = entity;
+		_name = name;
+		_keyPath = keyPath;
+	}
+
+	public EOEntity getEntity() {
+		return _entity;
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public void setName(String name) {
+		_name = name;
+	}
+
+	public String getKeyPath() {
+		return _keyPath;
+	}
+
+	public AbstractEOAttributePath getAttributePath() {
+		return _entity.resolveKeyPath(_keyPath);
+	}
+
+	public String toString() {
+		return "[EOQualifierBinding: " + _keyPath + " = $" + _name + "]";
+	}
+}
