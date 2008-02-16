518f034b7c3d4b3588eece2b240b692889c4d993
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2008-02-16 03:22:52 +0000

    Fix for NPE when switching from Java editor to ComponentEditor
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4794 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/plugins/org.objectstyle.wolips.tkhtmleditor/java/org/objectstyle/wolips/templateeditor/TemplateEditor.java
+++ wolips/plugins/org.objectstyle.wolips.tkhtmleditor/java/org/objectstyle/wolips/templateeditor/TemplateEditor.java
@@ -2,8 +2,29 @@ package org.objectstyle.wolips.templateeditor;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.content.IContentDescriber;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IMenuListener;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.revisions.RevisionInformation;
+import org.eclipse.jface.text.source.Annotation;
+import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IMemento;
+import org.eclipse.ui.INavigationLocation;
+import org.eclipse.ui.INavigationLocationProvider;
+import org.eclipse.ui.IPersistableEditor;
+import org.eclipse.ui.IReusableEditor;
+import org.eclipse.ui.ISaveablesSource;
+import org.eclipse.ui.Saveable;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+import org.eclipse.ui.texteditor.IStatusField;
+import org.eclipse.ui.texteditor.ITextEditor;
+import org.eclipse.ui.texteditor.ITextEditorExtension;
+import org.eclipse.ui.texteditor.ITextEditorExtension2;
+import org.eclipse.ui.texteditor.ITextEditorExtension3;
+import org.eclipse.ui.texteditor.ITextEditorExtension4;
 import org.objectstyle.wolips.components.editor.ComponentEditorInteraction;
 import org.objectstyle.wolips.components.editor.IEmbeddedEditor;
 import org.objectstyle.wolips.components.editor.IHtmlDocumentProvider;
@@ -16,7 +37,7 @@ import tk.eclipse.plugin.htmleditor.editors.HTMLEditor;
 import tk.eclipse.plugin.htmleditor.editors.HTMLEditorPart;
 import tk.eclipse.plugin.htmleditor.editors.HTMLSourceEditor;
 
-public class TemplateEditor extends HTMLEditor implements IEmbeddedEditor, IHtmlDocumentProvider, IWebobjectTagListener {
+public class TemplateEditor extends HTMLEditor implements IEmbeddedEditor, IHtmlDocumentProvider, IWebobjectTagListener, ITextEditor, IReusableEditor, ITextEditorExtension, ITextEditorExtension2, ITextEditorExtension3, ITextEditorExtension4, INavigationLocationProvider, ISaveablesSource, IPersistableEditor {
   private TemplateConfiguration _configuration;
   private ComponentEditorInteraction _editorInteraction;
 
@@ -31,6 +52,11 @@ public class TemplateEditor extends HTMLEditor implements IEmbeddedEditor, IHtml
     TemplateTripleClickAdapter tripleClickAdapter = new TemplateTripleClickAdapter(this);
     getSourceEditor().getViewer().getTextWidget().addMouseListener(tripleClickAdapter);
     getSourceEditor().getViewer().getTextWidget().addMouseMoveListener(tripleClickAdapter);
+  }
+  
+  @Override
+  public void setInput(IEditorInput input) {
+    super.setInput(input);
   }
 
   public TemplateSourceEditor getSourceEditor() {
@@ -82,11 +108,11 @@ public class TemplateEditor extends HTMLEditor implements IEmbeddedEditor, IHtml
       _editorInteraction.fireWebObjectChanged();
     }
   }
-  
+
   public void webObjectChanged() {
     getSourceEditor().getTemplateOutlinePage().update();
   }
-  
+
   public void webObjectTagSelected(String name) {
     // DO NOTHING
   }
@@ -132,5 +158,137 @@ public class TemplateEditor extends HTMLEditor implements IEmbeddedEditor, IHtml
     //      HTMLPlugin.logException(ex);
     //      //ex.printStackTrace();
     //    }
+  }
+
+  public void close(boolean save) {
+    getSourceEditor().close(save);
+  }
+
+  public void doRevertToSaved() {
+    getSourceEditor().doRevertToSaved();
+  }
+
+  public IAction getAction(String actionId) {
+    return getSourceEditor().getAction(actionId);
+  }
+
+  public IDocumentProvider getDocumentProvider() {
+    return getSourceEditor().getDocumentProvider();
+  }
+
+  public IRegion getHighlightRange() {
+    return getSourceEditor().getHighlightRange();
+  }
+
+  public ISelectionProvider getSelectionProvider() {
+    return getSourceEditor().getSelectionProvider();
+  }
+
+  public boolean isEditable() {
+    return getSourceEditor().isEditable();
+  }
+
+  public void removeActionActivationCode(String actionId) {
+    getSourceEditor().removeActionActivationCode(actionId);
+  }
+
+  public void resetHighlightRange() {
+    getSourceEditor().resetHighlightRange();
+  }
+
+  public void selectAndReveal(int offset, int length) {
+    getSourceEditor().selectAndReveal(offset, length);
+  }
+
+  public void setAction(String actionID, IAction action) {
+    getSourceEditor().setAction(actionID, action);
+  }
+
+  public void setActionActivationCode(String actionId, char activationCharacter, int activationKeyCode, int activationStateMask) {
+    getSourceEditor().setActionActivationCode(actionId, activationCharacter, activationKeyCode, activationStateMask);
+  }
+
+  public void setHighlightRange(int offset, int length, boolean moveCursor) {
+    getSourceEditor().setHighlightRange(offset, length, moveCursor);
+  }
+
+  public void showHighlightRangeOnly(boolean showHighlightRangeOnly) {
+    getSourceEditor().showHighlightRangeOnly(showHighlightRangeOnly);
+  }
+
+  public boolean showsHighlightRangeOnly() {
+    return getSourceEditor().showsHighlightRangeOnly();
+  }
+
+  public void addRulerContextMenuListener(IMenuListener listener) {
+    getSourceEditor().addRulerContextMenuListener(listener);
+  }
+
+  public boolean isEditorInputReadOnly() {
+    return getSourceEditor().isEditorInputReadOnly();
+  }
+
+  public void removeRulerContextMenuListener(IMenuListener listener) {
+    getSourceEditor().removeRulerContextMenuListener(listener);
+  }
+
+  public void setStatusField(IStatusField field, String category) {
+    getSourceEditor().setStatusField(field, category);
+  }
+
+  public boolean isEditorInputModifiable() {
+    return getSourceEditor().isEditorInputModifiable();
+  }
+
+  public boolean validateEditorInputState() {
+    return getSourceEditor().validateEditorInputState();
+  }
+
+  public InsertMode getInsertMode() {
+    return getSourceEditor().getInsertMode();
+  }
+
+  public boolean isChangeInformationShowing() {
+    return getSourceEditor().isChangeInformationShowing();
+  }
+
+  public void setInsertMode(InsertMode mode) {
+    getSourceEditor().setInsertMode(mode);
+  }
+
+  public void showChangeInformation(boolean show) {
+    getSourceEditor().showChangeInformation(show);
+  }
+
+  public Annotation gotoAnnotation(boolean forward) {
+    return getSourceEditor().gotoAnnotation(forward);
+  }
+
+  public void showRevisionInformation(RevisionInformation info, String quickDiffProviderId) {
+    getSourceEditor().showRevisionInformation(info, quickDiffProviderId);
+  }
+
+  public INavigationLocation createEmptyNavigationLocation() {
+    return getSourceEditor().createEmptyNavigationLocation();
+  }
+
+  public INavigationLocation createNavigationLocation() {
+    return getSourceEditor().createNavigationLocation();
+  }
+
+  public Saveable[] getActiveSaveables() {
+    return getSourceEditor().getActiveSaveables();
+  }
+
+  public Saveable[] getSaveables() {
+    return getSourceEditor().getSaveables();
+  }
+
+  public void restoreState(IMemento memento) {
+    getSourceEditor().restoreState(memento);
+  }
+
+  public void saveState(IMemento memento) {
+    getSourceEditor().saveState(memento);
   }
 }
