8cbb60b5deb00205087aadd7d36d722440b92f3b
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2008-02-25 04:17:19 +0000

    define an Actions constant
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4871 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/core/plugins/org.objectstyle.wolips.bindings/java/org/objectstyle/wolips/bindings/api/IApiBinding.java
+++ wolips/core/plugins/org.objectstyle.wolips.bindings/java/org/objectstyle/wolips/bindings/api/IApiBinding.java
@@ -6,17 +6,18 @@ import org.eclipse.jdt.core.JavaModelException;
 import org.objectstyle.wolips.bindings.wod.TypeCache;
 
 public interface IApiBinding {
-	public final static String[] ALL_DEFAULTS = new String[] { "Undefined", "Actions", "Boolean", "YES/NO", "Date Format Strings", "Number Format Strings", "MIME Types", "Direct Actions", "Direct Action Classes", "Page Names", "Frameworks", "Resources" };
+  public static final String ACTIONS_DEFAULT = "Actions";
+  public static final String[] ALL_DEFAULTS = new String[] { "Undefined", IApiBinding.ACTIONS_DEFAULT, "Boolean", "YES/NO", "Date Format Strings", "Number Format Strings", "MIME Types", "Direct Actions", "Direct Action Classes", "Page Names", "Frameworks", "Resources" };
 
-	public String getName();
+  public String getName();
 
-	public int getSelectedDefaults();
+  public int getSelectedDefaults();
 
-	public String getDefaults();
+  public String getDefaults();
 
-	public boolean isRequired();
+  public boolean isRequired();
 
-	public boolean isWillSet();
+  public boolean isWillSet();
 
-	public String[] getValidValues(String partialValue, IJavaProject javaProject, IType componentType, TypeCache typeCache) throws JavaModelException;
+  public String[] getValidValues(String partialValue, IJavaProject javaProject, IType componentType, TypeCache typeCache) throws JavaModelException;
 }
