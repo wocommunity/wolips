56a49478d6f82c088fb567f3626be2ec1f48cdaf
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2008-03-05 13:33:01 +0000

    updated eomodeldoc package script
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@4955 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/eomodeldoc/package.sh
+++ wolips/eomodeldoc/package.sh
@@ -1,38 +1,32 @@
 WORKSPACE=/Users/mschrag/Documents/workspace
 ECLIPSE=/Developer/Applications/eclipse
 TEMPDIR=/tmp/eomodeldocjar
-OUTPUT=/tmp
+MANIFEST=/tmp/MANIFEST.MF
+OUTPUT=/tmp/eomodeldoc.jar
 
 mkdir -p $TEMPDIR
-cp -r $WORKSPACE/org.objectstyle.wolips.eomodeler/bin/* $TEMPDIR
-cp -r $WORKSPACE/org.objectstyle.wolips.eomodeler.core/bin/* $TEMPDIR
-cp -r $WORKSPACE/org.objectstyle.wolips.eomodeler.eclipse/bin/* $TEMPDIR
-cp -r $WORKSPACE/org.objectstyle.wolips.thirdparty.cayenne/bin/* $TEMPDIR
-cp -r $WORKSPACE/org.objectstyle.wolips.baseforplugins/bin/* $TEMPDIR
-cp -r $WORKSPACE/woproject/wolips/eomodeldoc/bin/* $TEMPDIR
-cp -r $WORKSPACE/woproject/wolips/core/plugins/org.objectstyle.wolips.eomodeler.doc/bin/* $TEMPDIR
-cp -r $WORKSPACE/woproject/wolips/core/plugins/org.objectstyle.wolips.eomodeler.doc/bin/* $TEMPDIR
-cp -r $WORKSPACE/woproject/wolips/core/plugins/org.objectstyle.wolips.eomodeler.factories/bin/* $TEMPDIR
-cp -r $WORKSPACE/woproject/wolips/core/plugins/org.objectstyle.wolips.eomodeler.factories/bin/* $TEMPDIR
 cd $TEMPDIR
 
-cp $WORKSPACE/org.objectstyle.wolips.woproject/lib/woproject.jar $TEMPDIR
-jar xvf woproject.jar
-cp $ECLIPSE/plugins/org.eclipse.equinox.common_3.3.0.v20070426.jar $TEMPDIR
-jar xvf org.eclipse.equinox.common_3.3.0.v20070426.jar
-cp $ECLIPSE/plugins/org.eclipse.core.resources_3.3.0.v20070604.jar $TEMPDIR
-jar xvf org.eclipse.core.resources_3.3.0.v20070604.jar
-cp $ECLIPSE/plugins/org.eclipse.core.jobs_3.3.1.R33x_v20070709.jar $TEMPDIR
-jar xvf org.eclipse.core.jobs_3.3.1.R33x_v20070709.jar
-cp $WORKSPACE/org.objectstyle.wolips.thirdparty.cayenne/lib/cayenne-1.2M12.jar $TEMPDIR
-jar xvf cayenne-1.2M12.jar
+cp -r $WORKSPACE/org.objectstyle.wolips.eomodeler/bin/* .
+cp -r $WORKSPACE/org.objectstyle.wolips.eomodeler.core/bin/* .
+cp -r $WORKSPACE/org.objectstyle.wolips.eomodeler.eclipse/bin/* .
+cp -r $WORKSPACE/org.objectstyle.wolips.thirdparty.cayenne/bin/* .
+cp -r $WORKSPACE/org.objectstyle.wolips.baseforplugins/bin/* .
+cp -r $WORKSPACE/eomodeldoc/bin/* .
+cp -r $WORKSPACE/org.objectstyle.wolips.eomodeler.doc/bin/* .
+cp -r $WORKSPACE/org.objectstyle.wolips.eomodeler.factories/bin/* .
 
-rm *.jar
+jar xvf $ECLIPSE/plugins/org.eclipse.equinox.common_3.3.0.v20070426.jar
+jar xvf $ECLIPSE/plugins/org.eclipse.core.resources_3.3.1.R33x_v20080205.jar
+jar xvf $ECLIPSE/plugins/org.eclipse.core.jobs_3.3.1.R33x_v20070709.jar
+jar xvf $WORKSPACE/org.objectstyle.wolips.woproject/lib/woproject.jar
+jar xvf $WORKSPACE/org.objectstyle.wolips.thirdparty.cayenne/lib/cayenne-1.2M12.jar
+
 rm -rf META-INF
-rm /tmp/MANIFEST.MF
-echo Manifest-Version: 1.0 >> /tmp/MANIFEST.MF
-echo Main-Class: eomodeldoc.EOModelDoc >> /tmp/MANIFEST.MF
+rm $MANIFEST
+echo Manifest-Version: 1.0 >> $MANIFEST
+echo Main-Class: eomodeldoc.EOModelDoc >> $MANIFEST
 
-rm $OUTPUT/eomodeldoc.jar
-jar cvfm $OUTPUT/eomodeldoc.jar /tmp/MANIFEST.MF *
-rm /tmp/MANIFEST.MF
+rm $OUTPUT
+jar cvfm $OUTPUT $MANIFEST *
+rm $MANIFEST
