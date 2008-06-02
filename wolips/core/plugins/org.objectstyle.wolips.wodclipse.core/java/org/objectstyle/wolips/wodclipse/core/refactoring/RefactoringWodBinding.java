cc4303f2592ae6d9d5eb0ba03902f1a71fbf7007
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2008-06-02 03:25:06 +0000

    this looks like i did a lot more than i did because of reformatting, I think, but mostly just added support for 5.4 syntax when refacatoring bindings
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@5204 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/core/plugins/org.objectstyle.wolips.wodclipse.core/java/org/objectstyle/wolips/wodclipse/core/refactoring/RefactoringWodBinding.java
+++ wolips/core/plugins/org.objectstyle.wolips.wodclipse.core/java/org/objectstyle/wolips/wodclipse/core/refactoring/RefactoringWodBinding.java
@@ -7,108 +7,119 @@ import java.lang.reflect.InvocationTargetException;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.objectstyle.wolips.baseforplugins.util.ComparisonUtils;
+import org.objectstyle.wolips.bindings.Activator;
 import org.objectstyle.wolips.bindings.wod.IWodBinding;
 import org.objectstyle.wolips.bindings.wod.IWodElement;
 import org.objectstyle.wolips.bindings.wod.SimpleWodBinding;
 import org.objectstyle.wolips.wodclipse.core.completion.WodParserCache;
 
 public class RefactoringWodBinding {
-	public static final String BINDING_NAME = "bindingName";
+  public static final String BINDING_NAME = "bindingName";
 
-	public static final String BINDING_VALUE = "bindingValue";
+  public static final String BINDING_VALUE = "bindingValue";
 
-	private PropertyChangeSupport _propertyChange;
+  private PropertyChangeSupport _propertyChange;
 
-	private IWodElement _wodElement;
+  private IWodElement _wodElement;
 
-	private SimpleWodBinding _wodBinding;
+  private SimpleWodBinding _wodBinding;
 
-	private WodParserCache _cache;
+  private WodParserCache _cache;
 
-	public RefactoringWodBinding(IWodElement element, IWodBinding binding, WodParserCache cache) {
-		_cache = cache;
-		_wodElement = element;
-		_wodBinding = new SimpleWodBinding(binding);
-		_propertyChange = new PropertyChangeSupport(this);
-	}
+  public RefactoringWodBinding(IWodElement element, IWodBinding binding, WodParserCache cache) {
+    _cache = cache;
+    _wodElement = element;
+    _wodBinding = new SimpleWodBinding(binding);
+    _propertyChange = new PropertyChangeSupport(this);
+  }
 
-	public SimpleWodBinding getWodBinding() {
-		return _wodBinding;
-	}
+  public SimpleWodBinding getWodBinding() {
+    return _wodBinding;
+  }
 
-	public void setValue(String value) throws CoreException, InvocationTargetException, InterruptedException {
-		String oldValue = _wodBinding.getValue();
-		String newValue = _changeValue(oldValue, value);
-		_propertyChange.firePropertyChange(RefactoringWodBinding.BINDING_VALUE, oldValue, newValue);
-	}
+  public void setValue(String value) throws CoreException, InvocationTargetException, InterruptedException {
+    String oldValue = _wodBinding.getValue();
+    String newValue = _changeValue(oldValue, value);
+    _propertyChange.firePropertyChange(RefactoringWodBinding.BINDING_VALUE, oldValue, newValue);
+  }
 
-	public String _setValue(String value) throws CoreException, InvocationTargetException, InterruptedException {
-		return _changeValue(null, value);
-	}
+  public String _setValue(String value) throws CoreException, InvocationTargetException, InterruptedException {
+    return _changeValue(null, value);
+  }
 
-	public String _changeValue(String oldValue, String value) throws CoreException, InvocationTargetException, InterruptedException {
-		String newValue = value;
-		if (!ComparisonUtils.equals(oldValue, newValue, true)) {
-			newValue = newValue.trim();
-			
-			// Close missing quotes
-			if (newValue.startsWith("\"") && !newValue.endsWith("\"")) {
-				newValue = newValue + "\"";
-			}
-			
-			// Make non-string literals with spaces of %'s into literals (as long as there isn't a helper)
-			if (!newValue.startsWith("\"") && newValue.indexOf('|') == -1 && newValue.matches(".*[ %].*")) {
-				newValue = "\"" + newValue + "\"";
-			}
-			
-			// Process values for inline bindings
-			if (_wodElement.isInline()) {
-				if (newValue.startsWith("\"")) {
-					newValue = newValue.substring(1, newValue.length() - 1);
-				} else if (!newValue.startsWith("$")) {
-					newValue = "$" + value;
-				}
-			}
-			
-			ChangeBindingValueRefactoring.run(newValue, _wodElement, _wodBinding, _cache, new NullProgressMonitor());
-			_wodBinding.setValue(newValue);
-		}
-		return newValue;
-	}
+  public static String toBindingValue(boolean inline, boolean wo54, String value) {
+    String newValue = value;
+    newValue = newValue.trim();
 
-	public String getValue() {
-		return _wodBinding.getValue();
-	}
+    // Close missing quotes
+    if (newValue.startsWith("\"") && !newValue.endsWith("\"")) {
+      newValue = newValue + "\"";
+    }
 
-	public String getName() {
-		return _wodBinding.getName();
-	}
+    // Make non-string literals with spaces of %'s into literals (as long as there isn't a helper)
+    if (!newValue.startsWith("\"") && newValue.indexOf('|') == -1 && newValue.matches(".*[ %].*")) {
+      newValue = "\"" + newValue + "\"";
+    }
 
-	public void setName(String name) throws CoreException, InvocationTargetException, InterruptedException {
-		String oldName = _wodBinding.getName();
-		if (name == null || name.length() == 0) {
-		  name = RefactoringWodElement.findUnusedBindingName(_wodElement, name);
-		}
-		if (!ComparisonUtils.equals(name, oldName)) {
-  		ChangeBindingNameRefactoring.run(name, _wodElement, _wodBinding, _cache, new NullProgressMonitor());
-  		_wodBinding.setName(name);
-  		_propertyChange.firePropertyChange(RefactoringWodBinding.BINDING_NAME, oldName, name);
-		}
-	}
+    // Process values for inline bindings
+    if (inline) {
+      if (newValue.startsWith("\"")) {
+        newValue = newValue.substring(1, newValue.length() - 1);
+      }
+      else if (!wo54 && !newValue.startsWith("$")) {
+        newValue = "$" + value;
+      }
+      else if (wo54 && !newValue.startsWith("[")) {
+        newValue = "[" + value + "]";
+      }
+    }
+    
+    return newValue;
+  }
 
-	public void addPropertyChangeListener(PropertyChangeListener listener) {
-		_propertyChange.addPropertyChangeListener(listener);
-	}
+  public String _changeValue(String oldValue, String value) throws CoreException, InvocationTargetException, InterruptedException {
+    String newValue = value;
+    if (!ComparisonUtils.equals(oldValue, newValue, true)) {
+      newValue = RefactoringWodBinding.toBindingValue(_wodElement.isInline(), Activator.getDefault().isWO54(), newValue);
+      ChangeBindingValueRefactoring.run(newValue, _wodElement, _wodBinding, _cache, new NullProgressMonitor());
+      _wodBinding.setValue(newValue);
+    }
+    return newValue;
+  }
 
-	public void addPropertyChangeListener(String propertyName, PropertyChangeListener listener) {
-		_propertyChange.addPropertyChangeListener(propertyName, listener);
-	}
+  public String getValue() {
+    return _wodBinding.getValue();
+  }
 
-	public void removePropertyChangeListener(PropertyChangeListener listener) {
-		_propertyChange.removePropertyChangeListener(listener);
-	}
+  public String getName() {
+    return _wodBinding.getName();
+  }
 
-	public void removePropertyChangeListener(String propertyName, PropertyChangeListener listener) {
-		_propertyChange.removePropertyChangeListener(propertyName, listener);
-	}
+  public void setName(String name) throws CoreException, InvocationTargetException, InterruptedException {
+    String oldName = _wodBinding.getName();
+    if (name == null || name.length() == 0) {
+      name = RefactoringWodElement.findUnusedBindingName(_wodElement, name);
+    }
+    if (!ComparisonUtils.equals(name, oldName)) {
+      ChangeBindingNameRefactoring.run(name, _wodElement, _wodBinding, _cache, new NullProgressMonitor());
+      _wodBinding.setName(name);
+      _propertyChange.firePropertyChange(RefactoringWodBinding.BINDING_NAME, oldName, name);
+    }
+  }
+
+  public void addPropertyChangeListener(PropertyChangeListener listener) {
+    _propertyChange.addPropertyChangeListener(listener);
+  }
+
+  public void addPropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+    _propertyChange.addPropertyChangeListener(propertyName, listener);
+  }
+
+  public void removePropertyChangeListener(PropertyChangeListener listener) {
+    _propertyChange.removePropertyChangeListener(listener);
+  }
+
+  public void removePropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+    _propertyChange.removePropertyChangeListener(propertyName, listener);
+  }
 }
