05d3dd3591c22ad930375156732a17d328b6ccb0
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2008-06-17 19:18:13 +0000

    whoops
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@5247 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ wolips/core/plugins/org.objectstyle.wolips.refactoring/java/org/objectstyle/wolips/refactoring/RenameWOComponentWizard.java
@@ -0,0 +1,187 @@
+/* ====================================================================
+ * 
+ * The ObjectStyle Group Software License, Version 1.0 
+ *
+ * Copyright (c) 2007 The ObjectStyle Group 
+ * and individual authors of the software.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:  
+ *       "This product includes software developed by the 
+ *        ObjectStyle Group (http://objectstyle.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "ObjectStyle Group" and "Cayenne" 
+ *    must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written 
+ *    permission, please contact andrus@objectstyle.org.
+ *
+ * 5. Products derived from this software may not be called "ObjectStyle"
+ *    nor may "ObjectStyle" appear in their names without prior written
+ *    permission of the ObjectStyle Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE OBJECTSTYLE GROUP OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the ObjectStyle Group.  For more
+ * information on the ObjectStyle Group, please see
+ * <http://objectstyle.org/>.
+ *
+ */
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.objectstyle.wolips.refactoring;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jface.wizard.IWizardPage;
+import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+import org.eclipse.ltk.core.refactoring.participants.RenameRefactoring;
+import org.eclipse.ltk.internal.core.refactoring.resource.RenameResourceProcessor;
+import org.eclipse.ltk.internal.ui.refactoring.RefactoringUIMessages;
+import org.eclipse.ltk.ui.refactoring.RefactoringWizard;
+import org.eclipse.ltk.ui.refactoring.UserInputWizardPage;
+import org.eclipse.ltk.ui.refactoring.resource.RenameResourceWizard;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+/**
+ * Code copied from RenameResourceWizard
+ */
+public class RenameWOComponentWizard extends RefactoringWizard {
+
+	/**
+	 * Creates a {@link RenameResourceWizard}.
+	 * 
+	 * @param resource
+	 *             the resource to rename. The resource must exist.
+	 */
+	public RenameWOComponentWizard(IResource resource) {
+		super(new RenameRefactoring(new RenameWOComponentProcessor(resource)), DIALOG_BASED_USER_INTERFACE);
+		setDefaultPageTitle(RefactoringUIMessages.RenameResourceWizard_page_title);
+		setWindowTitle(RefactoringUIMessages.RenameResourceWizard_window_title);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ltk.ui.refactoring.RefactoringWizard#addUserInputPages()
+	 */
+	protected void addUserInputPages() {
+		RenameResourceProcessor processor= (RenameResourceProcessor) getRefactoring().getAdapter(RenameResourceProcessor.class);
+		addPage(new RenameResourceRefactoringConfigurationPage(processor));
+	}
+
+	private static class RenameResourceRefactoringConfigurationPage extends UserInputWizardPage {
+
+		private final RenameResourceProcessor fRefactoringProcessor;
+		private Text fNameField;
+
+		public RenameResourceRefactoringConfigurationPage(RenameResourceProcessor processor) {
+			super("RenameResourceRefactoringInputPage"); //$NON-NLS-1$
+			fRefactoringProcessor= processor;
+		}
+
+		/* (non-Javadoc)
+		 * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
+		 */
+		public void createControl(Composite parent) {
+			Composite composite= new Composite(parent, SWT.NONE);
+			composite.setLayout(new GridLayout(2, false));
+			composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+			composite.setFont(parent.getFont());
+
+			Label label= new Label(composite, SWT.NONE);
+			label.setText(RefactoringUIMessages.RenameResourceWizard_name_field_label);
+			label.setLayoutData(new GridData());
+
+			fNameField= new Text(composite, SWT.BORDER);
+			fNameField.setText(fRefactoringProcessor.getNewResourceName());
+			fNameField.setFont(composite.getFont());
+			fNameField.setLayoutData(new GridData(GridData.FILL, GridData.BEGINNING, true, false));
+			fNameField.addModifyListener(new ModifyListener() {
+				public void modifyText(ModifyEvent e) {
+					validatePage();
+				}
+			});
+
+			fNameField.selectAll();
+			setPageComplete(false);
+			setControl(composite);
+		}
+
+		public void setVisible(boolean visible) {
+			if (visible) {
+				fNameField.setFocus();
+			}
+			super.setVisible(visible);
+		}
+
+		protected final void validatePage() {
+			String text= fNameField.getText();
+			RefactoringStatus status= fRefactoringProcessor.validateNewElementName(text);
+			setPageComplete(status);
+		}
+
+		/* (non-Javadoc)
+		 * @see org.eclipse.ltk.ui.refactoring.UserInputWizardPage#performFinish()
+		 */
+		protected boolean performFinish() {
+			initializeRefactoring();
+			storeSettings();
+			return super.performFinish();
+		}
+
+		/* (non-Javadoc)
+		 * @see org.eclipse.ltk.ui.refactoring.UserInputWizardPage#getNextPage()
+		 */
+		public IWizardPage getNextPage() {
+			initializeRefactoring();
+			storeSettings();
+			return super.getNextPage();
+		}
+
+		private void storeSettings() {
+		}
+
+		private void initializeRefactoring() {
+			fRefactoringProcessor.setNewResourceName(fNameField.getText());
+		}
+	}
+}
