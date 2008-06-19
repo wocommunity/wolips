8261e6a3fc6de0c68dcb3c399d3942b843859b5a
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2008-06-19 15:23:28 +0000

    switched to new folder layout
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@5261 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- eomodeldoc/package.sh
+++ eomodeldoc/package.sh
@@ -10,21 +10,21 @@ mkdir -p $TEMPDIR
 cd $TEMPDIR
 
 echo Copying classes ...
-cp -r $WORKSPACE/org.objectstyle.wolips.eomodeler/bin/* .
-cp -r $WORKSPACE/org.objectstyle.wolips.eomodeler.core/bin/* .
-cp -r $WORKSPACE/org.objectstyle.wolips.eomodeler.eclipse/bin/* .
-cp -r $WORKSPACE/org.objectstyle.wolips.thirdparty.cayenne/bin/* .
-cp -r $WORKSPACE/org.objectstyle.wolips.baseforplugins/bin/* .
-cp -r $WORKSPACE/eomodeldoc/bin/* .
-cp -r $WORKSPACE/org.objectstyle.wolips.eomodeler.doc/bin/* .
-cp -r $WORKSPACE/org.objectstyle.wolips.eomodeler.factories/bin/* .
+cp -r $WORKSPACE/woproject/wolips/core/plugins/org.objectstyle.wolips.eomodeler/bin/* .
+cp -r $WORKSPACE/woproject/wolips/core/plugins/org.objectstyle.wolips.eomodeler.core/bin/* .
+cp -r $WORKSPACE/woproject/wolips/core/plugins/org.objectstyle.wolips.eomodeler.eclipse/bin/* .
+cp -r $WORKSPACE/woproject/wolips/3rdparty/plugins/org.objectstyle.wolips.thirdparty.cayenne/bin/* .
+cp -r $WORKSPACE/woproject/wolips/base/plugins/org.objectstyle.wolips.baseforplugins/bin/* .
+cp -r $WORKSPACE/woproject/wolips/eomodeldoc/bin/* .
+cp -r $WORKSPACE/woproject/wolips/core/plugins/org.objectstyle.wolips.eomodeler.doc/bin/* .
+cp -r $WORKSPACE/woproject/wolips/core/plugins/org.objectstyle.wolips.eomodeler.factories/bin/* .
 
 echo Unjarring plugins ...
 jar xvf $ECLIPSE/plugins/org.eclipse.equinox.common_3.4.0.v20080421-2006.jar > /dev/null
 jar xvf $ECLIPSE/plugins/org.eclipse.core.resources_3.4.0.v20080604-1400.jar > /dev/null
 jar xvf $ECLIPSE/plugins/org.eclipse.core.jobs_3.4.0.v20080512.jar > /dev/null
-jar xvf $WORKSPACE/org.objectstyle.wolips.woproject/lib/woproject.jar > /dev/null
-jar xvf $WORKSPACE/org.objectstyle.wolips.thirdparty.cayenne/lib/cayenne-1.2M12.jar > /dev/null
+jar xvf $WORKSPACE/woproject/wolips/core/plugins/org.objectstyle.wolips.woproject/lib/woproject.jar > /dev/null
+jar xvf $WORKSPACE/woproject/wolips/3rdparty/plugins/org.objectstyle.wolips.thirdparty.cayenne/lib/cayenne-1.2M12.jar > /dev/null
 
 rm -rf META-INF
 if [ -e $MANIFEST ]; then
