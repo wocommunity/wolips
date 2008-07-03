5e8b042cacbc33815f411982b6e05dec1985f2ec
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2008-07-03 04:05:10 +0000

    moved plist parsing up to the base
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@5283 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ wolips/base/plugins/org.objectstyle.wolips.baseforplugins/java/org/objectstyle/wolips/baseforplugins/plist/PropertyListParserException.java
@@ -0,0 +1,12 @@
+package org.objectstyle.wolips.baseforplugins.plist;
+
+public class PropertyListParserException extends Exception {
+	public PropertyListParserException(String message) {
+		super(message);
+	}
+
+	public PropertyListParserException(String message, Throwable rootCause) {
+		super(message, rootCause);
+	}
+
+}
