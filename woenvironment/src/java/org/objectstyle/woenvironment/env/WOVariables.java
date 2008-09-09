08f2ccd96d167318de2d9e5949d196ca2fa72879
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2008-09-09 13:47:42 +0000

    fixed warning
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@5467 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- woenvironment/src/java/org/objectstyle/woenvironment/env/WOVariables.java
+++ woenvironment/src/java/org/objectstyle/woenvironment/env/WOVariables.java
@@ -61,7 +61,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Properties;
-import java.util.prefs.Preferences;
 
 import org.objectstyle.woenvironment.util.FileStringScanner;
 
