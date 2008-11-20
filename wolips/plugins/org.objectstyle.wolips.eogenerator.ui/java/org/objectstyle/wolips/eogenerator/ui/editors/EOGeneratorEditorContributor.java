d702222eaccaf4d3c959fdf855d9baf7ca8cfc44
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2008-11-20 23:11:08 +0000

    WOL-902 ... that menu item isn't so useful, huh
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@5555 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/plugins/org.objectstyle.wolips.eogenerator.ui/java/org/objectstyle/wolips/eogenerator/ui/editors/EOGeneratorEditorContributor.java
+++ wolips/plugins/org.objectstyle.wolips.eogenerator.ui/java/org/objectstyle/wolips/eogenerator/ui/editors/EOGeneratorEditorContributor.java
@@ -49,14 +49,10 @@
  */
 package org.objectstyle.wolips.eogenerator.ui.editors;
 
-import org.eclipse.jface.action.*;
-import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.action.IAction;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbenchActionConstants;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.ActionFactory;
-import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.ide.IDEActionFactory;
 import org.eclipse.ui.part.MultiPageEditorActionBarContributor;
 import org.eclipse.ui.texteditor.ITextEditor;
@@ -71,14 +67,11 @@ import org.eclipse.ui.texteditor.ITextEditorActionConstants;
 public class EOGeneratorEditorContributor extends MultiPageEditorActionBarContributor {
 	private IEditorPart activeEditorPart;
 
-	private Action sampleAction;
-
 	/**
 	 * Creates a multi-page contributor.
 	 */
 	public EOGeneratorEditorContributor() {
 		super();
-		createActions();
 	}
 
 	/**
@@ -117,27 +110,5 @@ public class EOGeneratorEditorContributor extends MultiPageEditorActionBarContri
 			actionBars.setGlobalActionHandler(IDEActionFactory.BOOKMARK.getId(), getAction(editor, IDEActionFactory.BOOKMARK.getId()));
 			actionBars.updateActionBars();
 		}
-	}
-
-	private void createActions() {
-		sampleAction = new Action() {
-			public void run() {
-				MessageDialog.openInformation(null, "EOGenerator UI", "Sample Action Executed");
-			}
-		};
-		sampleAction.setText("Sample Action");
-		sampleAction.setToolTipText("Sample Action tool tip");
-		sampleAction.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(IDE.SharedImages.IMG_OBJS_TASK_TSK));
-	}
-
-	public void contributeToMenu(IMenuManager manager) {
-		IMenuManager menu = new MenuManager("Editor &Menu");
-		manager.prependToGroup(IWorkbenchActionConstants.MB_ADDITIONS, menu);
-		menu.add(sampleAction);
-	}
-
-	public void contributeToToolBar(IToolBarManager manager) {
-		manager.add(new Separator());
-		manager.add(sampleAction);
 	}
 }
