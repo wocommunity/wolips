357b52ef3131b69409bc373aa12bd923bd1fbef7
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2009-01-24 15:36:35 +0000

    WOL-834 Migration generated by Entity Modeler fails fix this properly, once and for all
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@5642 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

---
+++ wolips/plugins/org.objectstyle.wolips.eomodeler.core/java/org/objectstyle/wolips/eomodeler/core/model/EOEntityForest.java
@@ -0,0 +1,75 @@
+package org.objectstyle.wolips.eomodeler.core.model;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * EOEntityForest represents a collection of EOEntityTreeNodes. An arbitrary
+ * collection of EOEntityTreeNodes constitutes a forest as you can have multiple
+ * root nodes.
+ * 
+ * @author mschrag
+ */
+public class EOEntityForest {
+	private Map<EOEntity, EOEntityTreeNode> _nodes;
+
+	public EOEntityForest() {
+		_nodes = new HashMap<EOEntity, EOEntityTreeNode>();
+	}
+
+	public Set<EOEntityTreeNode> getRootNodes() {
+		Set<EOEntityTreeNode> rootNodes = new HashSet<EOEntityTreeNode>();
+		for (EOEntityTreeNode node : _nodes.values()) {
+			if (node.isRoot()) {
+				rootNodes.add(node);
+			}
+		}
+		return rootNodes;
+	}
+
+	public void add(Collection<EOEntity> entities) {
+		for (EOEntity entity : entities) {
+			add(entity);
+		}
+	}
+
+	public EOEntityTreeNode add(EOEntity entity) {
+		EOEntityTreeNode node = _nodes.get(entity);
+		if (node == null) {
+			node = new EOEntityTreeNode(entity);
+			_nodes.put(entity, node);
+
+			EOEntity parentEntity = entity.getParent();
+			EOEntityTreeNode parentNode = _nodes.get(parentEntity);
+			if (parentNode != null) {
+				parentNode.addChild(node);
+			}
+
+			Set<EOEntity> childrenEntities = entity.getChildrenEntities();
+			for (EOEntity childEntity : childrenEntities) {
+				EOEntityTreeNode childNode = _nodes.get(childEntity);
+				if (childNode != null) {
+					node.addChild(childNode);
+				}
+			}
+		}
+		return node;
+	}
+
+	public List<EOEntity> breadthFirst() {
+		List<EOEntity> entities = new LinkedList<EOEntity>();
+		Set<EOEntityTreeNode> rootNodes = getRootNodes();
+		for (EOEntityTreeNode rootNode : rootNodes) {
+			entities.add(rootNode.getEntity());
+		}
+		for (EOEntityTreeNode rootNode : rootNodes) {
+			rootNode._breadthFirst(entities);
+		}
+		return entities;
+	}
+}
---
+++ wolips/plugins/org.objectstyle.wolips.eomodeler.core/java/org/objectstyle/wolips/eomodeler/core/model/EOEntityTreeNode.java
@@ -0,0 +1,64 @@
+package org.objectstyle.wolips.eomodeler.core.model;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * EOEntityTreeNode is a data structure for representing a subtree of a set of
+ * entities. As an example, when you are generating SQ, you need to be able to
+ * order an arbitrary set of entities in order of their inheritance.
+ * 
+ * @author mschrag
+ */
+public class EOEntityTreeNode {
+	private EOEntity _entity;
+
+	private EOEntityTreeNode _parent;
+
+	private Set<EOEntityTreeNode> _children;
+
+	public EOEntityTreeNode(EOEntity entity) {
+		_entity = entity;
+		_children = new HashSet<EOEntityTreeNode>();
+	}
+
+	public EOEntity getEntity() {
+		return _entity;
+	}
+
+	public boolean isRoot() {
+		return _parent == null;
+	}
+
+	public EOEntityTreeNode getParent() {
+		return _parent;
+	}
+
+	public Set<EOEntityTreeNode> getChildren() {
+		return _children;
+	}
+
+	public void addChild(EOEntityTreeNode entity) {
+		entity._parent = this;
+		_children.add(entity);
+	}
+
+	public void removeChild(EOEntityTreeNode entity) {
+		entity._parent = null;
+		_children.remove(entity);
+	}
+
+	public void _breadthFirst(List<EOEntity> entities) {
+		for (EOEntityTreeNode childNode : getChildren()) {
+			entities.add(childNode.getEntity());
+		}
+		for (EOEntityTreeNode childNode : getChildren()) {
+			childNode._breadthFirst(entities);
+		}
+	}
+
+	public String toString() {
+		return "[EOEntityNode: entity=" + _entity.getName() + "]";
+	}
+}
