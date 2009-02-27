b32f5aee22df740578f38ded9d0c0a61ae2e05fb
qdolan <qdolan@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2009-02-27 07:10:00 +0000

    Added custom displaygroup class support to display group editor. Can now use ERXDisplayGroup etc, including auto populated generics. Note: This only checks that a class is a WODisplayGroup subclass, it doesn't reflect the class to ensure it has the necessary static methods for woo instantiation to work correctly.
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@5691 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/plugins/org.objectstyle.wolips.wodclipse.core/java/org/objectstyle/wolips/wodclipse/core/refactoring/AddKeyOperation.java
+++ wolips/plugins/org.objectstyle.wolips.wodclipse.core/java/org/objectstyle/wolips/wodclipse/core/refactoring/AddKeyOperation.java
@@ -5,7 +5,10 @@ package org.objectstyle.wolips.wodclipse.core.refactoring;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.Flags;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IField;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.NamingConventions;
@@ -24,118 +27,183 @@ import org.eclipse.ui.IEditorPart;
 import org.objectstyle.wolips.bindings.utils.BindingReflectionUtils;
 
 public class AddKeyOperation extends JavaModelOperation {
-  private AddKeyInfo _info;
-
-  public AddKeyOperation(AddKeyInfo info) {
-    super(new IJavaElement[] { info.getComponentType() });
-    _info = info;
-  }
-
-  @Override
-  protected void executeOperation() throws JavaModelException {
-    IType componentType = _info.getComponentType();
-
-    String keyType = _info.getJavaTypeName();
-    String keyParameterType = _info.getJavaParameterTypeName();
-
-    boolean useGenerics = JavaModelUtil.is50OrHigher(componentType.getJavaProject());
-    String simpleTypeName;
-    if (keyParameterType != null && useGenerics) {
-      simpleTypeName = Signature.getSimpleName(keyType) + "<" + Signature.getSimpleName(keyParameterType) + ">";
-    }
-    else {
-      simpleTypeName = Signature.getSimpleName(keyType);
-    }
-
-    if (BindingReflectionUtils.isImportRequired(keyType)) {
-      new CreateImportOperation(keyType, componentType.getCompilationUnit(), Flags.AccDefault).runOperation(progressMonitor);
-    }
-
-    if (BindingReflectionUtils.isImportRequired(keyParameterType)) {
-      new CreateImportOperation(keyParameterType, componentType.getCompilationUnit(), Flags.AccDefault).runOperation(progressMonitor);
-    }
-
-    String fieldName = _info.getFieldName();
-
-    if (_info.isCreateField()) {
-      String accessModifier = "private ";
-      if (!(_info.isCreateAccessorMethod() || _info.isCreateMutatorMethod())) {
-        accessModifier = "public ";
-      }
-      String keyField = accessModifier + simpleTypeName + " " + fieldName + ";";
-      new CreateFieldOperation(componentType, keyField, false).runOperation(progressMonitor);
-    }
-
-    boolean isBoolean = BindingReflectionUtils.isBoolean(keyType);
-    if (_info.isCreateAccessorMethod()) {
-      String accessorMethodName;
-      if (_info.isPrependGetToAccessorMethod()) {
-        accessorMethodName = NamingConventions.suggestGetterName(componentType.getJavaProject(), fieldName, Flags.AccPublic, isBoolean, null);
-      }
-      else {
-        accessorMethodName = _info.getName();
-      }
-
-      String source = null;
-      if (_info.isCreateField()) {
-        try {
-          source = GetterSetterUtil.getGetterStub(componentType.getField(fieldName), accessorMethodName, true, Flags.AccPublic);
-        }
-        catch (CoreException e) {
-          throw new JavaModelException(e);
-        }
-      }
-      else {
-        source = "public " + simpleTypeName + " " + accessorMethodName + "() {\n" + " // TODO\n" + "return null;\n" + "}";
-      }
-      if (source != null) {
-        String lineDelim = "\n";// TextUtilities.getDefaultLineDelimiter(document);
-        source = CodeFormatterUtil.format(CodeFormatter.K_CLASS_BODY_DECLARATIONS, source, 1, null, componentType.getJavaProject());
-        new CreateMethodOperation(componentType, source, false).runOperation(progressMonitor);
-      }
-    }
-
-    if (_info.isCreateMutatorMethod()) {
-      String mutatorMethodName = NamingConventions.suggestSetterName(componentType.getJavaProject(), fieldName, Flags.AccPublic, isBoolean, null);
-
-      String source = null;
-      if (_info.isCreateField()) {
-        try {
-          source = GetterSetterUtil.getSetterStub(componentType.getField(fieldName), mutatorMethodName, true, Flags.AccPublic);
-        }
-        catch (CoreException e) {
-          throw new JavaModelException(e);
-        }
-      }
-      else {
-        source = "public void " + mutatorMethodName + "(" + simpleTypeName + " " + _info.getName() + ") {\n" + " // TODO\n" + "}";
-      }
-      if (source != null) {
-        String lineDelim = "\n";// TextUtilities.getDefaultLineDelimiter(document);
-        source = CodeFormatterUtil.format(CodeFormatter.K_CLASS_BODY_DECLARATIONS, source, 1, null, componentType.getJavaProject());
-        new CreateMethodOperation(componentType, source, false).runOperation(progressMonitor);
-      }
-    }
-  }
-
   public static void addKey(AddKeyInfo info) throws CoreException {
     IEditorPart editorPart = JavaUI.openInEditor(info.getComponentType().getCompilationUnit());
     if (editorPart != null) {
-      // CompilationUnitEditor cuEditor =
-      // (CompilationUnitEditor) editorPart;
-      // cuEditor.getDocumentProvider().getDocument(componentType.getCompilationUnit()).get
       IRewriteTarget target = (IRewriteTarget) editorPart.getAdapter(IRewriteTarget.class);
       if (target != null) {
         target.beginCompoundChange();
       }
       try {
         new AddKeyOperation(info).run(null);
-      }
-      finally {
+      } finally {
         if (target != null) {
           target.endCompoundChange();
         }
       }
     }
+  }
+
+  public static void replaceField(AddKeyInfo info, String originalName) throws CoreException {
+    IEditorPart editorPart = JavaUI.openInEditor(info.getComponentType().getCompilationUnit());
+    if (editorPart != null) {
+      IRewriteTarget target = (IRewriteTarget) editorPart.getAdapter(IRewriteTarget.class);
+      if (target != null) {
+        target.beginCompoundChange();
+      }
+      try {
+        String newName = info.getName();
+
+        info.setName(originalName);
+        String originalFieldName = info.getFieldName();
+        info.setName(newName);
+
+        // This is less than ideal, but will do for now. At least this way the field order
+        // should remain the same.
+        IField originalField = info.getComponentType().getField(originalFieldName);
+        if (info.isCreateField() && originalField.exists()) {
+          AddKeyOperation op = new AddKeyOperation(info);
+          op._sibling = originalField;
+          op._force = true;
+          op.run(null);
+          originalField.delete(false, null);
+        }
+      } finally {
+        if (target != null) {
+          target.endCompoundChange();
+        }
+      }
+    }
+  }
+
+  private boolean _force = false;
+
+  private AddKeyInfo _info;
+
+  private IJavaElement _sibling;
+
+  public AddKeyOperation(AddKeyInfo info) {
+    super(new IJavaElement[] { info.getComponentType() });
+    _info = info;
+  }
+
+  private void createAccessor(String simpleTypeName, String fieldName) throws JavaModelException {
+    boolean isBoolean = BindingReflectionUtils.isBoolean(_info.getJavaTypeName());
+    IType componentType = getComponentType();
+
+    String accessorMethodName;
+    if (_info.isPrependGetToAccessorMethod()) {
+      accessorMethodName = NamingConventions.suggestGetterName(componentType.getJavaProject(), fieldName, Flags.AccPublic, isBoolean, null);
+    } else {
+      accessorMethodName = _info.getName();
+    }
+
+    String source = null;
+    if (_info.isCreateField()) {
+      try {
+        source = GetterSetterUtil.getGetterStub(componentType.getField(fieldName), accessorMethodName, true, Flags.AccPublic);
+      } catch (CoreException e) {
+        throw new JavaModelException(e);
+      }
+    } else {
+      source = "public " + simpleTypeName + " " + accessorMethodName + "() {\n" + " // TODO\n" + "return null;\n" + "}";
+    }
+    if (source != null) {
+      String lineDelim = "\n";// TextUtilities.getDefaultLineDelimiter(document);
+      source = CodeFormatterUtil.format(CodeFormatter.K_CLASS_BODY_DECLARATIONS, source, 1, null, componentType.getJavaProject());
+      new CreateMethodOperation(componentType, source, false).runOperation(progressMonitor);
+    }
+  }
+
+  private void createField(String simpleTypeName, String fieldName) throws JavaModelException {
+    IType componentType = getComponentType();
+
+    String accessModifier = "private ";
+    if (!(_info.isCreateAccessorMethod() || _info.isCreateMutatorMethod())) {
+      accessModifier = "public ";
+    }
+    String keyField = accessModifier + simpleTypeName + " " + fieldName + ";";
+    CreateFieldOperation op = new CreateFieldOperation(componentType, keyField, _force);
+    op.createAfter(_sibling);
+    op.runOperation(progressMonitor);
+  }
+
+  private void createImports(String keyType, String keyParameterType) throws JavaModelException {
+    if (BindingReflectionUtils.isImportRequired(keyType)) {
+      new CreateImportOperation(keyType, getCompilationUnit(), Flags.AccDefault).runOperation(progressMonitor);
+    }
+
+    if (BindingReflectionUtils.isImportRequired(keyParameterType)) {
+      new CreateImportOperation(keyParameterType, getCompilationUnit(), Flags.AccDefault).runOperation(progressMonitor);
+    }
+  }
+
+  private void createMutator(String simpleTypeName, String fieldName) throws JavaModelException {
+    boolean isBoolean = BindingReflectionUtils.isBoolean(_info.getJavaTypeName());
+    IType componentType = getComponentType();
+
+    String mutatorMethodName = NamingConventions.suggestSetterName(getJavaProject(), fieldName, Flags.AccPublic, isBoolean, null);
+
+    String source = null;
+    if (_info.isCreateField()) {
+      try {
+        source = GetterSetterUtil.getSetterStub(componentType.getField(fieldName), mutatorMethodName, true, Flags.AccPublic);
+      } catch (CoreException e) {
+        throw new JavaModelException(e);
+      }
+    } else {
+      source = "public void " + mutatorMethodName + "(" + simpleTypeName + " " + _info.getName() + ") {\n" + " // TODO\n" + "}";
+    }
+    if (source != null) {
+      String lineDelim = "\n";// TextUtilities.getDefaultLineDelimiter(document);
+      source = CodeFormatterUtil.format(CodeFormatter.K_CLASS_BODY_DECLARATIONS, source, 1, null, getJavaProject());
+      new CreateMethodOperation(componentType, source, false).runOperation(progressMonitor);
+    }
+  }
+
+  @Override
+  protected void executeOperation() throws JavaModelException {
+    String keyType = _info.getJavaTypeName();
+    String keyParameterType = _info.getJavaParameterTypeName();
+
+    String simpleTypeName = simpleTypeName(keyType, keyParameterType);
+    String fieldName = _info.getFieldName();
+
+    createImports(keyType, keyParameterType);
+
+    if (_info.isCreateField()) {
+      createField(simpleTypeName, fieldName);
+    }
+
+    if (_info.isCreateAccessorMethod()) {
+      createAccessor(simpleTypeName, fieldName);
+    }
+
+    if (_info.isCreateMutatorMethod()) {
+      createMutator(simpleTypeName, fieldName);
+    }
+  }
+
+  private ICompilationUnit getCompilationUnit() {
+    return _info.getComponentType().getCompilationUnit();
+  }
+
+  private IType getComponentType() {
+    return _info.getComponentType();
+  }
+
+  private IJavaProject getJavaProject() {
+    return _info.getComponentType().getJavaProject();
+  }
+
+  private String simpleTypeName(String keyType, String keyParameterType) {
+    boolean useGenerics = JavaModelUtil.is50OrHigher(getJavaProject());
+    String simpleTypeName;
+    if (keyParameterType != null && useGenerics) {
+      simpleTypeName = Signature.getSimpleName(keyType) + "<" + Signature.getSimpleName(keyParameterType) + ">";
+    } else {
+      simpleTypeName = Signature.getSimpleName(keyType);
+    }
+    return simpleTypeName;
   }
 }
