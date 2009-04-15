7fa86af39530b7ab96e9653d1156db0350253abf
mschrag <mschrag@930f2ab1-3212-0410-a8ea-d1eb4cc50a93>
2009-04-15 17:47:32 +0000

    trying to make it slightly easier to work on EntityMigration0.java + fixed parse error in the previous version
    git-svn-id: https://svn.objectstyle.org/repos/woproject/trunk@5721 930f2ab1-3212-0410-a8ea-d1eb4cc50a93

--- wolips/core/plugins/org.objectstyle.wolips.eomodeler.core/templates/EntityMigration0.java
+++ wolips/core/plugins/org.objectstyle.wolips.eomodeler.core/templates/EntityMigration0.java
@@ -6,26 +6,46 @@
 #end
 #foreach ($attribute in $entity.sortedAttributes)
 #if ($attribute.sqlGenerationCreateProperty)
-		#if ($attribute.prototype.name == "longText")${migrationTableName}.newLargeStringColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
-#elseif ($attribute.javaClassName == "String" && $attribute.width)${migrationTableName}.newStringColumn("${attribute.columnName}", ${attribute.width}, ${attribute.sqlGenerationAllowsNull});
-#elseif ($attribute.javaClassName == "String")${migrationTableName}.newStringColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
-#elseif ($attribute.javaClassName == "BigDecimal" || $attribute.javaClassName == "java.math.BigDecimal")${migrationTableName}.newBigDecimalColumn("${attribute.columnName}", ${attribute.precision}, ${attribute.scale}, ${attribute.sqlGenerationAllowsNull});
-#elseif ($attribute.javaClassName == "Integer" && $attribute.precision)${migrationTableName}.newIntegerColumn("${attribute.columnName}", ${attribute.precision}, ${attribute.sqlGenerationAllowsNull});
-#elseif ($attribute.javaClassName == "Integer")${migrationTableName}.newIntegerColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
-#elseif ($attribute.javaClassName == "Long" && $attribute.precision)${migrationTableName}.newBigIntegerColumn("${attribute.columnName}", ${attribute.precision}, ${attribute.sqlGenerationAllowsNull});
-#elseif ($attribute.javaClassName == "Long")${migrationTableName}.newBigIntegerColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
-#elseif ($attribute.javaClassName == "Double")${migrationTableName}.newDoubleColumn("${attribute.columnName}", ${attribute.precision}, ${attribute.scale}, ${attribute.sqlGenerationAllowsNull});
-#elseif ($attribute.javaClassName == "Float")${migrationTableName}.newFloatColumn("${attribute.columnName}", ${attribute.precision}, ${attribute.scale}, ${attribute.sqlGenerationAllowsNull});
-#elseif ($attribute.javaClassName == "Boolean" && $attribute.width == 5)${migrationTableName}.newBooleanColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
-#elseif ($attribute.javaClassName == "Boolean" && $attribute.externalType == "bool")${migrationTableName}.newFlagBooleanColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
-#elseif ($attribute.javaClassName == "Boolean")${migrationTableName}.newIntBooleanColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
-#elseif ($attribute.javaClassName == "NSTimestamp")${migrationTableName}.newTimestampColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
-#elseif ($attribute.javaClassName == "NSData")${migrationTableName}.newBlobColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
-#elseif ($attribute.adaptorValueConversionMethodName)#if ($attribute.factoryMethodArgumentType.ID == "EOFactoryMethodArgumentIsNSString")${migrationTableName}.newStringColumn("${attribute.columnName}", ${attribute.width}, ${attribute.sqlGenerationAllowsNull});
-#elseif ($attribute.adaptorValueConversionMethodName)#if ($attribute.factoryMethodArgumentType.ID == "EOFactoryMethodArgumentIsData")${migrationTableName}.newBlobColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
-#else FIX // Unable to create a migration for ${attribute.name} (Java Class Name: ${attribute.javaClassName}) with the factoryMethodArgumentType $attribute.factoryMethodArgumentType.ID
+#if ($attribute.prototype.name == "longText")
+		${migrationTableName}.newLargeStringColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
+#elseif ($attribute.javaClassName == "String" && $attribute.width)
+		${migrationTableName}.newStringColumn("${attribute.columnName}", ${attribute.width}, ${attribute.sqlGenerationAllowsNull});
+#elseif ($attribute.javaClassName == "String")
+		${migrationTableName}.newStringColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
+#elseif ($attribute.javaClassName == "BigDecimal" || $attribute.javaClassName == "java.math.BigDecimal")
+		${migrationTableName}.newBigDecimalColumn("${attribute.columnName}", ${attribute.precision}, ${attribute.scale}, ${attribute.sqlGenerationAllowsNull});
+#elseif ($attribute.javaClassName == "Integer" && $attribute.precision)
+		${migrationTableName}.newIntegerColumn("${attribute.columnName}", ${attribute.precision}, ${attribute.sqlGenerationAllowsNull});
+#elseif ($attribute.javaClassName == "Integer")
+		${migrationTableName}.newIntegerColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
+#elseif ($attribute.javaClassName == "Long" && $attribute.precision)
+		${migrationTableName}.newBigIntegerColumn("${attribute.columnName}", ${attribute.precision}, ${attribute.sqlGenerationAllowsNull});
+#elseif ($attribute.javaClassName == "Long")
+		${migrationTableName}.newBigIntegerColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
+#elseif ($attribute.javaClassName == "Double")
+		${migrationTableName}.newDoubleColumn("${attribute.columnName}", ${attribute.precision}, ${attribute.scale}, ${attribute.sqlGenerationAllowsNull});
+#elseif ($attribute.javaClassName == "Float")
+		${migrationTableName}.newFloatColumn("${attribute.columnName}", ${attribute.precision}, ${attribute.scale}, ${attribute.sqlGenerationAllowsNull});
+#elseif ($attribute.javaClassName == "Boolean" && $attribute.width == 5)
+		${migrationTableName}.newBooleanColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
+#elseif ($attribute.javaClassName == "Boolean" && $attribute.externalType == "bool")
+		${migrationTableName}.newFlagBooleanColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
+#elseif ($attribute.javaClassName == "Boolean")
+		${migrationTableName}.newIntBooleanColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
+#elseif ($attribute.javaClassName == "NSTimestamp")
+		${migrationTableName}.newTimestampColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
+#elseif ($attribute.javaClassName == "NSData")
+		${migrationTableName}.newBlobColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
+#elseif ($attribute.adaptorValueConversionMethodName)
+#if ($attribute.factoryMethodArgumentType.ID == "EOFactoryMethodArgumentIsNSString")
+		${migrationTableName}.newStringColumn("${attribute.columnName}", ${attribute.width}, ${attribute.sqlGenerationAllowsNull});
+#elseif ($attribute.factoryMethodArgumentType.ID == "EOFactoryMethodArgumentIsData")
+		${migrationTableName}.newBlobColumn("${attribute.columnName}", ${attribute.sqlGenerationAllowsNull});
+#else
+		FIX // Unable to create a migration for ${attribute.name} (Java Class Name: ${attribute.javaClassName}) with the factoryMethodArgumentType ${attribute.factoryMethodArgumentType.ID}
 #end
-#else FIX // Unable to create a migration for ${attribute.name} (Java Class Name: ${attribute.javaClassName})
+#else
+		FIX // Unable to create a migration for ${attribute.name} (Java Class Name: ${attribute.javaClassName})
 #end
 #end
 #end
