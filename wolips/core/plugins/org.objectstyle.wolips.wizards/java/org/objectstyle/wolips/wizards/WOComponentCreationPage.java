aa191d1df02a449004abc425150177ac3c499998
Pascal Robert <probert@macti.ca>
2012-01-15 21:21:01 -0500

    Issue #42. Put the WO Component by default into the Components folder

--- wolips/core/plugins/org.objectstyle.wolips.wizards/java/org/objectstyle/wolips/wizards/WOComponentCreationPage.java
+++ wolips/core/plugins/org.objectstyle.wolips.wizards/java/org/objectstyle/wolips/wizards/WOComponentCreationPage.java
@@ -113,6 +113,8 @@ import org.objectstyle.wolips.eomodeler.utils.StringLabelProvider;
 import org.objectstyle.wolips.locate.LocateException;
 import org.objectstyle.wolips.locate.LocatePlugin;
 import org.objectstyle.wolips.locate.result.LocalizedComponentsLocateResult;
+import org.objectstyle.wolips.baseforuiplugins.utils.ErrorUtils;
+import org.objectstyle.wolips.core.resources.types.project.ProjectAdapter;
 
 /**
  * @author mnolte
@@ -230,20 +232,11 @@ public class WOComponentCreationPage extends WizardNewWOResourcePage {
 	 *            the current selection
 	 */
 	public WOComponentCreationPage(IStructuredSelection selection) {
+	
 		super("createWOComponentPage1", WOComponentCreationPage.processSelection(selection));
 		this.setTitle(Messages.getString("WOComponentCreationPage.title"));
 		this.setDescription(Messages.getString("WOComponentCreationPage.description"));
 
-		if (selection != null) {
-			Object selectedObject = selection.getFirstElement();
-			if (selectedObject instanceof IFolder) {
-				IJavaElement parentJavaElement = JavaCore.create((IFolder) selectedObject);
-				if (parentJavaElement instanceof IPackageFragment) {
-					_currentSelection = parentJavaElement;
-					this.setContainerFullPath(componentPathForPackage((IPackageFragment)_currentSelection));
-				}
-			}
-		}
 	}
 
 	public static IStructuredSelection processSelection(IStructuredSelection selection) {
@@ -326,6 +319,14 @@ public class WOComponentCreationPage extends WizardNewWOResourcePage {
 	public void createControl(Composite parent) {
 		// inherit default container and name specification widgets
 		super.createControl(parent);
+
+		ProjectAdapter projectAdapter = (ProjectAdapter) getProject().getAdapter(ProjectAdapter.class);
+		if (projectAdapter != null) {
+			IContainer resource = (IContainer)ResourcesPlugin.getWorkspace().getRoot().findMember(getContainerFullPath());
+			if (!projectAdapter.isResourceContainer(resource)) {
+				setContainerFullPath(projectAdapter.getDefaultComponentsFolder().getFullPath());
+			}
+		}
 
 		Composite composite = (Composite) getControl();
 		// WorkbenchHelp.setHelp(composite,
@@ -819,5 +820,14 @@ public class WOComponentCreationPage extends WizardNewWOResourcePage {
 			handleSelectionEvent(event);
 		}
 
+	}
+	
+	public IProject getProject() {
+		IPath containerFullPath = getContainerFullPath();
+		if (containerFullPath == null) {
+			ErrorUtils.openErrorDialog(getShell(), "No Folder Selectd", "You must select a folder to create a new EOModel.");
+		}
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(containerFullPath.segment(0));
+		return project;
 	}
 }
