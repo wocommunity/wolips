b91bac9e5cb4f6838a954ba8c5dc1396ace45e90
Quinton Dolan <qdolan@gmail.com>
2012-10-22 15:58:53 +1000

    Configure ant based builds.

--- entityModeler/repackage.sh
+++ entityModeler/repackage.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
+archive=EntityModeler-macosx.cocoa.x86_64.zip
+basedir=`dirname $0`
+basedir=`(cd ${basedir}; pwd -P)`
 
-if [ ! -e "./Entity Modeler.app" ]; then
-	echo "Entity Modeler.app not found in current directory"
-	exit 1
+if [ -e "${archive}" ]; then
+  rm -fr dist
+  mkdir -p dist
+  (cd dist; unzip -q ../${archive})
+  mv dist/EntityModeler/* dist
+  rm -fr dist/EntityModeler
+  cd dist
 fi
 
-em_home=`dirname $0`
+if [ ! -e "Entity Modeler.app" ]; then
+  echo "Entity Modeler.app not found in current directory"
+  exit 1
+fi
 contents="Entity Modeler.app/Contents"
 resources="${contents}/Resources"
 test -d configuration && mv configuration "${resources}"
 test -d plugins && mv plugins "${resources}"
-cp ${em_home}/icons/EOModel.icns "${resources}"
-cp ${em_home}/Info.plist "${contents}"
-version=`grep '<product' "${em_home}/EntityModeler.product"  | sed -E 's/.*version="([^"]*)".*/\1/'`
+cp ${basedir}/icons/EOModel.icns "${resources}"
+cp ${basedir}/Info.plist "${contents}"
+version=`grep '<product' "${basedir}/EntityModeler.product"  | sed -E 's/.*version="([^"]*)".*/\1/'`
 sed -i "" "s/__VERSION__/${version}/" "${contents}/Info.plist"
+
+rm -f EntityModeler-*.zip
+zip -qr EntityModeler-${version}.zip Entity\ Modeler.app
