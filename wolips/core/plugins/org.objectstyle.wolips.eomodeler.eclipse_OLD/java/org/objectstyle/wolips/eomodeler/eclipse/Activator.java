2e3a8194cff84ae1ea24e7c0608303da65bbf582
swklein <stefan.klein@buero-sde.de>
2014-10-04 23:46:13 +0200

    Recreation of EOModeler.eclipse
    When org.objectstyle.wolips.eomodeler.eclipse was loaded into eclipse all PDE Dependencies where broken

--- wolips/core/plugins/org.objectstyle.wolips.eomodeler.eclipse_OLD/java/org/objectstyle/wolips/eomodeler/eclipse/Activator.java
+++ wolips/core/plugins/org.objectstyle.wolips.eomodeler.eclipse_OLD/java/org/objectstyle/wolips/eomodeler/eclipse/Activator.java
@@ -49,51 +49,28 @@
  */
 package org.objectstyle.wolips.eomodeler.eclipse;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.objectstyle.wolips.baseforuiplugins.AbstractBaseUIActivator;
 import org.osgi.framework.BundleContext;
 
-/**
- * The activator class controls the plug-in life cycle
- */
-public class Activator extends AbstractUIPlugin {
+public class Activator extends AbstractBaseUIActivator {
+	public static final String PLUGIN_ID = "org.objectstyle.wolips.eomodeler.eclipse";
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "org.objectstyle.wolips.eomodeler.eclipse"; //$NON-NLS-1$
+	private static Activator _plugin;
 
-	// The shared instance
-	private static Activator plugin;
-	
-	/**
-	 * The constructor
-	 */
 	public Activator() {
+		_plugin = this;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
-		plugin = this;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
 	public void stop(BundleContext context) throws Exception {
-		plugin = null;
+		_plugin = null;
 		super.stop(context);
 	}
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
 	public static Activator getDefault() {
-		return plugin;
+		return _plugin;
 	}
-
 }
---
+++ wolips/core/plugins/org.objectstyle.wolips.eomodeler.eclipse_OLD/java/org/objectstyle/wolips/eomodeler/eclipse/EclipseEOClassLoaderFactory.java
@@ -0,0 +1,71 @@
+package org.objectstyle.wolips.eomodeler.eclipse;
+
+import java.net.URL;
+import java.util.Set;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.JavaCore;
+import org.objectstyle.wolips.baseforplugins.util.URLUtils;
+import org.objectstyle.wolips.eomodeler.core.model.AbstractEOClassLoader;
+import org.objectstyle.wolips.eomodeler.core.model.EOModel;
+import org.objectstyle.wolips.eomodeler.utils.EclipseFileUtils;
+import org.objectstyle.wolips.launching.actions.WOJavaApplicationLaunchShortcut;
+
+public class EclipseEOClassLoaderFactory extends AbstractEOClassLoader {
+	@Override
+	protected String getCacheKey(EOModel model, Set<URL> classpathUrlSet) {
+		String cacheKey;
+		IProject project = null;
+		IFile eclipseFile = EclipseFileUtils.getEclipseFile(model.getModelURL());
+		if (eclipseFile != null) {
+			project = eclipseFile.getProject();
+		}
+		if (project != null) {
+			cacheKey = project.getName();
+		} else {
+			cacheKey = super.getCacheKey(model, classpathUrlSet);
+		}
+		return cacheKey;
+	}
+
+	@Override
+	protected void fillInDevelopmentClasspath(Set<URL> classpathUrls) throws Exception {
+		// AK: we don't want to re-jar each time we make a change....
+		// String workSpacePath =
+		// VariablesPlugin.getDefault().getWOProjectDevelopmentPath();
+		// if (workSpacePath != null) {
+		// URL classUrl = new URL("file://" + workSpacePath +
+		// "wolips/core/plugins/org.objectstyle.wolips.eomodeler.core/bin/");
+		// classpathUrls.add(classUrl);
+		// }
+	}
+
+	@Override
+	protected void fillInModelClasspath(EOModel model, Set<URL> classpathUrls) throws Exception {
+		IProject project = null;
+		IFile eclipseFile = EclipseFileUtils.getEclipseFile(model.getModelURL());
+		if (eclipseFile != null) {
+			project = eclipseFile.getProject();
+		}
+		if (project == null) {
+			URL modelURL = model.getModelURL();
+			if (modelURL != null) {
+				IContainer[] modelContainers = ResourcesPlugin.getWorkspace().getRoot().findContainersForLocation(new Path(URLUtils.cheatAndTurnIntoFile(modelURL).getAbsolutePath()));
+				// IContainer[] modelContainers =
+				// ResourcesPlugin.getWorkspace().getRoot().findContainersForLocation(new
+				// Path(modelURL.toString()));
+				for (int modelContainerNum = 0; modelContainerNum < modelContainers.length; modelContainerNum++) {
+					IContainer modelContainer = modelContainers[modelContainerNum];
+					IProject modelProject = modelContainer.getProject();
+					classpathUrls.addAll(WOJavaApplicationLaunchShortcut.createClasspathURLsForProject(JavaCore.create(modelProject)));
+				}
+			}
+		} else {
+			classpathUrls.addAll(WOJavaApplicationLaunchShortcut.createClasspathURLsForProject(JavaCore.create(project)));
+		}
+	}
+}
