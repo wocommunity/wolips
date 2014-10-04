da06f607af4a52b823a706ab110180dc7e61808f
swklein <stefan.klein@buero-sde.de>
2014-10-04 17:26:48 +0200

    Classpath stuff and remove of WO 5.6 (yes this was in the code) dependencies

--- wolips/core/plugins/org.objectstyle.wolips.eomodeler.core/java/org/objectstyle/wolips/eomodeler/core/sql/EOFSQLReverseEngineerFactory.java
+++ wolips/core/plugins/org.objectstyle.wolips.eomodeler.core/java/org/objectstyle/wolips/eomodeler/core/sql/EOFSQLReverseEngineerFactory.java
@@ -12,13 +12,7 @@ import org.objectstyle.wolips.eomodeler.core.model.EODatabaseConfig;
 public class EOFSQLReverseEngineerFactory implements IEOSQLReverseEngineerFactory {
 	public IEOSQLReverseEngineer reverseEngineer(EODatabaseConfig databaseConfig, ClassLoader eomodelClassLoader) throws SecurityException, NoSuchMethodException, ClassNotFoundException, IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException {
 		WOUtils.setWOSystemProperties();
-		String className;
-		if (WOUtils.version(eomodelClassLoader) == WOUtils.Version.WO_5_6) {
-			className = "org.objectstyle.wolips.eomodeler.core.sql.EOFSQLReverseEngineer56";
-		}
-		else {
-			className = "org.objectstyle.wolips.eomodeler.core.sql.EOFSQLReverseEngineer53";
-		}
+		String className = "org.objectstyle.wolips.eomodeler.core.sql.EOFSQLReverseEngineer53";
 		Class reverseEngineerClass = Class.forName(className, true, eomodelClassLoader);
 		Constructor reverseEngineerConstructor = reverseEngineerClass.getConstructor(new Class[] { String.class, Map.class });
 		Object reverseEngineerButICantCastItBecauseItCrossesClassLoaders = reverseEngineerConstructor.newInstance(new Object[] { databaseConfig.getAdaptorName(), databaseConfig.getConnectionDictionary() });
