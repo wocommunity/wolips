2bafe4d588cb67f4ab6a964a3246e83c694b984f
Savas Celik <savas.celik@rucotec.ch>
2019-01-16 15:58:00 +0100

    Nothing changed.

--- wolips/core/plugins/org.objectstyle.wolips.eomodeler/java/ch/rucotec/wolips/eomodeler/editors/diagram/EOClassDiagramBasicEditorSection.java
+++ wolips/core/plugins/org.objectstyle.wolips.eomodeler/java/ch/rucotec/wolips/eomodeler/editors/diagram/EOClassDiagramBasicEditorSection.java
@@ -38,139 +38,140 @@ import ch.rucotec.wolips.eomodeler.core.model.EOERDiagram;
  *
  */
 public class EOClassDiagramBasicEditorSection extends AbstractPropertySection {
+	
 	//---------------------------------------------------------------------------
-		// ### Variables and Constants
-		//---------------------------------------------------------------------------
-		
-		
-		private EOClassDiagram myDiagram;
-		private EOModel myCurrentModel;
-		
-		private Text myNameText;
-		
-		private DataBindingContext myBindingContext;
-		
-		private Composite parent;
-		
-		private ArrayList<Button> checkBoxes = new ArrayList<Button>();
-		
-		//---------------------------------------------------------------------------
-		// ### Construction
-		//---------------------------------------------------------------------------
-		
-		//---------------------------------------------------------------------------
-		// ### Methods
-		//---------------------------------------------------------------------------
-		
-		/**
-		 * This creates the form in the Properties Tab
-		 */
-		@Override
-		public void createControls(Composite _parent, TabbedPropertySheetPage _tabbedPropertySheetPage) {
-			super.createControls(_parent, _tabbedPropertySheetPage);
-			parent = _parent;
-			_parent.setLayout(new GridLayout(3, false));
+	// ### Variables and Constants
+	//---------------------------------------------------------------------------
+	
+	
+	private EOClassDiagram myDiagram;
+	private EOModel myCurrentModel;
+	
+	private Text myNameText;
+	
+	private DataBindingContext myBindingContext;
+	
+	private Composite parent;
+	
+	private ArrayList<Button> checkBoxes = new ArrayList<Button>();
+	
+	//---------------------------------------------------------------------------
+	// ### Construction
+	//---------------------------------------------------------------------------
+	
+	//---------------------------------------------------------------------------
+	// ### Methods
+	//---------------------------------------------------------------------------
+	
+	/**
+	 * This creates the form in the Properties Tab
+	 */
+	@Override
+	public void createControls(Composite _parent, TabbedPropertySheetPage _tabbedPropertySheetPage) {
+		super.createControls(_parent, _tabbedPropertySheetPage);
+		parent = _parent;
+		_parent.setLayout(new GridLayout(3, false));
 
-			getWidgetFactory().createCLabel(_parent, "Diagram Name", SWT.NONE);
-			myNameText = new Text(_parent, SWT.BORDER);
-			myNameText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 0));
-			UglyFocusHackWorkaroundListener.addListener(myNameText);
+		getWidgetFactory().createCLabel(_parent, "Diagram Name", SWT.NONE);
+		myNameText = new Text(_parent, SWT.BORDER);
+		myNameText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 0));
+		UglyFocusHackWorkaroundListener.addListener(myNameText);
+	}
+	
+	/**
+	 * Creates checkboxes for every entity in the EOModel.
+	 */
+	private void createEntityCheckBoxes() {
+		if (myCurrentModel == null) {
+			myCurrentModel = myDiagram._getModelParent().getModel();
+		} else if (myCurrentModel != myDiagram._getModelParent().getModel()) {
+			// die alten Checkboxen werden geloescht.
+			for (Button b : checkBoxes) {
+				b.dispose();
+			}
+			checkBoxes.clear();
+			myCurrentModel = myDiagram._getModelParent().getModel();
 		}
 		
-		/**
-		 * Creates checkboxes for every entity in the EOModel.
-		 */
-		private void createEntityCheckBoxes() {
-			if (myCurrentModel == null) {
-				myCurrentModel = myDiagram._getModelParent().getModel();
-			} else if (myCurrentModel != myDiagram._getModelParent().getModel()) {
-				// die alten Checkboxen werden geloescht.
-				for (Button b : checkBoxes) {
-					b.dispose();
-				}
-				checkBoxes.clear();
-				myCurrentModel = myDiagram._getModelParent().getModel();
-			}
+		// fuer jedes entity im EOModel wird ein Checkbox mit einem listener hinzugefuegt.
+		Iterator<EOEntity> entityIterator = myCurrentModel.getEntities().iterator();
+		while (entityIterator.hasNext()) {
+			final EOEntity entity = entityIterator.next();
 			
-			// fuer jedes entity im EOModel wird ein Checkbox mit einem listener hinzugefuegt.
-			Iterator<EOEntity> entityIterator = myCurrentModel.getEntities().iterator();
-			while (entityIterator.hasNext()) {
-				final EOEntity entity = entityIterator.next();
+			// mit dieser abfrage werden die join tables nicht als checkboxen angezeigt.
+			if (!entity.getClassNameWithoutPackage().equals("EOGenericRecord")) {
+				Button entityCheckBox = null;
+				for (Button checkbox : checkBoxes) {
+					if (entity.getName().equals(checkbox.getText())) {
+						entityCheckBox = checkbox;
+						break;
+					}
+				}
 				
-				// mit dieser abfrage werden die join tables nicht als checkboxen angezeigt.
-				if (!entity.getClassNameWithoutPackage().equals("EOGenericRecord")) {
-					Button entityCheckBox = null;
-					for (Button checkbox : checkBoxes) {
-						if (entity.getName().equals(checkbox.getText())) {
-							entityCheckBox = checkbox;
-							break;
-						}
-					}
-					
-					if (entityCheckBox == null) {
-						entityCheckBox = new Button(parent, SWT.CHECK);
-						entityCheckBox.setText(entity.getName());
-						checkBoxes.add(entityCheckBox);
-					}
-					
-					if (myDiagram.getEntities().contains(entity)) {
-						entityCheckBox.setSelection(true);
-					} else {
-						entityCheckBox.setSelection(false);
-					}
-					entityCheckBox.addSelectionListener(new SelectionAdapter() {
-		
-				        @Override
-				        public void widgetSelected(SelectionEvent event) {
-				            Button btn = (Button) event.getSource();
-				        	if (btn.getSelection()) {
-				        		myDiagram.addEntityToDiagram(entity);
-				        	} else if (!btn.getSelection()) {
-				        		myDiagram.removeEntityFromDiagram(entity);
-				        	}
-				        	DiagramTab.getInstance().setSelectedDiagram(myDiagram);
-				        	myDiagram._getModelParent().setModelDirty(true);
-				        }
-				    });
+				if (entityCheckBox == null) {
+					entityCheckBox = new Button(parent, SWT.CHECK);
+					entityCheckBox.setText(entity.getName());
+					checkBoxes.add(entityCheckBox);
 				}
+				
+				if (myDiagram.getEntities().contains(entity)) {
+					entityCheckBox.setSelection(true);
+				} else {
+					entityCheckBox.setSelection(false);
+				}
+				entityCheckBox.addSelectionListener(new SelectionAdapter() {
+	
+			        @Override
+			        public void widgetSelected(SelectionEvent event) {
+			            Button btn = (Button) event.getSource();
+			        	if (btn.getSelection()) {
+			        		myDiagram.addEntityToDiagram(entity);
+			        	} else if (!btn.getSelection()) {
+			        		myDiagram.removeEntityFromDiagram(entity);
+			        	}
+			        	DiagramTab.getInstance().setSelectedDiagram(myDiagram);
+			        	myDiagram._getModelParent().setModelDirty(true);
+			        }
+			    });
 			}
+		}
+	}
+	
+	/**
+	 * This sets the Selection and binds the data with the to a Node (Text, Label, Button, ComboViewer etc.), 
+	 * through selection we get the data which needs to be filled
+	 * in our form (which is in the Properties Tab).
+	 */
+	@Override
+	public void setInput(IWorkbenchPart part, ISelection selection) {
+		if (ComparisonUtils.equals(selection, getSelection())) {
+			return;
 		}
 		
-		/**
-		 * This sets the Selection and binds the data with the to a Node (Text, Label, Button, ComboViewer etc.), 
-		 * through selection we get the data which needs to be filled
-		 * in our form (which is in the Properties Tab).
-		 */
-		@Override
-		public void setInput(IWorkbenchPart part, ISelection selection) {
-			if (ComparisonUtils.equals(selection, getSelection())) {
-				return;
-			}
-			
-			super.setInput(part, selection);
-			disposeBindings();
-			
-			Object selectedObject = ((IStructuredSelection) selection).getFirstElement();
-			myDiagram = (EOClassDiagram) selectedObject;
-			if (myDiagram != null) {
-				myBindingContext = new DataBindingContext();
-				myBindingContext.bindValue(SWTObservables.observeText(myNameText, SWT.Modify), BeansObservables.observeValue(myDiagram, AbstractDiagram.NAME), null, null);
-				createEntityCheckBoxes();
-			}
-		}
+		super.setInput(part, selection);
+		disposeBindings();
 		
-		/**
-		 * Disposes the bindings which were created in the setInput() method
-		 */
-		protected void disposeBindings() {
-			if (myBindingContext != null) {
-				myBindingContext.dispose();
-			}
+		Object selectedObject = ((IStructuredSelection) selection).getFirstElement();
+		myDiagram = (EOClassDiagram) selectedObject;
+		if (myDiagram != null) {
+			myBindingContext = new DataBindingContext();
+			myBindingContext.bindValue(SWTObservables.observeText(myNameText, SWT.Modify), BeansObservables.observeValue(myDiagram, AbstractDiagram.NAME), null, null);
+			createEntityCheckBoxes();
 		}
-		
-		@Override
-		public void dispose() {
-			super.dispose();
-			disposeBindings();
+	}
+	
+	/**
+	 * Disposes the bindings which were created in the setInput() method
+	 */
+	protected void disposeBindings() {
+		if (myBindingContext != null) {
+			myBindingContext.dispose();
 		}
+	}
+	
+	@Override
+	public void dispose() {
+		super.dispose();
+		disposeBindings();
+	}
 }
