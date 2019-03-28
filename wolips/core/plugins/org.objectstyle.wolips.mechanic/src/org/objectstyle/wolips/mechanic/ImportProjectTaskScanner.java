e7a030473706729d279486f3c74aaa324b027c20
D Tim Cummings <tim@triptera.com.au>
2019-03-29 09:09:31 +1000

    Fix compile errors since updates to workspace mechanic in April 2014.

--- wolips/core/plugins/org.objectstyle.wolips.mechanic/src/org/objectstyle/wolips/mechanic/ImportProjectTaskScanner.java
+++ wolips/core/plugins/org.objectstyle.wolips.mechanic/src/org/objectstyle/wolips/mechanic/ImportProjectTaskScanner.java
@@ -3,7 +3,6 @@ package org.objectstyle.wolips.mechanic;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
@@ -14,12 +13,13 @@ import java.util.regex.Pattern;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 
-import com.google.eclipse.mechanic.DirectoryIteratingTaskScanner;
 import com.google.eclipse.mechanic.IResourceTaskProvider;
 import com.google.eclipse.mechanic.IResourceTaskReference;
+import com.google.eclipse.mechanic.ListCollector;
+import com.google.eclipse.mechanic.ResourceTaskScanner;
 import com.google.eclipse.mechanic.TaskCollector;
 
-public class ImportProjectTaskScanner extends DirectoryIteratingTaskScanner {
+public class ImportProjectTaskScanner extends ResourceTaskScanner {
   private static final Logger log = Logger.getLogger(ImportProjectsTask.class.getName());
 
   public ImportProjectTaskScanner() {
@@ -29,8 +29,13 @@ public class ImportProjectTaskScanner extends DirectoryIteratingTaskScanner {
   public void scan(IResourceTaskProvider source, TaskCollector collector) {
     Pattern variablePattern = Pattern.compile("^#\\s*@(\\S+)\\s+(.*)");
     
-    for (Iterator<IResourceTaskReference> iterator = source.getTaskReferences(".proj").iterator(); iterator.hasNext();) {
-      IResourceTaskReference ref = iterator.next();
+    // based on example scanner at https://github.com/alfsch/workspacemechanic/
+    // source folder: parent/bundles/com.google.eclipse.mechanic/src/
+    // source: com/google/eclipse/mechanic/internal/PreferenceFileTaskScanner.java
+    
+    ListCollector<IResourceTaskReference> taskCollector = ListCollector.create();
+    source.collectTaskReferences(".proj", taskCollector);
+    for (IResourceTaskReference ref : taskCollector.get()) {      
    	  File projectFile = ref.asFile();
       try {
         BufferedReader br = new BufferedReader(new FileReader(projectFile));
@@ -68,7 +73,7 @@ public class ImportProjectTaskScanner extends DirectoryIteratingTaskScanner {
             }
           }
           
-          collector.add(new ImportProjectsTask(id, title, description, importPaths, reconcile));
+          collector.collect(new ImportProjectsTask(id, title, description, importPaths, reconcile));
         }
         finally {
           br.close();
