f3bd974feb7bd3b6b6bde806998c76959763ef3e
Ramsey Gurley <ramseygurley@gmail.com>
2024-12-18 19:47:12 +0900

    Delete obsolete entity modeler product.

--- entityModeler/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ -z "${ECLIPSE_HOME}" ]; then
-  echo '$ECLIPSE_HOME has not been set'
-  exit 1
-fi
-java -jar ${ECLIPSE_HOME}/plugins/org.eclipse.equinox.launcher_*.jar -application org.eclipse.ant.core.antRunner -buildfile build-antrunner.xml
